bf1150e2cb832985d029899155ae652a17dfbc95
Aidan C. Brady <aidancbrady@aol.com>
2013-10-08 17:47:03 -0400

    Fixed minor GUI issues, recipe type interface is now a GuiElement Oh, I'm not dead btw

--- common/mekanism/client/gui/GuiFactory.java
+++ common/mekanism/client/gui/GuiFactory.java
@@ -21,11 +21,11 @@ public class GuiFactory extends GuiMekanism
     public GuiFactory(InventoryPlayer inventory, TileEntityFactory tentity)
     {
         super(new ContainerFactory(inventory, tentity));
-        xSize+=26;
         tileEntity = tentity;
         
         guiElements.add(new GuiRedstoneControl(this, tileEntity, tileEntity.tier.guiLocation));
         guiElements.add(new GuiUpgradeManagement(this, tileEntity, tileEntity.tier.guiLocation));
+        guiElements.add(new GuiRecipeType(this, tileEntity, tileEntity.tier.guiLocation));
     }
 
     @Override
@@ -42,7 +42,7 @@ public class GuiFactory extends GuiMekanism
         
 		if(xAxis >= 165 && xAxis <= 169 && yAxis >= 17 && yAxis <= 69)
 		{
-			drawCreativeTabHoveringText(ElectricityDisplay.getDisplayShort((float)tileEntity.electricityStored, ElectricUnit.JOULES), xAxis, yAxis);
+			drawCreativeTabHoveringText(ElectricityDisplay.getDisplayShort(tileEntity.getEnergyStored(), ElectricUnit.JOULES), xAxis, yAxis);
 		}
     }
 
@@ -64,9 +64,6 @@ public class GuiFactory extends GuiMekanism
         
         displayInt = tileEntity.getScaledEnergyLevel(52);
         drawTexturedModalRect(guiWidth + 165, guiHeight + 17 + 52 - displayInt, 176 + 26, 52 - displayInt, 4, displayInt);
-        
-        displayInt = tileEntity.getScaledRecipeProgress(15);
-        drawTexturedModalRect(guiWidth + 181, guiHeight + 94, 176 + 26, 86, 10, displayInt);
         
         if(tileEntity.tier == FactoryTier.BASIC)
         {
