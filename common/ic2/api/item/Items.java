cecff2947c8f6c9e2cf0be516d80761f91525159
Aidan Brady <aidancbrady@aol.com>
2013-10-31 22:46:16 -0400

    Update IC2 API.

--- common/ic2/api/item/Items.java
+++ common/ic2/api/item/Items.java
@@ -310,6 +310,7 @@ public final class Items {
 	// el. tools/devices/weapons
 	  miningDrill; 		// Mining Drill item, meta = damage value for charge level
 	  diamondDrill; 		// Diamond Tipped Mining Drill item, meta = damage value for charge level
+	  iridiumDrill;		// Iridium Tipped Mining Drill item, meta = damage value for charge level
 	  chainsaw; 			// Chainsaw item, meta = damage value for charge level
 	  electricWrench;		// Electric Wrench item, meta = damage value for charge level
 	  electricTreetap;	// Electric Treetap item, meta = damage value for charge level
