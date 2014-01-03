5e8dcd6c7d3e182376557a30dffb9b6b092bac1f
Aidan C. Brady <aidancbrady@aol.com>
2014-01-03 00:01:53 -0500

    Fix crash due to java being bad

--- common/mekanism/common/multipart/PartLogisticalTransporter.java
+++ common/mekanism/common/multipart/PartLogisticalTransporter.java
@@ -743,7 +743,7 @@ public class PartLogisticalTransporter extends PartSidedPipe implements ILogisti
 	@Override
 	public boolean onRightClick(EntityPlayer player, int side)
 	{
-		player.sendChatToPlayer(ChatMessageComponent.createFromText(EnumColor.DARK_BLUE + "[Mekanism]" + EnumColor.GREY + " " + MekanismUtils.localize("tooltip.configurator.viewColor") + ": " + color != null ? color.getName() : "None"));
+		player.sendChatToPlayer(ChatMessageComponent.createFromText(EnumColor.DARK_BLUE + "[Mekanism]" + EnumColor.GREY + " " + MekanismUtils.localize("tooltip.configurator.viewColor") + ": " + (color != null ? color.getName() : "None")));
 		return true;
 	}
 	
