dce4c918f4ec06b2e6e12c107b9d74f7cd7387e0
Aidan C. Brady <aidancbrady@aol.com>
2014-01-05 00:29:35 -0500

    Fix?

--- common/mekanism/common/asm/LoadingHook.java
+++ common/mekanism/common/asm/LoadingHook.java
@@ -13,31 +13,26 @@ public class LoadingHook implements IFMLLoadingPlugin
 		DepLoader.load();
 	}
 	
-	@Override
 	public String[] getLibraryRequestClass()
 	{
 		return null;
 	}
 
-	@Override
 	public String[] getASMTransformerClass()
 	{
 		return null;
 	}
 
-	@Override
 	public String getModContainerClass()
 	{
 		return null;
 	}
 
-	@Override
 	public String getSetupClass()
 	{
 		return null;
 	}
 
-	@Override
 	public void injectData(Map<String, Object> data)
 	{
 		
