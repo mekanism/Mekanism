0606d6d29f9c0af12c2149f5aa54977442ff6172
Aidan Brady <aidancbrady@aol.com>
2014-05-08 17:36:41 -0400

    Actual 6.0.4 release

--- common/mekanism/common/Mekanism.java
+++ common/mekanism/common/Mekanism.java
@@ -159,7 +159,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
  * @author AidanBrady
  *
  */
-@Mod(modid = "Mekanism", name = "Mekanism", version = "6.0.3")
+@Mod(modid = "Mekanism", name = "Mekanism", version = "6.0.4")
 @NetworkMod(channels = {"MEK"}, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class Mekanism
 {
@@ -184,7 +184,7 @@ public class Mekanism
 	public static Configuration configuration;
 
 	/** Mekanism version number */
-	public static Version versionNumber = new Version(6, 0, 3);
+	public static Version versionNumber = new Version(6, 0, 4);
 
 	/** Map of Teleporters */
 	public static Map<Teleporter.Code, ArrayList<Coord4D>> teleporters = new HashMap<Teleporter.Code, ArrayList<Coord4D>>();
