bc3636aaecfe6c05329b8fc4bf72ecbb340a17f1
Aidan C. Brady <aidancbrady@aol.com>
2014-06-26 01:20:09 -0400

    Quick cleanup

--- src/main/java/mekanism/common/Mekanism.java
+++ src/main/java/mekanism/common/Mekanism.java
@@ -625,13 +625,13 @@ public class Mekanism
             "SBS", Character.valueOf('S'), "ingotSteel", Character.valueOf('B'), Items.bucket
         }));
 		CraftingManager.getInstance().getRecipeList().add(new MekanismRecipe(new ItemStack(PartTransmitter, 7, 9), new Object[] {
-				"ETE", "TTT", "TTT", Character.valueOf('E'), EnrichedAlloy, Character.valueOf('T'), new ItemStack(PartTransmitter, 1, 4)
+			"ETE", "TTT", "TTT", Character.valueOf('E'), EnrichedAlloy, Character.valueOf('T'), new ItemStack(PartTransmitter, 1, 4)
 		}));
 		CraftingManager.getInstance().getRecipeList().add(new MekanismRecipe(new ItemStack(PartTransmitter, 7, 10), new Object[] {
-				"CTC", "TTT", "TTT", Character.valueOf('C'), "circuitBasic", Character.valueOf('T'), new ItemStack(PartTransmitter, 1, 9)
+			"CTC", "TTT", "TTT", Character.valueOf('C'), "circuitBasic", Character.valueOf('T'), new ItemStack(PartTransmitter, 1, 9)
 		}));
 		CraftingManager.getInstance().getRecipeList().add(new MekanismRecipe(new ItemStack(PartTransmitter, 7, 11), new Object[] {
-				"CTC", "TTT", "TTT", Character.valueOf('C'), AtomicCore, Character.valueOf('T'), new ItemStack(PartTransmitter, 1, 10)
+			"CTC", "TTT", "TTT", Character.valueOf('C'), AtomicCore, Character.valueOf('T'), new ItemStack(PartTransmitter, 1, 10)
 		}));
         CraftingManager.getInstance().getRecipeList().add(new MekanismRecipe(new ItemStack(PartTransmitter, 8, 5), new Object[] {
             "SGS", Character.valueOf('S'), "ingotSteel", Character.valueOf('G'), Blocks.glass
