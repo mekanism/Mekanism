dc922a3914d17b6da42494c18cf7f827eea3c5ef
Ben Spiers <ben.spiers22@gmail.com>
2014-08-13 03:37:30 +0100

    Remove two entirely unreferenced Config variables from the config class.
    logPackets is unused as well, but is still present in the config file.
    Presumably got lost in the 1.7 update. @aidancbrady, should we reimplement
    the functionality, or remove the option?

--- src/main/java/mekanism/api/MekanismConfig.java
+++ src/main/java/mekanism/api/MekanismConfig.java
@@ -31,8 +31,6 @@ public class MekanismConfig
 		public static double TO_IC2;
 		public static double TO_BC;
 		public static double TO_TE;
-		public static double TO_UE = .001;
-		public static double FROM_UE = 1/TO_UE;
 		public static double FROM_H2;
 		public static double FROM_IC2;
 		public static double FROM_BC;
