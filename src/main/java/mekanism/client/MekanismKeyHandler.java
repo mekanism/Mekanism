c81da2aebecf559dc3b1404f2f1ca641a8966bef
Ben Spiers <ben.spiers22@gmail.com>
2014-08-27 02:15:09 +0100

    Remove unneccessary logging

--- src/main/java/mekanism/client/MekanismKeyHandler.java
+++ src/main/java/mekanism/client/MekanismKeyHandler.java
@@ -67,7 +67,6 @@ public class MekanismKeyHandler extends MekKeyHandler
 	@Override
 	public void keyDown(Type types, KeyBinding kb, boolean tickEnd, boolean isRepeat)
 	{
-		Mekanism.logger.info(isRepeat ? "Repeating" : "Not Repeating");
 		if(kb == modeSwitchKey)
 		{
 			EntityPlayer player = FMLClientHandler.instance().getClient().thePlayer;
