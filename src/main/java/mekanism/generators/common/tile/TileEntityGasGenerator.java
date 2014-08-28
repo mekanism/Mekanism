e3fc6945325eaa22543b9731548da81b45301de2
Ben Spiers <ben.spiers22@gmail.com>
2014-08-28 20:00:56 +0100

    Fix Gas Generator item gas tank restoration.

--- src/main/java/mekanism/generators/common/tile/TileEntityGasGenerator.java
+++ src/main/java/mekanism/generators/common/tile/TileEntityGasGenerator.java
@@ -290,6 +290,6 @@ public class TileEntityGasGenerator extends TileEntityGenerator implements IGasH
 	@Override
 	public void readSustainedData(ItemStack itemStack) 
 	{
-		fuelTank.setGas(GasStack.readFromNBT(itemStack.stackTagCompound.getCompoundTag("fuelTank")));
+		fuelTank.read(itemStack.stackTagCompound.getCompoundTag("fuelTank"));
 	}
 }
