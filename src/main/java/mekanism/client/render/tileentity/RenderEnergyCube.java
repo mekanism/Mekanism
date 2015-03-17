6847a1e9eeabd64e905d8fcb2632f40c906d4579
Aidan C. Brady <aidancbrady@aol.com>
2015-03-17 17:36:32 -0400

    Minor cleanup

--- src/main/java/mekanism/client/render/tileentity/RenderEnergyCube.java
+++ src/main/java/mekanism/client/render/tileentity/RenderEnergyCube.java
@@ -57,7 +57,6 @@ public class RenderEnergyCube extends TileEntitySpecialRenderer
 
 		if(tileEntity.getEnergy()/tileEntity.getMaxEnergy() > 0.1)
 		{
-
 			GL11.glPushMatrix();
 			GL11.glTranslated(x + 0.5, y + 0.5, z + 0.5);
 			bindTexture(MekanismUtils.getResource(ResourceType.RENDER, "EnergyCore.png"));
@@ -67,16 +66,14 @@ public class RenderEnergyCube extends TileEntitySpecialRenderer
 
 			EnumColor c = tileEntity.tier.getBaseTier().getColor();
 
-
 			GL11.glPushMatrix();
 			GL11.glScalef(0.4F, 0.4F, 0.4F);
-			GL11.glColor4f(c.getColor(0), c.getColor(1), c.getColor(2), (float) (tileEntity.getEnergy() / tileEntity.getMaxEnergy()));
-			GL11.glTranslatef(0, (float) Math.sin(Math.toRadians((MekanismClient.ticksPassed + partialTick) * 3)) / 7, 0);
+			GL11.glColor4f(c.getColor(0), c.getColor(1), c.getColor(2), (float)(tileEntity.getEnergy() / tileEntity.getMaxEnergy()));
+			GL11.glTranslatef(0, (float)Math.sin(Math.toRadians((MekanismClient.ticksPassed + partialTick) * 3)) / 7, 0);
 			GL11.glRotatef((MekanismClient.ticksPassed + partialTick) * 4, 0, 1, 0);
 			GL11.glRotatef(36F + (MekanismClient.ticksPassed + partialTick) * 4, 0, 1, 1);
 			core.render(0.0625F);
 			GL11.glPopMatrix();
-
 
 			MekanismRenderer.glowOff();
 			MekanismRenderer.blendOff();
