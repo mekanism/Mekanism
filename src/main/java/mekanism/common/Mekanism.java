2546d9c28eff45b903416d7de4f983949faf9e7d
Aidan C. Brady <aidancbrady@aol.com>
2015-03-26 18:08:56 -0400

    Bump version to 8.0.3

--- src/main/java/mekanism/common/Mekanism.java
+++ src/main/java/mekanism/common/Mekanism.java
@@ -133,7 +133,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
  * @author AidanBrady
  *
  */
-@Mod(modid = "Mekanism", name = "Mekanism", version = "8.0.2", guiFactory = "mekanism.client.gui.ConfigGuiFactory",
+@Mod(modid = "Mekanism", name = "Mekanism", version = "8.0.3", guiFactory = "mekanism.client.gui.ConfigGuiFactory",
 		dependencies = "after:ForgeMultipart;after:BuildCraft;after:BuildCraftAPI;after:IC2;after:CoFHCore;" +
 				"after:ComputerCraft;after:Galacticraft API;after:MetallurgyCore")
 public class Mekanism
@@ -162,7 +162,7 @@ public class Mekanism
     public static Configuration configuration;
     
 	/** Mekanism version number */
-	public static Version versionNumber = new Version(8, 0, 2);
+	public static Version versionNumber = new Version(8, 0, 3);
 	
 	/** MultiblockManagers for various structrures */
 	public static MultiblockManager<SynchronizedTankData> tankManager = new MultiblockManager<SynchronizedTankData>("dynamicTank");
