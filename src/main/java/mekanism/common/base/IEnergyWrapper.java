d51f40864eb050584418154b1d5e3b0d832d0388
yogpstop <git@yogpc.com>
2015-03-26 02:36:10 +0000

    Fix API package name

--- src/main/java/mekanism/common/base/IEnergyWrapper.java
+++ src/main/java/mekanism/common/base/IEnergyWrapper.java
@@ -18,7 +18,7 @@ import cpw.mods.fml.common.Optional.InterfaceList;
 @InterfaceList({
 	@Interface(iface = "ic2.api.energy.tile.IEnergySink", modid = "IC2"),
 	@Interface(iface = "ic2.api.energy.tile.IEnergySource", modid = "IC2"),
-	@Interface(iface = "ic2.api.tile.IEnergyStorage", modid = "IC2"),
+	@Interface(iface = "ic2.api.energy.tile.IEnergyStorage", modid = "IC2"),
 	@Interface(iface = "cofh.api.energy.IEnergyHandler", modid = "CoFHCore"),
 })
 public interface IEnergyWrapper extends IStrictEnergyStorage, IEnergyHandler, IEnergySink, IEnergySource, IEnergyStorage, IStrictEnergyAcceptor, ICableOutputter, IInventory
