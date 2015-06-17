6bdbb41b2daddc0fbc4a200bb87cfe23fd38fb5d
aidancbrady <me@aidancbrady.com>
2015-06-17 15:30:52 -0400

    Attempt a packet log check

--- src/main/java/mekanism/common/network/PacketTileEntity.java
+++ src/main/java/mekanism/common/network/PacketTileEntity.java
@@ -1,18 +1,20 @@
 package mekanism.common.network;
 
+import io.netty.buffer.ByteBuf;
+
 import java.util.ArrayList;
 
 import mekanism.api.Coord4D;
 import mekanism.common.PacketHandler;
 import mekanism.common.base.ITileNetwork;
 import mekanism.common.network.PacketTileEntity.TileEntityMessage;
-
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
-
-import io.netty.buffer.ByteBuf;
+import cpw.mods.fml.relauncher.Side;
 
 public class PacketTileEntity implements IMessageHandler<TileEntityMessage, IMessage>
 {
@@ -59,7 +61,11 @@ public class PacketTileEntity implements IMessageHandler<TileEntityMessage, IMes
 			dataStream.writeInt(coord4D.zCoord);
 			dataStream.writeInt(coord4D.dimensionId);
 			
-			PacketHandler.log("Sending TileEntity packet from coordinate " + coord4D);
+			if(FMLCommonHandler.instance().getSide() == Side.SERVER)
+			{
+				World world = FMLCommonHandler.instance().getMinecraftServerInstance().worldServerForDimension(coord4D.dimensionId);
+				PacketHandler.log("Sending TileEntity packet from coordinate " + coord4D + " (" + coord4D.getTileEntity(world) + ")");
+			}
 	
 			PacketHandler.encode(new Object[] {parameters}, dataStream);
 		}
