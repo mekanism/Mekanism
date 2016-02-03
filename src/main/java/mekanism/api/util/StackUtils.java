7e7b89477e2cbcb06ec1311f1a8c05ca226dd89a
Steven Peter Krauel <geekconglomerate@gmail.com>
2016-02-02 18:22:11 -0700

    add null check here
    I'm fairly certain that this check will stop a world unloading glitch triggered by turning off ore generation in some other mods

--- src/main/java/mekanism/api/util/StackUtils.java
+++ src/main/java/mekanism/api/util/StackUtils.java
@@ -59,6 +59,11 @@ public final class StackUtils
 		{
 			return check == wild;
 		}
+		
+		if (OreDictionary.WILDCARD_VALUE == null)
+		{
+			return false;
+		}
 		return wild.getItem() == check.getItem() && (wild.getItemDamage() == OreDictionary.WILDCARD_VALUE || wild.getItemDamage() == check.getItemDamage());
 	}
 
