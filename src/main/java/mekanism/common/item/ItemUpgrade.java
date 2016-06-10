0723168c4c81f0a022745378b3e27f05e3f7776c
aidancbrady <me@aidancbrady.com>
2016-06-10 11:51:47 -0400

    Allow shift-right clicking machines with upgrades

--- src/main/java/mekanism/common/item/ItemUpgrade.java
+++ src/main/java/mekanism/common/item/ItemUpgrade.java
@@ -5,10 +5,14 @@ import java.util.List;
 import mekanism.api.EnumColor;
 import mekanism.common.Upgrade;
 import mekanism.common.base.IUpgradeItem;
+import mekanism.common.base.IUpgradeTile;
+import mekanism.common.tile.component.TileComponentUpgrade;
 import mekanism.common.util.LangUtils;
 import mekanism.common.util.MekanismUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 import org.lwjgl.input.Keyboard;
 
@@ -43,5 +47,38 @@ public class ItemUpgrade extends ItemMekanism implements IUpgradeItem
 	public Upgrade getUpgradeType(ItemStack stack) 
 	{
 		return upgrade;
+	}
+	
+	@Override
+	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
+	{
+		if(world.isRemote) 
+		{
+			return false;
+		}
+		
+		if(player.isSneaking())
+		{
+			TileEntity tile = world.getTileEntity(x, y, z);
+			Upgrade type = getUpgradeType(stack);
+			
+			if(tile instanceof IUpgradeTile)
+			{
+				TileComponentUpgrade component = ((IUpgradeTile)tile).getComponent();
+				
+				if(component.supports(type))
+				{
+					if(component.getUpgrades(type) < type.getMax())
+					{
+						component.addUpgrade(type);
+						stack.stackSize--;
+					}
+				}
+				
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
