0df044a869071219d1ce4258df79b9fe5a6dc433
Marcel Petzold <info@deltanet-production.de>
2016-07-16 22:41:13 +0200

    fixed typo

--- src/main/java/mekanism/additions/client/AdditionsClientProxy.java
+++ src/main/java/mekanism/additions/client/AdditionsClientProxy.java
@@ -2,5 +2,5 @@ package mekanism.additions.client;
 
 import mekanism.additions.common.AdditionsCommonProxy;
 
-public class AdditonsClientProxy extends AdditionsCommonProxy {
+public class AdditionsClientProxy extends AdditionsCommonProxy {
 }
