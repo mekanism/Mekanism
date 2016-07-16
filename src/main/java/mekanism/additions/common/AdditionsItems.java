b97350229b4573fb29c84a0f2362254ef626b795
Marcel Petzold <info@deltanet-production.de>
2016-07-16 22:45:54 +0200

    fixed missing gasses

--- src/main/java/mekanism/additions/common/AdditionsItems.java
+++ src/main/java/mekanism/additions/common/AdditionsItems.java
@@ -1,5 +1,6 @@
 package mekanism.additions.common;
 
+import mekanism.api.gas.Gas;
 import mekanism.api.gas.GasRegistry;
 import mekanism.common.item.ItemMekanism;
 import net.minecraft.init.Items;
@@ -19,6 +20,16 @@ public class AdditionsItems {
     public static final Item EnrichedDihydrogenSulfidIII = new ItemMekanism().setMaxStackSize(1).setContainerItem(Items.bucket).setUnlocalizedName("EnrichedDihydrogenSulfidIIIBucket");
 
     public static void register() {
+
+        GasRegistry.register(new Gas("enrichedwater")).registerFluid();
+        GasRegistry.register(new Gas("enrichedwatersnd")).registerFluid();
+        GasRegistry.register(new Gas("enrichedwaterrd")).registerFluid();
+        GasRegistry.register(new Gas("dihydrogensulfid")).registerFluid();
+
+        GasRegistry.register(new Gas("enricheddihydrogensulfidGas")).registerFluid();
+        GasRegistry.register(new Gas("enricheddihydrogensulfidsnd")).registerFluid();
+        GasRegistry.register(new Gas("enricheddihydrogensulfidrd")).registerFluid();
+
         /** new fluids for GPS */
         FluidContainerRegistry.registerFluidContainer(FluidRegistry.getFluid("enrichedwater"), new ItemStack(EnrichedWaterI), FluidContainerRegistry.EMPTY_BUCKET);
         FluidContainerRegistry.registerFluidContainer(FluidRegistry.getFluid("enrichedwatersnd"), new ItemStack(EnrichedWaterII), FluidContainerRegistry.EMPTY_BUCKET);
