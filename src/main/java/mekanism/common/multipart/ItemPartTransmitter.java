80ccee9110d089b3bc208b72b47701eb5d31e180
aidancbrady <me@aidancbrady.com>
2017-04-28 14:58:44 -0400

    Add missing tooltip to Thermodynamic Conductor
    (cherry picked from commit e0229d281510779ceb070475cb3735e33dc0b2c3)

--- src/main/java/mekanism/common/multipart/ItemPartTransmitter.java
+++ src/main/java/mekanism/common/multipart/ItemPartTransmitter.java
@@ -75,6 +75,12 @@ public class ItemPartTransmitter extends ItemMultiPart implements IMetaItem
 				list.add(EnumColor.INDIGO + LangUtils.localize("tooltip.speed") + ": " + EnumColor.GREY + (Tier.TransporterTier.get(tier).speed/(100/20)) + " m/s");
 				list.add(EnumColor.INDIGO + LangUtils.localize("tooltip.pumpRate") + ": " + EnumColor.GREY + Tier.TransporterTier.get(tier).pullAmount*2 + "/s");
 			}
+			else if(transmission == TransmissionType.HEAT)
+			{
+				list.add(EnumColor.INDIGO + LangUtils.localize("tooltip.conduction") + ": " + EnumColor.GREY + Tier.ConductorTier.get(tier).inverseConduction);
+				list.add(EnumColor.INDIGO + LangUtils.localize("tooltip.insulation") + ": " + EnumColor.GREY + Tier.ConductorTier.get(tier).inverseConductionInsulation);
+				list.add(EnumColor.INDIGO + LangUtils.localize("tooltip.heatCapacity") + ": " + EnumColor.GREY + Tier.ConductorTier.get(tier).inverseHeatCapacity);
+			}
 
 			list.add(LangUtils.localize("tooltip.hold") + " " + EnumColor.AQUA + GameSettings.getKeyDisplayString(MekanismKeyHandler.sneakKey.getKeyCode()) + EnumColor.GREY + " " + LangUtils.localize("tooltip.forDetails"));
 		}
