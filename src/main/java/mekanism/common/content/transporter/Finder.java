3deecd8a64d7214d48eb927701d7fea85fb0eadf
Tom Elfring <tomelfring@gmail.com>
2017-06-02 19:10:05 +0200

    Allow all items to be filtered on modid
    Fixes #3856

--- src/main/java/mekanism/common/content/transporter/Finder.java
+++ src/main/java/mekanism/common/content/transporter/Finder.java
@@ -124,7 +124,7 @@ public abstract class Finder
 		@Override
 		public boolean modifies(ItemStack stack)
 		{
-			if(stack == null || !(stack.getItem() instanceof ItemBlock))
+			if(stack == null)
 			{
 				return false;
 			}
