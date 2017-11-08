08a13cff4f73e927671dc15891d30f8fa9216642
Xander Victory <x@xandervictory.id.au>
2017-11-08 11:28:56 +0800

    check init phase in recipe handler

--- src/main/java/mekanism/common/recipe/APIHandler.java
+++ src/main/java/mekanism/common/recipe/APIHandler.java
@@ -1,100 +1,125 @@
 package mekanism.common.recipe;
 
+import com.google.common.base.Preconditions;
 import mekanism.api.MekanismRecipeHelper;
 import mekanism.api.gas.Gas;
 import mekanism.api.gas.GasStack;
 import mekanism.api.infuse.InfuseType;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.LoaderState;
 
 public class APIHandler implements MekanismRecipeHelper {
-    @Override
-    public void addEnrichmentChamberRecipe(ItemStack input, ItemStack output) {
-        RecipeHandler.addEnrichmentChamberRecipe(input, output);
-    }
+	private static void checkPhase(){
+		Preconditions.checkState(Loader.instance().getLoaderState().ordinal() > LoaderState.INITIALIZATION.ordinal(), "Recipes should be registered during Init.");
+	}
+	
+	@Override
+	public void addEnrichmentChamberRecipe(ItemStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addEnrichmentChamberRecipe(input, output);
+	}
 
-    @Override
-    public void addOsmiumCompressorRecipe(ItemStack input, ItemStack output) {
-        RecipeHandler.addOsmiumCompressorRecipe(input, output);
-    }
+	@Override
+	public void addOsmiumCompressorRecipe(ItemStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addOsmiumCompressorRecipe(input, output);
+	}
 
-    @Override
-    public void addCombinerRecipe(ItemStack input, ItemStack output) {
-        RecipeHandler.addCombinerRecipe(input, output);
-    }
+	@Override
+	public void addCombinerRecipe(ItemStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addCombinerRecipe(input, output);
+	}
 
-    @Override
-    public void addCrusherRecipe(ItemStack input, ItemStack output) {
-        RecipeHandler.addCrusherRecipe(input, output);
-    }
+	@Override
+	public void addCrusherRecipe(ItemStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addCrusherRecipe(input, output);
+	}
 
-    @Override
-    public void addPurificationChamberRecipe(ItemStack input, ItemStack output) {
-        RecipeHandler.addPurificationChamberRecipe(input, output);
-    }
+	@Override
+	public void addPurificationChamberRecipe(ItemStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addPurificationChamberRecipe(input, output);
+	}
 
-    @Override
-    public void addMetallurgicInfuserRecipe(InfuseType infuse, int amount, ItemStack input, ItemStack output) {
-        RecipeHandler.addMetallurgicInfuserRecipe(infuse, amount, input, output);
-    }
+	@Override
+	public void addMetallurgicInfuserRecipe(InfuseType infuse, int amount, ItemStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addMetallurgicInfuserRecipe(infuse, amount, input, output);
+	}
 
-    @Override
-    public void addChemicalInfuserRecipe(GasStack leftInput, GasStack rightInput, GasStack output) {
-        RecipeHandler.addChemicalInfuserRecipe(leftInput, rightInput, output);
-    }
+	@Override
+	public void addChemicalInfuserRecipe(GasStack leftInput, GasStack rightInput, GasStack output) {
+		checkPhase();
+		RecipeHandler.addChemicalInfuserRecipe(leftInput, rightInput, output);
+	}
 
-    @Override
-    public void addChemicalOxidizerRecipe(ItemStack input, GasStack output) {
-        RecipeHandler.addChemicalOxidizerRecipe(input, output);
-    }
+	@Override
+	public void addChemicalOxidizerRecipe(ItemStack input, GasStack output) {
+		checkPhase();
+		RecipeHandler.addChemicalOxidizerRecipe(input, output);
+	}
 
-    @Override
-    public void addChemicalInjectionChamberRecipe(ItemStack input, Gas gas, ItemStack output) {
-        RecipeHandler.addChemicalInjectionChamberRecipe(input, gas, output);
-    }
+	@Override
+	public void addChemicalInjectionChamberRecipe(ItemStack input, Gas gas, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addChemicalInjectionChamberRecipe(input, gas, output);
+	}
 
-    @Override
-    public void addElectrolyticSeparatorRecipe(FluidStack fluid, double energy, GasStack leftOutput, GasStack rightOutput) {
-        RecipeHandler.addElectrolyticSeparatorRecipe(fluid, energy, leftOutput, rightOutput);
-    }
+	@Override
+	public void addElectrolyticSeparatorRecipe(FluidStack fluid, double energy, GasStack leftOutput, GasStack rightOutput) {
+		checkPhase();
+		RecipeHandler.addElectrolyticSeparatorRecipe(fluid, energy, leftOutput, rightOutput);
+	}
 
-    @Override
-    public void addPrecisionSawmillRecipe(ItemStack input, ItemStack primaryOutput, ItemStack secondaryOutput, double chance) {
-        RecipeHandler.addPrecisionSawmillRecipe(input, primaryOutput, secondaryOutput, chance);
-    }
+	@Override
+	public void addPrecisionSawmillRecipe(ItemStack input, ItemStack primaryOutput, ItemStack secondaryOutput, double chance) {
+		checkPhase();
+		RecipeHandler.addPrecisionSawmillRecipe(input, primaryOutput, secondaryOutput, chance);
+	}
 
-    @Override
-    public void addPrecisionSawmillRecipe(ItemStack input, ItemStack primaryOutput) {
-        RecipeHandler.addPrecisionSawmillRecipe(input, primaryOutput);
-    }
+	@Override
+	public void addPrecisionSawmillRecipe(ItemStack input, ItemStack primaryOutput) {
+		checkPhase();
+		RecipeHandler.addPrecisionSawmillRecipe(input, primaryOutput);
+	}
 
-    @Override
-    public void addChemicalDissolutionChamberRecipe(ItemStack input, GasStack output) {
-        RecipeHandler.addChemicalDissolutionChamberRecipe(input, output);
-    }
+	@Override
+	public void addChemicalDissolutionChamberRecipe(ItemStack input, GasStack output) {
+		checkPhase();
+		RecipeHandler.addChemicalDissolutionChamberRecipe(input, output);
+	}
 
-    @Override
-    public void addChemicalWasherRecipe(GasStack input, GasStack output) {
-        RecipeHandler.addChemicalWasherRecipe(input, output);
-    }
+	@Override
+	public void addChemicalWasherRecipe(GasStack input, GasStack output) {
+		checkPhase();
+		RecipeHandler.addChemicalWasherRecipe(input, output);
+	}
 
-    @Override
-    public void addChemicalCrystallizerRecipe(GasStack input, ItemStack output) {
-        RecipeHandler.addChemicalCrystallizerRecipe(input, output);
-    }
+	@Override
+	public void addChemicalCrystallizerRecipe(GasStack input, ItemStack output) {
+		checkPhase();
+		RecipeHandler.addChemicalCrystallizerRecipe(input, output);
+	}
 
-    @Override
-    public void addPRCRecipe(ItemStack inputSolid, FluidStack inputFluid, GasStack inputGas, ItemStack outputSolid, GasStack outputGas, double extraEnergy, int ticks) {
-        RecipeHandler.addPRCRecipe(inputSolid, inputFluid, inputGas, outputSolid, outputGas, extraEnergy, ticks);
-    }
+	@Override
+	public void addPRCRecipe(ItemStack inputSolid, FluidStack inputFluid, GasStack inputGas, ItemStack outputSolid, GasStack outputGas, double extraEnergy, int ticks) {
+		checkPhase();
+		RecipeHandler.addPRCRecipe(inputSolid, inputFluid, inputGas, outputSolid, outputGas, extraEnergy, ticks);
+	}
 
-    @Override
-    public void addThermalEvaporationRecipe(FluidStack inputFluid, FluidStack outputFluid) {
-        RecipeHandler.addThermalEvaporationRecipe(inputFluid, outputFluid);
-    }
+	@Override
+	public void addThermalEvaporationRecipe(FluidStack inputFluid, FluidStack outputFluid) {
+		checkPhase();
+		RecipeHandler.addThermalEvaporationRecipe(inputFluid, outputFluid);
+	}
 
-    @Override
-    public void addSolarNeutronRecipe(GasStack inputGas, GasStack outputGas) {
-        RecipeHandler.addSolarNeutronRecipe(inputGas, outputGas);
-    }
+	@Override
+	public void addSolarNeutronRecipe(GasStack inputGas, GasStack outputGas) {
+		checkPhase();
+		RecipeHandler.addSolarNeutronRecipe(inputGas, outputGas);
+	}
 }
