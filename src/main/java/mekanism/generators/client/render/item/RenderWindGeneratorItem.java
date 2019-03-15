e2fd77fc1cbb304e790f279bafbc1e50c9b06566
pupnewfster <richard@freimer.com>
2019-03-15 17:59:48 -0400

    Make the wind generator item spin

--- src/main/java/mekanism/generators/client/render/item/RenderWindGeneratorItem.java
+++ src/main/java/mekanism/generators/client/render/item/RenderWindGeneratorItem.java
@@ -12,12 +12,20 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class RenderWindGeneratorItem {
 
     private static ModelWindGenerator windGenerator = new ModelWindGenerator();
+    private static int angle = 0;
 
     public static void renderStack() {
+        GlStateManager.pushMatrix();
         GlStateManager.rotate(180F, 0.0F, 0.0F, 1.0F);
         GlStateManager.rotate(180F, 0.0F, 1.0F, 0.0F);
         GlStateManager.translate(0.0F, 0.4F, 0.0F);
         MekanismRenderer.bindTexture(MekanismUtils.getResource(ResourceType.RENDER, "WindGenerator.png"));
-        windGenerator.render(0.016F, 0);
+        //TODO: Only update angle if the player is not in a blacklisted dimension, one that has no "wind".
+        //The best way to do this would be to add an event listener for dimension change.
+        //The event is server side only so we would need to send a packet to clients to tell them if they are
+        //in a blacklisted dimension or not.
+        angle = (angle + 2) % 360;
+        windGenerator.render(0.016F, angle);
+        GlStateManager.popMatrix();
     }
 }
