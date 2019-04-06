737e56b2eecdeac6bc78ca51a7a10c562911ef45
pupnewfster <richard@freimer.com>
2019-04-05 20:10:11 -0400

    Port 486e5d451 clamp int for forge energy wrapper

--- src/main/java/mekanism/common/util/MekanismUtils.java
+++ src/main/java/mekanism/common/util/MekanismUtils.java
@@ -889,7 +889,7 @@ public final class MekanismUtils {
             renderer = stack.getItem().getFontRenderer(stack);
         }
 
-        if(renderer != null) {
+        if (renderer != null) {
             return renderer.listFormattedStringToWidth(s, 200);
         } else {
             return Collections.emptyList();
@@ -1115,6 +1115,29 @@ public final class MekanismUtils {
     public static TileEntity getTileEntitySafe(IBlockAccess worldIn, BlockPos pos) {
         return worldIn instanceof ChunkCache ? ((ChunkCache) worldIn)
               .getTileEntity(pos, Chunk.EnumCreateEntityType.CHECK) : worldIn.getTileEntity(pos);
+    }
+
+    /**
+     * Clamp a double to int without using Math.min due to double representation issues. Primary use: power systems that
+     * use int, where Mek uses doubles internally
+     *
+     * <code>
+     * double d = 1e300; // way bigger than longs, so the long should always be what's returned by Math.min
+     * System.out.println((long)Math.min(123456781234567812L, d)); // result is 123456781234567808 - 4 less than what
+     * you'd expect System.out.println((long)Math.min(123456789012345678L, d)); // result is 123456789012345680 - 2 more
+     * than what you'd expect
+     * </code>
+     *
+     * @param d double to clamp
+     * @return an int clamped to Integer.MAX_VALUE
+     * @see <a href="https://github.com/aidancbrady/Mekanism/pull/5203">Original PR</a>
+     */
+    public static int clampToInt(double d) {
+        if (d < Integer.MAX_VALUE) {
+            return (int) d;
+        } else {
+            return Integer.MAX_VALUE;
+        }
     }
 
     public enum ResourceType {
