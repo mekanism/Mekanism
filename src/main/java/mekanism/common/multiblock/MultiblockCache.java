d336ba35cfc90e511ff3afeace865c89466f9809
pupnewfster <richard@freimer.com>
2019-05-24 01:04:12 -0400

    Make a couple sets more generic

--- src/main/java/mekanism/common/multiblock/MultiblockCache.java
+++ src/main/java/mekanism/common/multiblock/MultiblockCache.java
@@ -1,12 +1,13 @@
 package mekanism.common.multiblock;
 
 import java.util.HashSet;
+import java.util.Set;
 import mekanism.api.Coord4D;
 import net.minecraft.nbt.NBTTagCompound;
 
 public abstract class MultiblockCache<T extends SynchronizedData<T>> {
 
-    public HashSet<Coord4D> locations = new HashSet<>();
+    public Set<Coord4D> locations = new HashSet<>();
 
     public abstract void apply(T data);
 
