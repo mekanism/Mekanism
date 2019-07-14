db8dfa749ad2e10fc71417c1df70efde7b2cf53a
pupnewfster <richard@freimer.com>
2019-07-14 13:59:58 -0400

    Start making GuiSeismicReader more closely mirror GuiMekanism

--- src/main/java/mekanism/client/gui/GuiSeismicReader.java
+++ src/main/java/mekanism/client/gui/GuiSeismicReader.java
@@ -2,18 +2,24 @@ package mekanism.client.gui;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import javax.annotation.Nonnull;
 import mekanism.api.Coord4D;
+import mekanism.client.gui.button.GuiButtonSeismicReader;
 import mekanism.client.render.MekanismRenderer;
+import mekanism.common.Mekanism;
 import mekanism.common.util.LangUtils;
 import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.MekanismUtils.ResourceType;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -29,8 +35,10 @@ public class GuiSeismicReader extends GuiScreen {
     private int xSize = 137;
     private int ySize = 182;
     private World worldObj;
-    private ArrayList<Pair<Integer, Block>> blockList = new ArrayList<>();
-    private Rectangle upButton, downButton, tooltip;
+    private List<Pair<Integer, Block>> blockList = new ArrayList<>();
+    private Rectangle tooltip;
+    private GuiButton upButton;
+    private GuiButton downButton;
 
     private int currentLayer;
 
@@ -47,36 +55,29 @@ public class GuiSeismicReader extends GuiScreen {
         super.initGui();
         int guiLeft = (width - xSize) / 2;
         int guiTop = (height - ySize) / 2;
-        upButton = new Rectangle(guiLeft + 70, guiTop + 75, 13, 13);
-        downButton = new Rectangle(guiLeft + 70, guiTop + 92, 13, 13);
+        buttonList.add(upButton = new GuiButtonSeismicReader(0, guiLeft + 70, guiTop + 75, 13, 13, 137, 0, getGuiLocation()));
+        buttonList.add(downButton = new GuiButtonSeismicReader(1, guiLeft + 70, guiTop + 92, 13, 13, 150, 0, getGuiLocation()));
         tooltip = new Rectangle(guiLeft + 30, guiTop + 82, 16, 16);
+        updateEnabledButtons();
+    }
+
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
+        updateEnabledButtons();
+    }
+
+    private void updateEnabledButtons() {
+        upButton.enabled = currentLayer + 1 <= blockList.size() - 1;
+        downButton.enabled = currentLayer - 1 >= 1;
     }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTick) {
         int guiLeft = (width - xSize) / 2;
         int guiTop = (height - ySize) / 2;
-        mc.renderEngine.bindTexture(MekanismUtils.getResource(ResourceType.GUI, "GuiSeismicReader.png"));
+        mc.renderEngine.bindTexture(getGuiLocation());
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-        // Draws the up button
-        boolean upIntersects = upButton.intersects(new Rectangle(mouseX, mouseY, 1, 1));
-        if (upIntersects) {
-            GlStateManager.color(0.5F, 0.5F, 1);
-        }
-        drawTexturedModalRect(upButton.getX(), upButton.getY(), 137, 0, upButton.getWidth(), upButton.getHeight());
-        if (upIntersects) {
-            MekanismRenderer.resetColor();
-        }
-
-        // Draws the down button
-        boolean downIntersects = downButton.intersects(new Rectangle(mouseX, mouseY, 1, 1));
-        if (downIntersects) {
-            GlStateManager.color(0.5F, 0.5F, 1);
-        }
-        drawTexturedModalRect(downButton.getX(), downButton.getY(), 150, 0, downButton.getWidth(), downButton.getHeight());
-        if (downIntersects) {
-            MekanismRenderer.resetColor();
-        }
 
         // Fix the overlapping if > 100
         GlStateManager.pushMatrix();
@@ -95,7 +96,8 @@ public class GuiSeismicReader extends GuiScreen {
             int centralX = guiLeft + 32, centralY = guiTop + 103;
             int layer = currentLayer + (i - 5);
             if (0 <= layer && layer < blockList.size()) {
-                ItemStack stack = new ItemStack(blockList.get(layer).getRight(), 1, blockList.get(layer).getLeft());
+                Pair<Integer, Block> integerBlockPair = blockList.get(layer);
+                ItemStack stack = new ItemStack(integerBlockPair.getRight(), 1, integerBlockPair.getLeft());
                 GlStateManager.pushMatrix();
                 GlStateManager.translate(centralX - 2, centralY - i * 16 + (22 * 2), 0);
                 if (i < 4) {
@@ -105,18 +107,16 @@ public class GuiSeismicReader extends GuiScreen {
                     GlStateManager.translate(1.5F, 0, 0);
                     GlStateManager.scale(0.8F, 0.8F, 0.8F);
                 }
-                GlStateManager.enableDepth();
-                RenderHelper.enableGUIStandardItemLighting();
-                itemRender.renderItemAndEffectIntoGUI(stack, 0, 0);
-                RenderHelper.disableStandardItemLighting();
-                GlStateManager.disableDepth();
+                renderItem(stack, 0, 0);
                 GlStateManager.popMatrix();
             }
         }
 
+        int frequency = 0;
         // Get the name from the stack and render it
         if (currentLayer - 1 >= 0) {
-            ItemStack nameStack = new ItemStack(blockList.get(currentLayer - 1).getRight(), 1, blockList.get(currentLayer - 1).getLeft());
+            Pair<Integer, Block> integerBlockPair = blockList.get(currentLayer - 1);
+            ItemStack nameStack = new ItemStack(integerBlockPair.getRight(), 1, integerBlockPair.getLeft());
             String renderString = nameStack.getDisplayName();
 
             String capitalised = renderString.substring(0, 1).toUpperCase() + renderString.substring(1);
@@ -139,15 +139,10 @@ public class GuiSeismicReader extends GuiScreen {
                 fontRenderer.drawString(capitalised, renderX + 4, renderY + 4, 0x919191);
                 GlStateManager.popMatrix();
             }
-        }
 
-        int frequency = 0;
-
-        for (Pair<Integer, Block> pair : blockList) {
-            if (blockList.get(currentLayer - 1) != null) {
-                Block block = blockList.get(currentLayer - 1).getRight();
-
-                if (pair.getRight() == block && Objects.equals(pair.getLeft(), blockList.get(currentLayer - 1).getLeft())) {
+            for (Pair<Integer, Block> pair : blockList) {
+                Block block = integerBlockPair.getRight();
+                if (pair.getRight() == block && Objects.equals(pair.getLeft(), integerBlockPair.getLeft())) {
                     frequency++;
                 }
             }
@@ -158,6 +153,7 @@ public class GuiSeismicReader extends GuiScreen {
         GlStateManager.scale(0.7F, 0.7F, 0.7F);
         fontRenderer.drawString(LangUtils.localize("gui.abundancy") + ": " + frequency, 0, 0, 0x919191);
         GlStateManager.popMatrix();
+        MekanismRenderer.resetColor();
         super.drawScreen(mouseX, mouseY, partialTick);
     }
 
@@ -177,23 +173,37 @@ public class GuiSeismicReader extends GuiScreen {
     }
 
     @Override
-    protected void mouseClicked(int xPos, int yPos, int buttonClicked) throws IOException {
-        super.mouseClicked(xPos, yPos, buttonClicked);
-        if (upButton.intersects(new Rectangle(xPos, yPos, 1, 1))) {
-            if (currentLayer + 1 <= blockList.size() - 1) {
-                currentLayer++;
-            }
-        }
-
-        if (downButton.intersects(new Rectangle(xPos, yPos, 1, 1))) {
-            if (currentLayer - 1 >= 1) {
-                currentLayer--;
-            }
+    protected void actionPerformed(GuiButton guibutton) throws IOException {
+        super.actionPerformed(guibutton);
+        if (guibutton.id == upButton.id) {
+            currentLayer++;
+        } else if (guibutton.id == downButton.id) {
+            currentLayer--;
         }
     }
 
     @Override
     public boolean doesGuiPauseGame() {
         return false;
+    }
+
+    protected void renderItem(@Nonnull ItemStack stack, int xAxis, int yAxis) {
+        if (!stack.isEmpty()) {
+            try {
+                GlStateManager.pushMatrix();
+                GlStateManager.enableDepth();
+                RenderHelper.enableGUIStandardItemLighting();
+                itemRender.renderItemAndEffectIntoGUI(stack, xAxis, yAxis);
+                RenderHelper.disableStandardItemLighting();
+                GlStateManager.disableDepth();
+                GlStateManager.popMatrix();
+            } catch (Exception e) {
+                Mekanism.logger.error("Failed to render stack into gui: " + stack, e);
+            }
+        }
+    }
+
+    protected ResourceLocation getGuiLocation() {
+        return MekanismUtils.getResource(ResourceType.GUI, "GuiSeismicReader.png");
     }
 }
