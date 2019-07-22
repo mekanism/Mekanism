a06f2f07f468d206f9c6c3d129ba1dbc2af075ee
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2019-07-22 18:45:44 +0200

    Implement #5253(Extended vein Mining, and make it possible to disable various Atomic Disassembler Modes (#5544)
    * Implement Extended vein mine key and…
    
    …Coal Gasification.
    
    * fix one wrong lang key
    
    was a copy past proble...
    
    * Undo Coal Gasification in the Main Class
    
    * Undo Coal Gasification in the OreDictHandler
    
    * Remove a unused Constructor and add Vein Mining Config Options
    
    * Add a Config Option to enable Extended Vein Mining
    
    Sorry wasn't seeing that part of your comment.
    
    * Fix the only thing i found that looks like a merge problem.
    
    * Remove the not needed Creative mode test
    
    * Fix a couple typos in the config options
    
    * Make extended vein mining be its own mode instead of a key bind and allow for disabling various modes via the config
    
    * Fix minor spacing dif

--- src/main/java/mekanism/common/config/GeneralConfig.java
+++ src/main/java/mekanism/common/config/GeneralConfig.java
@@ -114,6 +114,24 @@ public class GeneralConfig extends BaseConfig {
 
     public final IntOption disassemblerEnergyUsageWeapon = new IntOption(this, "general", "DisassemblerEnergyUsageWeapon", 2000,
           "Cost in Joules of using the Atomic Disassembler as a weapon.");
+  
+    public final IntOption disassemblerMiningRange = new IntOption(this, "general", "DisassemblerMiningRange", 10,
+            "The Range of the Atomic Disassembler Extended Vein Mining.");
+
+    public final IntOption disassemblerMiningCount = new IntOption(this, "general", "DisassemblerMiningCount", 128,
+            "The max Atomic Disassembler Vein Mining Block Count.");
+
+    public final BooleanOption disassemblerSlowMode = new BooleanOption(this, "general", "DisassemblerSlowMode", true,
+          "Enable the 'Slow' mode for the Atomic Disassembler.");
+
+    public final BooleanOption disassemblerFastMode = new BooleanOption(this, "general", "DisassemblerFastMode", true,
+          "Enable the 'Fast' mode for the Atomic Disassembler.");
+
+    public final BooleanOption disassemblerVeinMining = new BooleanOption(this, "general", "DisassemblerVeinMiningMode", true,
+          "Enable the 'Vein Mining' mode for the Atomic Disassembler.");
+
+    public final BooleanOption disassemblerExtendedMining = new BooleanOption(this, "general", "DisassemblerExtendedMiningMode", true,
+            "Enable the 'Extended Vein Mining' mode for the Atomic Disassembler. (Allows vein mining everything not just ores/logs)");
 
     public final IntOption disassemblerDamageMin = new IntOption(this, "general", "DisassemblerDamageMin", 4,
           "The amount of damage the Atomic Disassembler does when it is out of power. (Value is in number of half hearts)");
