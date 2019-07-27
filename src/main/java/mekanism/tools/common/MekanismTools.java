0df3e376e87245aa9f41a8713d682751744f299c
pupnewfster <richard@freimer.com>
2019-07-27 12:03:23 -0400

    Switch back to if statements for setting entity equipment

--- src/main/java/mekanism/tools/common/MekanismTools.java
+++ src/main/java/mekanism/tools/common/MekanismTools.java
@@ -97,18 +97,28 @@ public class MekanismTools implements IModule {
         GameRegistry.addSmelting(toolsItem.getItemStackAnyDamage(), nugget, 0.1F);
     }
 
-    private void setStackIfCaseAndEmpty(EntityLivingBase entity, EntityEquipmentSlot slot, ItemStack item, boolean shouldSet) {
-        if (shouldSet && entity.getItemStackFromSlot(slot).isEmpty()) {
+    private void setStackIfEmpty(EntityLivingBase entity, EntityEquipmentSlot slot, ItemStack item) {
+        if (entity.getItemStackFromSlot(slot).isEmpty()) {
             entity.setItemStackToSlot(slot, item);
         }
     }
 
     private void setEntityArmorWithChance(Random random, EntityLivingBase entity, ToolsItem sword, ToolsItem helmet, ToolsItem chestplate, ToolsItem leggings, ToolsItem boots) {
-        setStackIfCaseAndEmpty(entity, EntityEquipmentSlot.MAINHAND, sword.getItemStack(), entity instanceof EntityZombie && random.nextInt(100) < 50);
-        setStackIfCaseAndEmpty(entity, EntityEquipmentSlot.HEAD, helmet.getItemStack(), random.nextInt(100) < 50);
-        setStackIfCaseAndEmpty(entity, EntityEquipmentSlot.CHEST, chestplate.getItemStack(), random.nextInt(100) < 50);
-        setStackIfCaseAndEmpty(entity, EntityEquipmentSlot.LEGS, leggings.getItemStack(), random.nextInt(100) < 50);
-        setStackIfCaseAndEmpty(entity, EntityEquipmentSlot.FEET, boots.getItemStack(), random.nextInt(100) < 50);
+        if (entity instanceof EntityZombie && random.nextInt(100) < 50) {
+            setStackIfEmpty(entity, EntityEquipmentSlot.MAINHAND, sword.getItemStack());
+        }
+        if (random.nextInt(100) < 50) {
+            setStackIfEmpty(entity, EntityEquipmentSlot.HEAD, helmet.getItemStack());
+        }
+        if (random.nextInt(100) < 50) {
+            setStackIfEmpty(entity, EntityEquipmentSlot.CHEST, chestplate.getItemStack());
+        }
+        if (random.nextInt(100) < 50) {
+            setStackIfEmpty(entity, EntityEquipmentSlot.LEGS, leggings.getItemStack());
+        }
+        if (random.nextInt(100) < 50) {
+            setStackIfEmpty(entity, EntityEquipmentSlot.FEET, boots.getItemStack());
+        }
     }
 
     @SubscribeEvent
