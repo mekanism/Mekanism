6b6ffa8897229f07b0633fd46045722333da32e4
Wvisee <william.visee@student.uclouvain.be>
2019-11-12 16:46:48 +0100

    Flamethrower can now make ObsidianTNT explode

--- src/main/java/mekanism/common/block/BlockObsidianTNT.java
+++ src/main/java/mekanism/common/block/BlockObsidianTNT.java
@@ -16,6 +16,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockObsidianTNT extends Block {
