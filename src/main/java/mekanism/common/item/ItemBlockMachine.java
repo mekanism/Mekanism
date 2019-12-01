378592dd0520ccd79d26b22e886c7f1cdca8f8dc
Thiakil <xander@thiakil.com>
2019-12-01 13:54:54 +0800

    tidy the item metas in the javadoc

--- src/main/java/mekanism/common/item/ItemBlockMachine.java
+++ src/main/java/mekanism/common/item/ItemBlockMachine.java
@@ -89,12 +89,48 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
- * Item class for handling multiple machine block IDs. 0:0: Enrichment Chamber 0:1: Osmium Compressor 0:2: Combiner 0:3: Crusher 0:4: Digital Miner 0:5: Basic Factory
- * 0:6: Advanced Factory 0:7: Elite Factory 0:8: Metallurgic Infuser 0:9: Purification Chamber 0:10: Energized Smelter 0:11: Teleporter 0:12: Electric Pump 0:13: Personal
- * Chest 0:14: Chargepad 0:15: Logistical Sorter 1:0: Rotary Condensentrator 1:1: Chemical Oxidizer 1:2: Chemical Infuser 1:3: Chemical Injection Chamber 1:4:
- * Electrolytic Separator 1:5: Precision Sawmill 1:6: Chemical Dissolution Chamber 1:7: Chemical Washer 1:8: Chemical Crystallizer 1:9: Seismic Vibrator 1:10: Pressurized
- * Reaction Chamber 1:11: Portable Tank 1:12: Fluidic Plenisher 1:13: Laser 1:14: Laser Amplifier 1:15: Laser Tractor Beam 2:0: Entangled Block 2:1: Solar Neutron
- * Activator 2:2: Ambient Accumulator 2:3: Oredictionificator 2:4: Resistive Heater 2:5: Formulaic Assemblicator 2:6: Fuelwood Heater
+ * Item class for handling multiple machine block IDs.
+ * 0:0: Enrichment Chamber
+ * 0:1: Osmium Compressor
+ * 0:2: Combiner
+ * 0:3: Crusher
+ * 0:4: Digital Miner
+ * 0:5: Basic Factory
+ * 0:6: Advanced Factory
+ * 0:7: Elite Factory
+ * 0:8: Metallurgic Infuser
+ * 0:9: Purification Chamber
+ * 0:10: Energized Smelter
+ * 0:11: Teleporter
+ * 0:12: Electric Pump
+ * 0:13: Personal Chest
+ * 0:14: Chargepad
+ * 0:15: Logistical Sorter
+ *
+ * 1:0: Rotary Condensentrator
+ * 1:1: Chemical Oxidizer
+ * 1:2: Chemical Infuser
+ * 1:3: Chemical Injection Chamber
+ * 1:4: Electrolytic Separator
+ * 1:5: Precision Sawmill
+ * 1:6: Chemical Dissolution Chamber
+ * 1:7: Chemical Washer
+ * 1:8: Chemical Crystallizer
+ * 1:9: Seismic Vibrator
+ * 1:10: Pressurized Reaction Chamber
+ * 1:11: Portable Tank
+ * 1:12: Fluidic Plenisher
+ * 1:13: Laser
+ * 1:14: Laser Amplifier
+ * 1:15: Laser Tractor Beam
+ *
+ * 2:0: Entangled Block
+ * 2:1: Solar Neutron Activator
+ * 2:2: Ambient Accumulator
+ * 2:3: Oredictionificator
+ * 2:4: Resistive Heater
+ * 2:5: Formulaic Assemblicator
+ * 2:6: Fuelwood Heater
  *
  * @author AidanBrady
  */
