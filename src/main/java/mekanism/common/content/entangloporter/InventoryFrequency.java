68cd490f56dbcfe112cc134504afb45717dfab93
Aidan Brady <me@aidancbrady.com>
2020-02-11 23:36:20 -0500

    Fix quantum entangloporter item transport

--- src/main/java/mekanism/common/content/entangloporter/InventoryFrequency.java
+++ src/main/java/mekanism/common/content/entangloporter/InventoryFrequency.java
@@ -6,20 +6,26 @@ import java.util.UUID;
 import java.util.function.Supplier;
 import mekanism.api.TileNetworkList;
 import mekanism.api.gas.GasTank;
+import mekanism.api.inventory.IMekanismInventory;
 import mekanism.api.inventory.slot.IInventorySlot;
 import mekanism.common.PacketHandler;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.frequency.Frequency;
+import mekanism.common.inventory.slot.BasicInventorySlot;
 import mekanism.common.inventory.slot.InternalInventorySlot;
 import mekanism.common.tier.FluidTankTier;
 import mekanism.common.tier.GasTankTier;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.Direction;
 import net.minecraftforge.common.util.Constants.NBT;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
 
-public class InventoryFrequency extends Frequency {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class InventoryFrequency extends Frequency implements IMekanismInventory {
 
     public static final String ENTANGLOPORTER = "Entangloporter";
     private static final Supplier<FluidTank> FLUID_TANK_SUPPLIER = () -> new FluidTank(MekanismConfig.general.quantumEntangloporterFluidBuffer.get());
@@ -29,17 +35,15 @@ public class InventoryFrequency extends Frequency {
     public FluidTank storedFluid;
     public GasTank storedGas;
     public double temperature;
+    public IInventorySlot storedItem;
 
-    //TODO: input: configComponent.getOutput(TransmissionType.ITEM, side, getDirection()).ioState == IOState.INPUT
-    //TODO: output: configComponent.getOutput(TransmissionType.ITEM, side, getDirection()).ioState == IOState.OUTPUT
-    //TODO: FIXME?? ideally we don't pass null as the inventory??
-    //TODO: FIX INVENTORY PERSISTENCE
-    public final List<IInventorySlot> inventorySlots = Collections.singletonList(InternalInventorySlot.create(null));
+    public List<IInventorySlot> inventorySlots = Collections.singletonList(storedItem);
 
     public InventoryFrequency(String n, UUID uuid) {
         super(n, uuid);
         storedFluid = FLUID_TANK_SUPPLIER.get();
         storedGas = GAS_TANK_SUPPLIER.get();
+        storedItem = BasicInventorySlot.at(this, 0, 0);
     }
 
     public InventoryFrequency(CompoundNBT nbtTags) {
@@ -60,15 +64,7 @@ public class InventoryFrequency extends Frequency {
         if (!storedGas.isEmpty()) {
             nbtTags.put("storedGas", storedGas.write(new CompoundNBT()));
         }
-        ListNBT tagList = new ListNBT();
-        for (int slotCount = 0; slotCount < inventorySlots.size(); slotCount++) {
-            CompoundNBT tagCompound = inventorySlots.get(slotCount).serializeNBT();
-            if (!tagCompound.isEmpty()) {
-                tagCompound.putByte("Slot", (byte) slotCount);
-                tagList.add(tagCompound);
-            }
-        }
-        nbtTags.put("Items", tagList);
+        nbtTags.put("storedItem", storedItem.serializeNBT());
         nbtTags.putDouble("temperature", temperature);
     }
 
@@ -78,6 +74,7 @@ public class InventoryFrequency extends Frequency {
         storedFluid = FLUID_TANK_SUPPLIER.get();
         storedGas = GAS_TANK_SUPPLIER.get();
         storedEnergy = nbtTags.getDouble("storedEnergy");
+        storedItem = BasicInventorySlot.at(this, 0, 0);
 
         if (nbtTags.contains("storedFluid")) {
             storedFluid.readFromNBT(nbtTags.getCompound("storedFluid"));
@@ -87,14 +84,7 @@ public class InventoryFrequency extends Frequency {
             storedGas.setCapacity(MekanismConfig.general.quantumEntangloporterGasBuffer.get());
         }
 
-        ListNBT tagList = nbtTags.getList("Items", NBT.TAG_COMPOUND);
-        for (int tagCount = 0; tagCount < tagList.size(); tagCount++) {
-            CompoundNBT tagCompound = tagList.getCompound(tagCount);
-            byte slotID = tagCompound.getByte("Slot");
-            if (slotID >= 0 && slotID < inventorySlots.size()) {
-                inventorySlots.get(slotID).deserializeNBT(tagCompound);
-            }
-        }
+        storedItem.deserializeNBT(nbtTags.getCompound("storedItem"));
         temperature = nbtTags.getDouble("temperature");
     }
 
@@ -104,17 +94,30 @@ public class InventoryFrequency extends Frequency {
         data.add(storedEnergy);
         data.add(storedFluid.getFluid());
         data.add(storedGas.getStack());
+        data.add(storedItem.serializeNBT());
         data.add(temperature);
     }
 
     @Override
     protected void read(PacketBuffer dataStream) {
         super.read(dataStream);
+        storedItem = BasicInventorySlot.at(this, 0, 0);
         storedFluid = new FluidTank(FluidTankTier.ULTIMATE.getOutput());
         storedGas = new GasTank(GasTankTier.ULTIMATE.getOutput());
         storedEnergy = dataStream.readDouble();
         storedFluid.setFluid(dataStream.readFluidStack());
         storedGas.setStack(PacketHandler.readGasStack(dataStream));
+        storedItem.deserializeNBT(dataStream.readCompoundTag());
         temperature = dataStream.readDouble();
+    }
+
+    @Override
+    @Nonnull
+    public List<IInventorySlot> getInventorySlots(@Nullable Direction side) {
+        return inventorySlots;
+    }
+
+    @Override
+    public void onContentsChanged() {
     }
 }
