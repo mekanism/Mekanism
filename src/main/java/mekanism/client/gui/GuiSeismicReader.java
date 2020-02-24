1d10417a0f4cb28979a3dc7b072f6766fb3761cc
pupnewfster <richard@freimer.com>
2020-02-23 20:01:57 -0500

    Add a scroll bar to the seismic reader

--- src/main/java/mekanism/client/gui/GuiSeismicReader.java
+++ src/main/java/mekanism/client/gui/GuiSeismicReader.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import mekanism.client.gui.element.GuiArrowSelection;
 import mekanism.client.gui.element.GuiInnerScreen;
+import mekanism.client.gui.element.GuiScrollBar;
 import mekanism.client.gui.element.button.MekanismButton;
 import mekanism.client.gui.element.button.MekanismImageButton;
 import mekanism.client.render.MekanismRenderer;
@@ -16,7 +17,6 @@ import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.MekanismUtils.ResourceType;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
@@ -28,47 +28,61 @@ public class GuiSeismicReader extends GuiMekanism<SeismicReaderContainer> {
     private Object2IntMap<Block> frequencies = new Object2IntOpenHashMap<>();
     private MekanismButton upButton;
     private MekanismButton downButton;
-
-    private int currentLayer;
+    private double scroll;
 
     public GuiSeismicReader(SeismicReaderContainer container, PlayerInventory inv, ITextComponent title) {
         super(container, inv, title);
-        xSize = 137;
+        xSize = 147;
         ySize = 182;
-        PlayerEntity player = inv.player;
-        BlockPos pos = player.getPosition();
+        BlockPos pos = inv.player.getPosition();
         //Calculate all the blocks in the column
         for (BlockPos p : BlockPos.getAllInBoxMutable(new BlockPos(pos.getX(), 0, pos.getZ()), pos)) {
-            blockList.add(player.world.getBlockState(p));
+            blockList.add(inv.player.world.getBlockState(p));
         }
-        currentLayer = Math.max(0, blockList.size() - 1);
-        //TODO: Add scroll bar element
     }
 
     @Override
     public void init() {
         super.init();
-        addButton(new GuiInnerScreen(this, 12, 11, 51, 159));
-        addButton(new GuiInnerScreen(this, 67, 11, 63, 49));
-        addButton(new GuiArrowSelection(this, 14, 81, () -> currentLayer - 1 >= 0 ? blockList.get(currentLayer - 1).getBlock().getNameTextComponent() : null));
-        addButton(upButton = new MekanismImageButton(this, getGuiLeft() + 70, getGuiTop() + 75, 13,
-              MekanismUtils.getResource(ResourceType.GUI_BUTTON, "up.png"), () -> {
-            currentLayer++;
-            upButton.active = currentLayer + 1 <= blockList.size();
+        addButton(new GuiInnerScreen(this, 7, 11, 63, 49));
+        addButton(new GuiInnerScreen(this, 74, 11, 51, 159));
+        addButton(new GuiArrowSelection(this, 76, 81, () -> {
+            int currentLayer = getCurrentLayer();
+            if (currentLayer - 1 >= 0) {
+                return blockList.get(currentLayer - 1).getBlock().getNameTextComponent();
+            }
+            return null;
         }));
-        addButton(downButton = new MekanismImageButton(this, getGuiLeft() + 70, getGuiTop() + 92, 13,
-              MekanismUtils.getResource(ResourceType.GUI_BUTTON, "down.png"), () -> {
-            currentLayer--;
-            downButton.active = currentLayer - 1 >= 1;
+        //Scroll bar
+        addButton(new GuiScrollBar(this, 126, 25, 131, this::needsScrollBars, () -> scroll, value -> {
+            scroll = value;
+            updateButtons();
         }));
-        upButton.active = currentLayer + 1 <= blockList.size();
-        downButton.active = currentLayer - 1 >= 1;
+        addButton(upButton = new MekanismImageButton(this, getGuiLeft() + 126, getGuiTop() + 11, 14,
+              MekanismUtils.getResource(ResourceType.GUI_BUTTON, "up.png"), () -> adjustScroll(1)));
+        addButton(downButton = new MekanismImageButton(this, getGuiLeft() + 126, getGuiTop() + 156, 14,
+              MekanismUtils.getResource(ResourceType.GUI_BUTTON, "down.png"), () -> adjustScroll(-1)));
+        updateButtons();
+    }
+
+    private void updateButtons() {
+        upButton.active = scroll > 0;
+        downButton.active = scroll < 1;
+    }
+
+    private int getCurrentLayer() {
+        if (needsScrollBars()) {
+            int size = blockList.size();
+            return size - (int) ((size - 0.5) * scroll);
+        }
+        return 1;
     }
 
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        int currentLayer = getCurrentLayer();
         //Render the layer text scaled, so that it does not start overlapping past 100
-        renderScaledText(MekanismLang.GENERIC.translate(currentLayer), 49, 87, 0x00CD00, 13);
+        renderScaledText(MekanismLang.GENERIC.translate(currentLayer), 111, 87, 0x00CD00, 13);
 
         //TODO: Eventually instead of just rendering the item stacks, it would be nice to be able to render the actual vertical column of blocks
         //Render the item stacks
@@ -77,7 +91,7 @@ public class GuiSeismicReader extends GuiMekanism<SeismicReaderContainer> {
             if (0 <= layer && layer < blockList.size()) {
                 BlockState state = blockList.get(layer);
                 ItemStack stack = new ItemStack(state.getBlock());
-                int renderX = 30;
+                int renderX = 92;
                 int renderY = 147 - 16 * i;
                 if (i == 4) {
                     renderItem(stack, renderX, renderY);
@@ -100,10 +114,10 @@ public class GuiSeismicReader extends GuiMekanism<SeismicReaderContainer> {
         if (currentLayer - 1 >= 0) {
             Block block = blockList.get(currentLayer - 1).getBlock();
             ITextComponent displayName = block.getNameTextComponent();
-            renderScaledText(displayName, 70, 16, 0x00CD00, 57);
+            renderScaledText(displayName, 10, 16, 0x00CD00, 57);
             frequency = frequencies.computeIntIfAbsent(block, b -> (int) blockList.stream().filter(blockState -> b == blockState.getBlock()).count());
         }
-        renderScaledText(MekanismLang.ABUNDANCY.translate(frequency), 70, 26, 0x00CD00, 57);
+        renderScaledText(MekanismLang.ABUNDANCY.translate(frequency), 10, 26, 0x00CD00, 57);
         MekanismRenderer.resetColor();
         super.drawGuiContainerForegroundLayer(mouseX, mouseY);
     }
@@ -112,5 +126,34 @@ public class GuiSeismicReader extends GuiMekanism<SeismicReaderContainer> {
     public void onClose() {
         super.onClose();
         blockList.clear();
+    }
+
+    private boolean needsScrollBars() {
+        return blockList.size() > 1;
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+        if (delta != 0 && needsScrollBars()) {
+            adjustScroll(delta);
+            return true;
+        }
+        return super.mouseScrolled(mouseX, mouseY, delta);
+    }
+
+    private void adjustScroll(double delta) {
+        int j = blockList.size() - 1;
+        if (delta > 0) {
+            delta = 1;
+        } else {
+            delta = -1;
+        }
+        scroll = (float) (scroll - delta / j);
+        if (scroll < 0.0F) {
+            scroll = 0.0F;
+        } else if (scroll > 1.0F) {
+            scroll = 1.0F;
+        }
+        updateButtons();
     }
 }
