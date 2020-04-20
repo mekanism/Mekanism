08dec25e068ecfb6288558808f3be05183423fc0
Aidan Brady <me@aidancbrady.com>
2020-04-20 14:58:01 -0400

    Polished Lightning Bolt renderer, confirmed working both in-world and in screens Could still improve 3D geometry of bolts, but seems to work fine with existing implementation

--- src/main/java/mekanism/client/render/BoltRenderer.java
+++ src/main/java/mekanism/client/render/BoltRenderer.java
@@ -10,33 +10,62 @@ import net.minecraft.util.math.Vec3d;
 
 public class BoltRenderer {
 
-    // max distance perpendicular to straight line
-    private static float maxDiffScale = 0.3F;
-    // max distance perpendicular to straight line in single step
+    private Random random = new Random();
 
-    public static void render(Vec3d start, Vec3d end, int count, int segments, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
-        Random random = new Random();
+    private float parallelNoise;
+    private float size;
+    private float spreadFactor;
+
+    private float red = 0.45F, green = 0.45F, blue = 0.5F, alpha = 0.8F;
+
+    private SpreadFunction spreadFunction;
+    private RandomFunction randomFunction;
+
+    public BoltRenderer() {
+        this(SpreadFunction.SINE, RandomFunction.GAUSSIAN);
+    }
+
+    public BoltRenderer(SpreadFunction spreadFunction, RandomFunction randomFunction) {
+        this(spreadFunction, randomFunction, 0.1F, 0.1F, 0.1F);
+    }
+
+    public BoltRenderer(SpreadFunction spreadFunction, RandomFunction randomFunction, float parallelNoise, float size, float spreadFactor) {
+        this.spreadFunction = spreadFunction;
+        this.randomFunction = randomFunction;
+        this.parallelNoise = parallelNoise;
+        this.size = size;
+        this.spreadFactor = spreadFactor;
+    }
+
+    public BoltRenderer withColor(float red, float green, float blue, float alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+        return this;
+    }
+
+    public void render(Vec3d start, Vec3d end, int count, int segments, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
         IVertexBuilder buffer = bufferIn.getBuffer(RenderType.getLightning());
         Matrix4f matrix = matrixStackIn.getLast().getMatrix();
         Vec3d diff = end.subtract(start);
-        float distance = (float) diff.length();
+        float totalDistance = (float) diff.length();
         for (int i = 0; i < count; i++) {
             Vec3d segmentStart = start;
+            float progress = 0;
             for (int segment = 0; segment < segments; segment++) {
-                // last segment
+                progress = progress + (1F / segments) * (1 - parallelNoise + random.nextFloat() * parallelNoise * 2);
                 Vec3d segmentEnd = null;
-                if (segment == segments - 1) {
+                if (progress >= 1) {
                     segmentEnd = end;
                 } else {
-                    // represents how far we can be (perpendicular from the line) at this end segment
-                    float segmentDiffScale = (segment + 1 - Math.max(0, 2 * segment + 1 - (segments - 1))) / (segments - 1 / 2F);
-                    float maxDiff = maxDiffScale * segmentDiffScale * distance * random.nextFloat();
+                    float segmentDiffScale = spreadFunction.getMaxSpread(progress);
+                    float maxDiff = spreadFactor * segmentDiffScale * totalDistance * randomFunction.getRandom(random);
                     Vec3d cur = findRandomOrthogonalVector(diff, random).scale(maxDiff);
                     // new vector is original + current progress through segments + perpendicular change
-                    segmentEnd = start.add(diff.scale((segment + 1) / (float) segments)).add(cur);
+                    segmentEnd = start.add(diff.scale(progress)).add(cur);
                 }
-                //System.out.println(segmentStart + " " + segmentEnd);
-                renderBolt(matrix, buffer, segmentStart, segmentEnd, 0.45F, 0.45F, 0.5F);
+                renderBolt(matrix, buffer, segmentStart, segmentEnd);
                 segmentStart = segmentEnd;
             }
         }
@@ -47,20 +76,51 @@ public class BoltRenderer {
         return vec.crossProduct(newVec).normalize();
     }
 
-    private static void renderBolt(Matrix4f matrix, IVertexBuilder buffer, Vec3d start, Vec3d end, float r, float g, float b) {
-        // start bottom
+    private void renderBolt(Matrix4f matrix, IVertexBuilder buffer, Vec3d start, Vec3d end) {
         buffer.pos(matrix, (float) start.x, (float) start.y, (float) start.z)
-            .color(r, g, b, 0.3F).endVertex();
-        buffer.pos(matrix, (float) start.x+0.1F, (float) start.y, (float) start.z)
-            .color(r, g, b, 0.3F).endVertex();
-        buffer.pos(matrix, (float) start.x+0.05F, (float) start.y, (float) start.z+0.1F)
-            .color(r, g, b, 0.3F).endVertex();
-
+            .color(red, green, blue, alpha).endVertex();
         buffer.pos(matrix, (float) end.x, (float) end.y, (float) end.z)
-            .color(r, g, b, 0.3F).endVertex();
-        buffer.pos(matrix, (float) end.x+0.1F, (float) end.y, (float) end.z)
-            .color(r, g, b, 0.3F).endVertex();
-        buffer.pos(matrix, (float) end.x+0.05F, (float) end.y, (float) end.z+0.1F)
-            .color(r, g, b, 0.3F).endVertex();
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) end.x + size, (float) end.y, (float) end.z)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) start.x + size, (float) start.y, (float) start.z)
+            .color(red, green, blue, alpha).endVertex();
+
+        buffer.pos(matrix, (float) start.x + size, (float) start.y, (float) start.z)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) end.x + size, (float) end.y, (float) end.z)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) end.x + size/2, (float) end.y, (float) end.z + size)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) start.x + size/2, (float) start.y, (float) start.z + size)
+            .color(red, green, blue, alpha).endVertex();
+
+        buffer.pos(matrix, (float) start.x + size/2, (float) start.y, (float) start.z + size)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) end.x + size/2, (float) end.y, (float) end.z + size)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) end.x, (float) end.y, (float) end.z)
+            .color(red, green, blue, alpha).endVertex();
+        buffer.pos(matrix, (float) start.x, (float) start.y, (float) start.z)
+            .color(red, green, blue, alpha).endVertex();
+    }
+
+    public interface SpreadFunction {
+        /** A steady linear increase in perpendicular noise. */
+        public static final SpreadFunction LINEAR_ASCENT = (progress) -> progress;
+        /** A steady linear increase in perpendicular noise, followed by a steady decrease after the halfway point. */
+        public static final SpreadFunction LINEAR_ASCENT_DESCENT = (progress) -> (progress - Math.max(0, 2 * progress - 1)) / (0.5F);
+        /** Represents a unit sine wave from 0 to PI, scaled by progress. */
+        public static final SpreadFunction SINE = (progress) -> (float) Math.sin(Math.PI * progress);
+
+        float getMaxSpread(float progress);
+    }
+
+    public interface RandomFunction {
+
+        public static final RandomFunction UNIFORM = (rand) -> rand.nextFloat();
+        public static final RandomFunction GAUSSIAN = (rand) -> (float) rand.nextGaussian();
+
+        float getRandom(Random rand);
     }
 }
--- src/main/java/mekanism/client/render/tileentity/RenderIndustrialAlarm.java
+++ src/main/java/mekanism/client/render/tileentity/RenderIndustrialAlarm.java
@@ -2,9 +2,7 @@ package mekanism.client.render.tileentity;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import mekanism.api.Pos3D;
 import mekanism.client.model.ModelIndustrialAlarm;
-import mekanism.client.render.BoltRenderer;
 import mekanism.common.base.ProfilerConstants;
 import mekanism.common.block.attribute.Attribute;
 import mekanism.common.tile.TileEntityIndustrialAlarm;
@@ -28,7 +26,6 @@ public class RenderIndustrialAlarm extends MekanismTileEntityRenderer<TileEntity
         performTranslations(tile, matrix);
         float rotation = (tile.getWorld().getDayTime() + partialTick) * ROTATE_SPEED % 360;
         model.render(matrix, renderer, light, overlayLight, Attribute.isActive(tile.getBlockState()), rotation, false);
-        BoltRenderer.render(new Pos3D(0, 0, 0), new Pos3D(10, 10, 10), 5, 15, partialTick, matrix, renderer, light);
         matrix.pop();
     }
 
