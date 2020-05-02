a1772c4cbc7a0cc062ab2f5bbaacf01821b27ccd
Aidan Brady <me@aidancbrady.com>
2020-05-02 15:31:47 -0400

    Fix ore generation config bounds (#6125), adjust turbine formation logic (#6121), fix energy conversion config naming and Gas Generator generation rate miscalculation (#6126)

--- src/main/java/mekanism/common/config/WorldConfig.java
+++ src/main/java/mekanism/common/config/WorldConfig.java
@@ -68,9 +68,9 @@ public class WorldConfig extends BaseMekanismConfig {
             //The max for perChunk and vein size are the values of the max number of blocks in a chunk.
             //TODO: Improve upon it at some point so that the max vein size then gets determined by per chunk as well
             this.perChunk = CachedIntValue.wrap(config, builder.comment("Chance that " + ore + " generates in a chunk.")
-                  .defineInRange("perChunk", perChunk, 1, 65536));
+                  .defineInRange("perChunk", perChunk, 1, 512));
             this.maxVeinSize = CachedIntValue.wrap(config, builder.comment("Maximum number of blocks in a vein of " + ore + ".")
-                  .defineInRange("maxVeinSize", maxVeinSize, 1, 65536));
+                  .defineInRange("maxVeinSize", maxVeinSize, 1, 512));
             //TODO: See if we can use world.getHeight() somehow
             this.maxHeight = CachedIntValue.wrap(config, builder.comment("Base maximum height (exclusive) that veins of " + ore + " can spawn. Height is calculated by: random.nextInt(maxHeight - topOffset) + bottomOffset")
                   .defineInRange("maxHeight", maxHeight, 1, 256));
