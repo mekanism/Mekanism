ea525ab351ef2d58b1c3f7576af42a0e7a9011d2
Aidan Brady <me@aidancbrady.com>
2020-06-12 11:32:28 -0400

    Enhancement to vein mining animation

--- src/main/java/mekanism/common/content/gear/mekatool/ModuleVeinMiningUnit.java
+++ src/main/java/mekanism/common/content/gear/mekatool/ModuleVeinMiningUnit.java
@@ -61,11 +61,12 @@ public class ModuleVeinMiningUnit extends ModuleMekaTool {
                 if (!found.contains(pos) && (maxRange == -1 || MekanismUtils.distanceBetween(location, pos) <= maxRange)) {
                     if (world.isBlockPresent(pos) && startBlock == world.getBlockState(pos).getBlock()) {
                         //Make sure to add it as immutable
-                        //not checking if we've already added found pos before adding
-                        openSet.add(pos.toImmutable());
-                        BoltEffect bolt = new BoltEffect(BoltRenderInfo.ELECTRICITY, new Vec3d(blockPos).add(0.5, 0.5, 0.5), new Vec3d(pos).add(0.5, 0.5, 0.5), 10)
-                            .size(0.04F).lifespan(20).spawn(SpawnFunction.NO_DELAY);
-                        Mekanism.proxy.renderBolt(Objects.hash(blockPos, pos), bolt);
+                        if (!openSet.contains(pos)) {
+                            openSet.add(pos.toImmutable());
+                            BoltEffect bolt = new BoltEffect(BoltRenderInfo.ELECTRICITY, new Vec3d(blockPos).add(0.5, 0.5, 0.5), new Vec3d(pos).add(0.5, 0.5, 0.5), 10)
+                                .size(0.015F).lifespan(12).spawn(SpawnFunction.NO_DELAY);
+                            Mekanism.proxy.renderBolt(Objects.hash(blockPos, pos), bolt);
+                        }
                     }
                 }
             }
