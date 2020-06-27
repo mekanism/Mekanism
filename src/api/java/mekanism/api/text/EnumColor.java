a0eff300c85a9afa2d7f849a93a52ca4120c1b7f
CyanideX <brandon_j_d@hotmail.com>
2020-06-27 15:40:53 -0700

    Tweaked Colors for Less Eye Bleeding

--- src/api/java/mekanism/api/text/EnumColor.java
+++ src/api/java/mekanism/api/text/EnumColor.java
@@ -19,26 +19,26 @@ import net.minecraftforge.common.Tags;
  * @author AidanBrady
  */
 public enum EnumColor implements IIncrementalEnum<EnumColor> {
-    BLACK("\u00a70", APILang.COLOR_BLACK, "Black", "Black", "black", new int[]{0, 0, 0}, TextFormatting.BLACK, DyeColor.BLACK),
-    DARK_BLUE("\u00a71", APILang.COLOR_DARK_BLUE, "Blue", "Blue", "blue", new int[]{0, 0, 170}, TextFormatting.DARK_BLUE, DyeColor.BLUE),
-    DARK_GREEN("\u00a72", APILang.COLOR_DARK_GREEN, "Green", "Green", "green", new int[]{0, 170, 0}, TextFormatting.DARK_GREEN, DyeColor.GREEN),
-    DARK_AQUA("\u00a73", APILang.COLOR_DARK_AQUA, "Cyan", "Cyan", "cyan", new int[]{0, 255, 255}, TextFormatting.DARK_AQUA, DyeColor.CYAN),
-    DARK_RED("\u00a74", APILang.COLOR_DARK_RED, "Dark Red", null, "dark_red", new int[]{170, 0, 0}, TextFormatting.DARK_RED,
+    BLACK("\u00a70", APILang.COLOR_BLACK, "Black", "Black", "black", new int[]{64, 64, 64}, TextFormatting.BLACK, DyeColor.BLACK),
+    DARK_BLUE("\u00a71", APILang.COLOR_DARK_BLUE, "Blue", "Blue", "blue", new int[]{54, 107, 208}, TextFormatting.DARK_BLUE, DyeColor.BLUE),
+    DARK_GREEN("\u00a72", APILang.COLOR_DARK_GREEN, "Green", "Green", "green", new int[]{89, 193, 95}, TextFormatting.DARK_GREEN, DyeColor.GREEN),
+    DARK_AQUA("\u00a73", APILang.COLOR_DARK_AQUA, "Cyan", "Cyan", "cyan", new int[]{0, 243, 208}, TextFormatting.DARK_AQUA, DyeColor.CYAN),
+    DARK_RED("\u00a74", APILang.COLOR_DARK_RED, "Dark Red", null, "dark_red", new int[]{201, 7, 31}, TextFormatting.DARK_RED,
           MaterialColor.NETHERRACK, Tags.Items.DYES_RED),
-    PURPLE("\u00a75", APILang.COLOR_PURPLE, "Purple", "Purple", "purple", new int[]{170, 0, 170}, TextFormatting.DARK_PURPLE, DyeColor.PURPLE),
-    ORANGE("\u00a76", APILang.COLOR_ORANGE, "Orange", "Orange", "orange", new int[]{255, 170, 0}, TextFormatting.GOLD, DyeColor.ORANGE),
-    GRAY("\u00a77", APILang.COLOR_GRAY, "Light Gray", "LightGray", "light_gray", new int[]{170, 170, 170}, TextFormatting.GRAY, DyeColor.LIGHT_GRAY),
-    DARK_GRAY("\u00a78", APILang.COLOR_DARK_GRAY, "Gray", "Gray", "gray", new int[]{85, 85, 85}, TextFormatting.DARK_GRAY, DyeColor.GRAY),
-    INDIGO("\u00a79", APILang.COLOR_INDIGO, "Light Blue", "LightBlue", "light_blue", new int[]{85, 85, 255}, TextFormatting.BLUE, DyeColor.LIGHT_BLUE),
-    BRIGHT_GREEN("\u00a7a", APILang.COLOR_BRIGHT_GREEN, "Lime", "Lime", "lime", new int[]{85, 255, 85}, TextFormatting.GREEN, DyeColor.LIME),
-    AQUA("\u00a7b", APILang.COLOR_AQUA, "Aqua", null, "aqua", new int[]{85, 255, 255}, TextFormatting.AQUA, MaterialColor.LIGHT_BLUE, Tags.Items.DYES_LIGHT_BLUE),
-    RED("\u00a7c", APILang.COLOR_RED, "Red", "Red", "red", new int[]{255, 0, 0}, TextFormatting.RED, DyeColor.RED),
-    PINK("\u00a7d", APILang.COLOR_PINK, "Magenta", "Magenta", "magenta", new int[]{255, 85, 255}, TextFormatting.LIGHT_PURPLE, DyeColor.MAGENTA),
-    YELLOW("\u00a7e", APILang.COLOR_YELLOW, "Yellow", "Yellow", "yellow", new int[]{255, 255, 85}, TextFormatting.YELLOW, DyeColor.YELLOW),
+    PURPLE("\u00a75", APILang.COLOR_PURPLE, "Purple", "Purple", "purple", new int[]{208, 105, 184}, TextFormatting.DARK_PURPLE, DyeColor.PURPLE),
+    ORANGE("\u00a76", APILang.COLOR_ORANGE, "Orange", "Orange", "orange", new int[]{255, 161, 96}, TextFormatting.GOLD, DyeColor.ORANGE),
+    GRAY("\u00a77", APILang.COLOR_GRAY, "Light Gray", "LightGray", "light_gray", new int[]{207, 207, 207}, TextFormatting.GRAY, DyeColor.LIGHT_GRAY),
+    DARK_GRAY("\u00a78", APILang.COLOR_DARK_GRAY, "Gray", "Gray", "gray", new int[]{122, 122, 122}, TextFormatting.DARK_GRAY, DyeColor.GRAY),
+    INDIGO("\u00a79", APILang.COLOR_INDIGO, "Light Blue", "LightBlue", "light_blue", new int[]{85, 158, 255}, TextFormatting.BLUE, DyeColor.LIGHT_BLUE),
+    BRIGHT_GREEN("\u00a7a", APILang.COLOR_BRIGHT_GREEN, "Lime", "Lime", "lime", new int[]{117, 255, 137}, TextFormatting.GREEN, DyeColor.LIME),
+    AQUA("\u00a7b", APILang.COLOR_AQUA, "Aqua", null, "aqua", new int[]{48, 255, 249}, TextFormatting.AQUA, MaterialColor.LIGHT_BLUE, Tags.Items.DYES_LIGHT_BLUE),
+    RED("\u00a7c", APILang.COLOR_RED, "Red", "Red", "red", new int[]{255, 56, 60}, TextFormatting.RED, DyeColor.RED),
+    PINK("\u00a7d", APILang.COLOR_PINK, "Magenta", "Magenta", "magenta", new int[]{255, 164, 249}, TextFormatting.LIGHT_PURPLE, DyeColor.MAGENTA),
+    YELLOW("\u00a7e", APILang.COLOR_YELLOW, "Yellow", "Yellow", "yellow", new int[]{255, 221, 79}, TextFormatting.YELLOW, DyeColor.YELLOW),
     WHITE("\u00a7f", APILang.COLOR_WHITE, "White", "White", "white", new int[]{255, 255, 255}, TextFormatting.WHITE, DyeColor.WHITE),
     //Extras for dye-completeness
-    BROWN("\u00a76", APILang.COLOR_BROWN, "Brown", "Brown", "brown", new int[]{150, 75, 0}, TextFormatting.GOLD, DyeColor.BROWN),
-    BRIGHT_PINK("\u00a7d", APILang.COLOR_BRIGHT_PINK, "Pink", "Pink", "pink", new int[]{255, 192, 203}, TextFormatting.LIGHT_PURPLE, DyeColor.PINK);
+    BROWN("\u00a76", APILang.COLOR_BROWN, "Brown", "Brown", "brown", new int[]{161, 118, 73}, TextFormatting.GOLD, DyeColor.BROWN),
+    BRIGHT_PINK("\u00a7d", APILang.COLOR_BRIGHT_PINK, "Pink", "Pink", "pink", new int[]{255, 188, 196}, TextFormatting.LIGHT_PURPLE, DyeColor.PINK);
 
     private static final EnumColor[] COLORS = values();
     /**
