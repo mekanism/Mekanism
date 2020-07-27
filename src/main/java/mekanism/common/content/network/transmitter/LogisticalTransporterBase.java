765ee5cf549a348eb2036b734e51589a691bb9ad
pupnewfster <richard@freimer.com>
2020-07-27 12:53:27 -0400

    Fix insertRR using the wrong side for checking canReceiveFrom (#6393)

--- src/main/java/mekanism/common/content/network/transmitter/LogisticalTransporterBase.java
+++ src/main/java/mekanism/common/content/network/transmitter/LogisticalTransporterBase.java
@@ -1,14 +1,14 @@
 package mekanism.common.content.network.transmitter;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.UUID;
 import java.util.function.IntConsumer;
 import javax.annotation.Nonnull;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
 import mekanism.api.NBTConstants;
 import mekanism.api.text.EnumColor;
 import mekanism.common.Mekanism;
@@ -381,7 +381,7 @@ public abstract class LogisticalTransporterBase extends Transmitter<IItemHandler
         BlockPos outputterPos = outputter.getPos();
         Direction from = MekanismUtils.sideDifference(getTilePos(), outputterPos);
         TransporterStack stack = insertStack(outputterPos, color);
-        if (!canReceiveFrom(from) || !stack.canInsertToTransporterNN(this, from, outputter)) {
+        if (!canReceiveFrom(from.getOpposite()) || !stack.canInsertToTransporterNN(this, from, outputter)) {
             return request.getEmptyResponse();
         }
         return updateTransit(doEmit, stack, stack.recalculateRRPath(request, outputter, this, min));
