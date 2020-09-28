1a54bbeac2fcaa8066972281183e4b4b97782586
Richard Freimer <richard@freimer.com>
2020-09-28 14:49:18 -0400

    Make mobs be able to spawn with osmium armor, and make rates more configurable https://github.com/mekanism/Mekanism-Feature-Requests/issues/110 Note: the base armor spawn chance will need to be set again as the config option moved locations

--- src/tools/java/mekanism/tools/common/MekanismTools.java
+++ src/tools/java/mekanism/tools/common/MekanismTools.java
@@ -7,6 +7,7 @@ import mekanism.common.base.IModule;
 import mekanism.common.config.MekanismModConfig;
 import mekanism.common.lib.Version;
 import mekanism.tools.common.config.MekanismToolsConfig;
+import mekanism.tools.common.config.ToolsConfig.ArmorSpawnChanceConfig;
 import mekanism.tools.common.registries.ToolsItems;
 import mekanism.tools.common.registries.ToolsRecipeSerializers;
 import net.minecraft.entity.LivingEntity;
@@ -76,20 +77,21 @@ public class MekanismTools implements IModule {
         }
     }
 
-    private void setEntityArmorWithChance(Random random, LivingEntity entity, IItemProvider sword, IItemProvider helmet, IItemProvider chestplate, IItemProvider leggings, IItemProvider boots) {
-        if (entity instanceof ZombieEntity && random.nextInt(100) < 50) {
+    private void setEntityArmorWithChance(Random random, LivingEntity entity, IItemProvider sword, IItemProvider helmet, IItemProvider chestplate, IItemProvider leggings,
+          IItemProvider boots, ArmorSpawnChanceConfig chanceConfig) {
+        if (entity instanceof ZombieEntity && random.nextDouble() < chanceConfig.swordChance.get()) {
             setStackIfEmpty(entity, EquipmentSlotType.MAINHAND, sword.getItemStack());
         }
-        if (random.nextInt(100) < 50) {
+        if (random.nextDouble() < chanceConfig.helmetChance.get()) {
             setStackIfEmpty(entity, EquipmentSlotType.HEAD, helmet.getItemStack());
         }
-        if (random.nextInt(100) < 50) {
+        if (random.nextDouble() < chanceConfig.chestplateChance.get()) {
             setStackIfEmpty(entity, EquipmentSlotType.CHEST, chestplate.getItemStack());
         }
-        if (random.nextInt(100) < 50) {
+        if (random.nextDouble() < chanceConfig.leggingsChance.get()) {
             setStackIfEmpty(entity, EquipmentSlotType.LEGS, leggings.getItemStack());
         }
-        if (random.nextInt(100) < 50) {
+        if (random.nextDouble() < chanceConfig.bootsChance.get()) {
             setStackIfEmpty(entity, EquipmentSlotType.FEET, boots.getItemStack());
         }
     }
@@ -102,22 +104,25 @@ public class MekanismTools implements IModule {
             double chance = random.nextDouble();
             if (chance < MekanismToolsConfig.tools.armorSpawnRate.get()) {
                 //We can only spawn refined glowstone armor on piglins
-                int armorType = entity instanceof PiglinEntity ? 0 : random.nextInt(5);
+                int armorType = entity instanceof PiglinEntity ? 0 : random.nextInt(6);
                 if (armorType == 0) {
                     setEntityArmorWithChance(random, entity, ToolsItems.REFINED_GLOWSTONE_SWORD, ToolsItems.REFINED_GLOWSTONE_HELMET, ToolsItems.REFINED_GLOWSTONE_CHESTPLATE,
-                          ToolsItems.REFINED_GLOWSTONE_LEGGINGS, ToolsItems.REFINED_GLOWSTONE_BOOTS);
+                          ToolsItems.REFINED_GLOWSTONE_LEGGINGS, ToolsItems.REFINED_GLOWSTONE_BOOTS, MekanismToolsConfig.tools.refinedGlowstoneSpawnRate);
                 } else if (armorType == 1) {
                     setEntityArmorWithChance(random, entity, ToolsItems.LAPIS_LAZULI_SWORD, ToolsItems.LAPIS_LAZULI_HELMET, ToolsItems.LAPIS_LAZULI_CHESTPLATE,
-                          ToolsItems.LAPIS_LAZULI_LEGGINGS, ToolsItems.LAPIS_LAZULI_BOOTS);
+                          ToolsItems.LAPIS_LAZULI_LEGGINGS, ToolsItems.LAPIS_LAZULI_BOOTS, MekanismToolsConfig.tools.lapisLazuliSpawnRate);
                 } else if (armorType == 2) {
                     setEntityArmorWithChance(random, entity, ToolsItems.REFINED_OBSIDIAN_SWORD, ToolsItems.REFINED_OBSIDIAN_HELMET, ToolsItems.REFINED_OBSIDIAN_CHESTPLATE,
-                          ToolsItems.REFINED_OBSIDIAN_LEGGINGS, ToolsItems.REFINED_OBSIDIAN_BOOTS);
+                          ToolsItems.REFINED_OBSIDIAN_LEGGINGS, ToolsItems.REFINED_OBSIDIAN_BOOTS, MekanismToolsConfig.tools.refinedObsidianSpawnRate);
                 } else if (armorType == 3) {
-                    setEntityArmorWithChance(random, entity, ToolsItems.STEEL_SWORD, ToolsItems.STEEL_HELMET, ToolsItems.STEEL_CHESTPLATE,
-                          ToolsItems.STEEL_LEGGINGS, ToolsItems.STEEL_BOOTS);
+                    setEntityArmorWithChance(random, entity, ToolsItems.STEEL_SWORD, ToolsItems.STEEL_HELMET, ToolsItems.STEEL_CHESTPLATE, ToolsItems.STEEL_LEGGINGS,
+                          ToolsItems.STEEL_BOOTS, MekanismToolsConfig.tools.steelSpawnRate);
                 } else if (armorType == 4) {
-                    setEntityArmorWithChance(random, entity, ToolsItems.BRONZE_SWORD, ToolsItems.BRONZE_HELMET, ToolsItems.BRONZE_CHESTPLATE,
-                          ToolsItems.BRONZE_LEGGINGS, ToolsItems.BRONZE_BOOTS);
+                    setEntityArmorWithChance(random, entity, ToolsItems.BRONZE_SWORD, ToolsItems.BRONZE_HELMET, ToolsItems.BRONZE_CHESTPLATE, ToolsItems.BRONZE_LEGGINGS,
+                          ToolsItems.BRONZE_BOOTS, MekanismToolsConfig.tools.bronzeSpawnRate);
+                } else {//armorType == 5
+                    setEntityArmorWithChance(random, entity, ToolsItems.OSMIUM_SWORD, ToolsItems.OSMIUM_HELMET, ToolsItems.OSMIUM_CHESTPLATE, ToolsItems.OSMIUM_LEGGINGS,
+                          ToolsItems.OSMIUM_BOOTS, MekanismToolsConfig.tools.osmiumSpawnRate);
                 }
             }
         }
--- src/tools/java/mekanism/tools/common/config/ToolsConfig.java
+++ src/tools/java/mekanism/tools/common/config/ToolsConfig.java
@@ -1,6 +1,7 @@
 package mekanism.tools.common.config;
 
 import mekanism.common.config.BaseMekanismConfig;
+import mekanism.common.config.IMekanismConfig;
 import mekanism.common.config.value.CachedDoubleValue;
 import mekanism.tools.common.material.MaterialCreator;
 import mekanism.tools.common.material.impl.BronzeMaterialDefaults;
@@ -17,6 +18,12 @@ public class ToolsConfig extends BaseMekanismConfig {
     private final ForgeConfigSpec configSpec;
 
     public final CachedDoubleValue armorSpawnRate;
+    public final ArmorSpawnChanceConfig bronzeSpawnRate;
+    public final ArmorSpawnChanceConfig lapisLazuliSpawnRate;
+    public final ArmorSpawnChanceConfig osmiumSpawnRate;
+    public final ArmorSpawnChanceConfig refinedGlowstoneSpawnRate;
+    public final ArmorSpawnChanceConfig refinedObsidianSpawnRate;
+    public final ArmorSpawnChanceConfig steelSpawnRate;
     public final MaterialCreator bronze;
     public final MaterialCreator lapisLazuli;
     public final MaterialCreator osmium;
@@ -28,8 +35,16 @@ public class ToolsConfig extends BaseMekanismConfig {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.comment("Mekanism Tools Config. This config is synced from server to client.").push("tools");
 
+        builder.push("mobArmorSpawnRate");
         armorSpawnRate = CachedDoubleValue.wrap(this, builder.comment("The chance that Mekanism Armor can spawn on mobs.")
-              .defineInRange("mobArmorSpawnRate", 0.03D, 0, 1));
+              .defineInRange("general", 0.03D, 0, 1));
+        bronzeSpawnRate = new ArmorSpawnChanceConfig(this, builder, "bronze", "Bronze", 0.5, 0.5, 0.5, 0.5, 0.5);
+        lapisLazuliSpawnRate = new ArmorSpawnChanceConfig(this, builder, "lapis_lazuli", "Lapis Lazuli", 0.5, 0.5, 0.5, 0.5, 0.5);
+        osmiumSpawnRate = new ArmorSpawnChanceConfig(this, builder, "osmium", "Osmium", 0.5, 0.5, 0.5, 0.5, 0.5);
+        refinedGlowstoneSpawnRate = new ArmorSpawnChanceConfig(this, builder, "refined_glowstone", "Refined Glowstone", 0.5, 0.5, 0.5, 0.5, 0.5);
+        refinedObsidianSpawnRate = new ArmorSpawnChanceConfig(this, builder, "refined_obsidian", "Refined Obsidian", 0.5, 0.5, 0.5, 0.5, 0.5);
+        steelSpawnRate = new ArmorSpawnChanceConfig(this, builder, "steel", "Steel", 0.5, 0.5, 0.5, 0.5, 0.5);
+        builder.pop();
 
         bronze = new MaterialCreator(this, builder, new BronzeMaterialDefaults());
         lapisLazuli = new MaterialCreator(this, builder, new LapisLazuliMaterialDefaults());
@@ -54,5 +69,31 @@ public class ToolsConfig extends BaseMekanismConfig {
     @Override
     public Type getConfigType() {
         return Type.SERVER;
+    }
+
+    public static class ArmorSpawnChanceConfig {
+
+        public final CachedDoubleValue swordChance;
+        public final CachedDoubleValue helmetChance;
+        public final CachedDoubleValue chestplateChance;
+        public final CachedDoubleValue leggingsChance;
+        public final CachedDoubleValue bootsChance;
+
+        private ArmorSpawnChanceConfig(IMekanismConfig config, ForgeConfigSpec.Builder builder, String armorKey, String armor, double swordChance, double helmetChance,
+              double chestplateChance, double leggingsChance, double bootsChance) {
+            builder.comment("Spawn chances for pieces of " + armor + " gear. Note: These values are after the general mobArmorSpawnRate has been checked, and " +
+                  "after an even split between armor types has been done.").push(armorKey);
+            this.swordChance = CachedDoubleValue.wrap(config, builder.comment("The chance that mobs can spawn with " + armor + " Swords.")
+                  .defineInRange("swordChance", swordChance, 0, 1));
+            this.helmetChance = CachedDoubleValue.wrap(config, builder.comment("The chance that mobs can spawn with " + armor + " Helmets.")
+                  .defineInRange("helmetChance", helmetChance, 0, 1));
+            this.chestplateChance = CachedDoubleValue.wrap(config, builder.comment("The chance that mobs can spawn with " + armor + " Chestplates.")
+                  .defineInRange("chestplateChance", chestplateChance, 0, 1));
+            this.leggingsChance = CachedDoubleValue.wrap(config, builder.comment("The chance that mobs can spawn with " + armor + " Leggings.")
+                  .defineInRange("leggingsChance", leggingsChance, 0, 1));
+            this.bootsChance = CachedDoubleValue.wrap(config, builder.comment("The chance that mobs can spawn with " + armor + " Boots.")
+                  .defineInRange("bootsChance", bootsChance, 0, 1));
+            builder.pop();
+        }
     }
 }
