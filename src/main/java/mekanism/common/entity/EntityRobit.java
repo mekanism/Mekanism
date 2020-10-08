22124d051b5034ef463d94bb6d138fccf0cd7cd0
Richard Freimer <richard@freimer.com>
2020-10-08 17:01:30 -0400

    Make it so that if a robit is following an online player and the chunk the robit is in unloads, register a chunk loading ticket for one second to allow the robit to recheck its distance and teleport to its owner so that it doesn't get lost in an unloaded chunk due to the owner teleporting away. Note: It may take a few seconds for the robit to actually appear

--- src/main/java/mekanism/common/entity/EntityRobit.java
+++ src/main/java/mekanism/common/entity/EntityRobit.java
@@ -54,6 +54,7 @@ import mekanism.common.registries.MekanismDamageSource;
 import mekanism.common.registries.MekanismEntityTypes;
 import mekanism.common.registries.MekanismItems;
 import mekanism.common.tile.TileEntityChargepad;
+import mekanism.common.tile.component.TileComponentChunkLoader;
 import mekanism.common.tile.interfaces.ISustainedInventory;
 import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.NBTUtils;
@@ -83,10 +84,12 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.server.TicketType;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.util.Constants.NBT;
 import net.minecraftforge.fml.network.NetworkHooks;
@@ -97,6 +100,7 @@ import net.minecraftforge.items.ItemHandlerHelper;
 public class EntityRobit extends CreatureEntity implements IMekanismInventory, ISustainedInventory, ICachedRecipeHolder<ItemStackToItemStackRecipe>,
       IMekanismStrictEnergyHandler {
 
+    private static final TicketType<Integer> ROBIT_CHUNK_UNLOAD = TicketType.create("robit_chunk_unload", Integer::compareTo, 20);
     private static final DataParameter<String> OWNER_UUID = EntityDataManager.createKey(EntityRobit.class, DataSerializers.STRING);
     private static final DataParameter<String> OWNER_NAME = EntityDataManager.createKey(EntityRobit.class, DataSerializers.STRING);
     private static final DataParameter<Boolean> FOLLOW = EntityDataManager.createKey(EntityRobit.class, DataSerializers.BOOLEAN);
@@ -198,10 +202,24 @@ public class EntityRobit extends CreatureEntity implements IMekanismInventory, I
     }
 
     @Override
+    public void onRemovedFromWorld() {
+        if (world != null && !world.isRemote && getFollowing() && getOwner() != null) {
+            //If this robit is currently following its owner and is being removed from the world (due to chunk unloading)
+            // register a ticket that loads the chunk for a second, so that it has time to have its following check run again
+            // (as it runs every 10 ticks, half a second), and then teleport to the owner.
+            ((ServerWorld) world).getChunkProvider().registerTicket(ROBIT_CHUNK_UNLOAD, new ChunkPos(getPosition()), TileComponentChunkLoader.TICKET_DISTANCE, getEntityId());
+        }
+        super.onRemovedFromWorld();
+    }
+
+    @Override
     public void baseTick() {
         if (!world.isRemote) {
-            if (getFollowing() && getOwner() != null && getDistanceSq(getOwner()) > 4 && !getNavigator().noPath() && !energyContainer.isEmpty()) {
-                energyContainer.extract(getRoundedTravelEnergy(), Action.EXECUTE, AutomationType.INTERNAL);
+            if (getFollowing()) {
+                PlayerEntity owner = getOwner();
+                if (owner != null && getDistanceSq(owner) > 4 && !getNavigator().noPath() && !energyContainer.isEmpty()) {
+                    energyContainer.extract(getRoundedTravelEnergy(), Action.EXECUTE, AutomationType.INTERNAL);
+                }
             }
         }
 
