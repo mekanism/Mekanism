8789f617c180bea1fad966aae9ea1c56a329fa9b
Richard Freimer <richard@freimer.com>
2020-10-28 14:07:21 -0400

    Fix not registering our configured features because apparently there is a vanilla registry for them that we need to register to so that they don't potentially cause issues for other mods

--- src/main/java/mekanism/common/world/GenHandler.java
+++ src/main/java/mekanism/common/world/GenHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Random;
 import javax.annotation.Nullable;
 import mekanism.api.providers.IBlockProvider;
+import mekanism.common.Mekanism;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.config.WorldConfig.OreConfig;
 import mekanism.common.config.WorldConfig.SaltConfig;
@@ -17,6 +18,8 @@ import mekanism.common.util.EnumUtils;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.Category;
 import net.minecraft.world.gen.GenerationStage;
@@ -33,6 +36,7 @@ import net.minecraft.world.gen.placement.NoPlacementConfig;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.fml.ModLoader;
 
 public class GenHandler {
 
@@ -46,17 +50,34 @@ public class GenHandler {
     private static ConfiguredFeature<?, ?> SALT_RETROGEN_FEATURE;
 
     public static void setupWorldGenFeatures() {
-        for (OreType type : EnumUtils.ORE_TYPES) {
-            ORES.put(type, getOreFeature(MekanismBlocks.ORES.get(type), MekanismConfig.world.ores.get(type), Feature.ORE));
-        }
-        SALT_FEATURE = getSaltFeature(MekanismBlocks.SALT_BLOCK, MekanismConfig.world.salt, Placements.KELP_PLACEMENT);
-        //Retrogen features
-        if (MekanismConfig.world.enableRegeneration.get()) {
+        if (ModLoader.isLoadingStateValid()) {
+            //Validate our loading state is valid, and if it is register our configured features to the configured features registry
             for (OreType type : EnumUtils.ORE_TYPES) {
-                ORE_RETROGENS.put(type, getOreFeature(MekanismBlocks.ORES.get(type), MekanismConfig.world.ores.get(type), MekanismFeatures.ORE_RETROGEN.getFeature()));
+                ConfiguredFeature<?, ?> oreFeature = getOreFeature(MekanismBlocks.ORES.get(type), MekanismConfig.world.ores.get(type), Feature.ORE);
+                if (oreFeature != null) {
+                    ORES.put(type, oreFeature);
+                    Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, Mekanism.rl("ore_" + type.getResource().getRegistrySuffix()), oreFeature);
+                }
             }
-            SALT_RETROGEN_FEATURE = getSaltFeature(MekanismBlocks.SALT_BLOCK, MekanismConfig.world.salt,
-                  MekanismPlacements.TOP_SOLID_RETROGEN.getConfigured(IPlacementConfig.NO_PLACEMENT_CONFIG));
+            SALT_FEATURE = getSaltFeature(MekanismBlocks.SALT_BLOCK, MekanismConfig.world.salt, Placements.KELP_PLACEMENT);
+            if (SALT_FEATURE != null) {
+                Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, Mekanism.rl("salt"), SALT_FEATURE);
+            }
+            //Retrogen features
+            if (MekanismConfig.world.enableRegeneration.get()) {
+                for (OreType type : EnumUtils.ORE_TYPES) {
+                    ConfiguredFeature<?, ?> oreFeature = getOreFeature(MekanismBlocks.ORES.get(type), MekanismConfig.world.ores.get(type), MekanismFeatures.ORE_RETROGEN.getFeature());
+                    if (oreFeature != null) {
+                        ORE_RETROGENS.put(type, oreFeature);
+                        Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, Mekanism.rl("ore_" + type.getResource().getRegistrySuffix() + "_retrogen"), oreFeature);
+                    }
+                }
+                SALT_RETROGEN_FEATURE = getSaltFeature(MekanismBlocks.SALT_BLOCK, MekanismConfig.world.salt,
+                      MekanismPlacements.TOP_SOLID_RETROGEN.getConfigured(IPlacementConfig.NO_PLACEMENT_CONFIG));
+                if (SALT_RETROGEN_FEATURE != null) {
+                    Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, Mekanism.rl("salt_retrogen"), SALT_RETROGEN_FEATURE);
+                }
+            }
         }
     }
 
@@ -65,22 +86,18 @@ public class GenHandler {
             BiomeGenerationSettingsBuilder generation = event.getGeneration();
             //Add ores
             for (ConfiguredFeature<?, ?> feature : ORES.values()) {
-                addFeature(generation, feature);
+                generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, feature);
             }
             //Add salt
-            addFeature(generation, SALT_FEATURE);
+            if (SALT_FEATURE != null) {
+                generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, SALT_FEATURE);
+            }
         }
     }
 
     private static boolean isValidBiome(Biome.Category biomeCategory) {
         //If this does weird things to unclassified biomes (Category.NONE), then we should also mark that biome as invalid
         return biomeCategory != Category.THEEND && biomeCategory != Category.NETHER;
-    }
-
-    private static void addFeature(BiomeGenerationSettingsBuilder generation, @Nullable ConfiguredFeature<?, ?> feature) {
-        if (feature != null) {
-            generation.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES, feature);
-        }
     }
 
     @Nullable
@@ -122,10 +139,6 @@ public class GenHandler {
     }
 
     private static boolean placeFeature(@Nullable ConfiguredFeature<?, ?> feature, ServerWorld world, Random random, BlockPos blockPos) {
-        if (feature != null) {
-            feature.func_242765_a(world, world.getChunkProvider().getChunkGenerator(), random, blockPos);
-            return true;
-        }
-        return false;
+        return feature != null && feature.func_242765_a(world, world.getChunkProvider().getChunkGenerator(), random, blockPos);
     }
 }
