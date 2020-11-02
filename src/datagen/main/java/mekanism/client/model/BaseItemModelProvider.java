0397a17849e9b4304a28f4a3b8f736ca49348750
Richard Freimer <richard@freimer.com>
2020-11-02 11:26:50 -0500

    Update dev version of forge and adjust our model data generation for buckets to use forge's model builder instead of our hacked together limited one

--- src/datagen/main/java/mekanism/client/model/BaseItemModelProvider.java
+++ src/datagen/main/java/mekanism/client/model/BaseItemModelProvider.java
@@ -2,15 +2,14 @@ package mekanism.client.model;
 
 import javax.annotation.Nonnull;
 import mekanism.api.providers.IItemProvider;
-import mekanism.client.model.builder.BucketModelBuilder;
 import mekanism.common.registration.impl.FluidRegistryObject;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.item.BucketItem;
 import net.minecraft.resources.ResourcePackType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile.UncheckedModelFile;
+import net.minecraftforge.client.model.generators.loaders.DynamicBucketModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public abstract class BaseItemModelProvider extends ItemModelProvider {
@@ -77,20 +76,9 @@ public abstract class BaseItemModelProvider extends ItemModelProvider {
 
     //Note: This isn't the best way to do this in terms of model file validation, but it works
     protected void registerBucket(FluidRegistryObject<?, ?, ?, ?> fluidRO) {
-        BucketItem bucket = fluidRO.getBucket();
-        ResourceLocation outputLoc = extendWithFolder(bucket.getRegistryName());
-        if (generatedModels.containsKey(outputLoc)) {
-            throw new RuntimeException("Model with output loc: '" + outputLoc + "' has already been registered");
-        }
-        ItemModelBuilder modelBuilder = new BucketModelBuilder(outputLoc, existingFileHelper, fluidRO.getStillFluid().getRegistryName())
-              .parent(new UncheckedModelFile(new ResourceLocation("forge", "item/bucket")));
-        generatedModels.put(outputLoc, modelBuilder);
-    }
-
-    protected ResourceLocation extendWithFolder(ResourceLocation rl) {
-        if (rl.getPath().contains("/")) {
-            return rl;
-        }
-        return new ResourceLocation(rl.getNamespace(), folder + "/" + rl.getPath());
+        getBuilder(fluidRO.getBucket().getRegistryName().getPath())
+              .parent(new UncheckedModelFile(new ResourceLocation("forge", "item/bucket")))
+              .customLoader(DynamicBucketModelBuilder::begin)
+              .fluid(fluidRO.getStillFluid());
     }
 }
