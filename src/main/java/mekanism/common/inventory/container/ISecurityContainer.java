ebf355c7443f03111c3d2c0c57269d1a6476358b
Sara Freimer <sara@freimer.com>
2021-02-16 11:34:36 -0500

    Make GuiSecurityTab's ISecurityObject properly "update" when the held stack changes to properly update what level it displays #6993

--- src/main/java/mekanism/common/inventory/container/ISecurityContainer.java
+++ src/main/java/mekanism/common/inventory/container/ISecurityContainer.java
@@ -4,5 +4,8 @@ import mekanism.common.lib.security.ISecurityObject;
 
 public interface ISecurityContainer {
 
+    /**
+     * @apiNote Only for use on the server, which means that it doesn't need to properly update on the client side if the stack changes
+     */
     ISecurityObject getSecurityObject();
 }
