6fdfafce1f8e6f896647fcd15384a30c9071f7ed
Sara Freimer <sara@freimer.com>
2021-04-17 13:12:09 -0400

    Fix security desk not persisting inventory when broken and placed again #7111

--- src/main/java/mekanism/common/item/block/ItemBlockSecurityDesk.java
+++ src/main/java/mekanism/common/item/block/ItemBlockSecurityDesk.java
@@ -1,16 +1,29 @@
 package mekanism.common.item.block;
 
+import java.util.List;
 import javax.annotation.Nonnull;
+import mekanism.api.text.EnumColor;
+import mekanism.common.MekanismLang;
 import mekanism.common.block.basic.BlockSecurityDesk;
+import mekanism.common.item.interfaces.IItemSustainedInventory;
 import mekanism.common.registration.impl.ItemDeferredRegister;
 import mekanism.common.util.WorldUtils;
+import mekanism.common.util.text.BooleanStateDisplay.YesNo;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
 
-public class ItemBlockSecurityDesk extends ItemBlockTooltip<BlockSecurityDesk> {
+public class ItemBlockSecurityDesk extends ItemBlockTooltip<BlockSecurityDesk> implements IItemSustainedInventory {
 
     public ItemBlockSecurityDesk(BlockSecurityDesk block) {
         super(block, ItemDeferredRegister.getMekBaseProperties());
+    }
+
+    @Override
+    public void addDetails(@Nonnull ItemStack stack, World world, @Nonnull List<ITextComponent> tooltip, boolean advanced) {
+        tooltip.add(MekanismLang.HAS_INVENTORY.translateColored(EnumColor.AQUA, EnumColor.GRAY, YesNo.of(hasInventory(stack))));
     }
 
     @Override
