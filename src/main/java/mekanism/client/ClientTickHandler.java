8b2d2e833581127879474909f499b28a3cc3f166
Milo <milo.mai.cn@gmail.com>
2021-07-20 04:22:26 +0800

    Avoid VisionEnhancement night vision flashing (#7250)

--- src/main/java/mekanism/client/ClientTickHandler.java
+++ src/main/java/mekanism/client/ClientTickHandler.java
@@ -242,8 +242,8 @@ public class ClientTickHandler {
 
             if (isVisionEnhancementOn(minecraft.player)) {
                 visionEnhancement = true;
-                // adds if it doesn't exist, otherwise tops off duration to 200
-                minecraft.player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 200, 0, false, true, false));
+                // adds if it doesn't exist, otherwise tops off duration to 220. equal or less than 200 will make vision flickers
+                minecraft.player.addPotionEffect(new EffectInstance(Effects.NIGHT_VISION, 220, 0, false, true, false));
             } else if (visionEnhancement) {
                 visionEnhancement = false;
                 minecraft.player.removePotionEffect(Effects.NIGHT_VISION);
