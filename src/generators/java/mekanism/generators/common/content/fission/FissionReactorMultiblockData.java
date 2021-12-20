mekanism/Mekanism

commit a38078fc75343f1b5c5c3ba93a0b572a61215b24

Fix a variety of issues relating to temperature (#7346)
- Make use of the biome's temperature in just about every ambient temperature calculation instead of just in the evaporation plant #6702
- Make the fission reactor's simulate environment method mirror the boiler one as there aren't exactly sides.
- Fix an issue in `ITileHeatHandler#simulateAdjacent` where it was calculating temperature differential against the environment instead of against the sinks temperature, and also using an incorrect heat capacity. This has the side effect of making thermodynamic conductors "usable"
- Move the evaporation plant's temp multiplier calculations to after the temperature is actually updated.
- Improve handling of how the evaporation plant updates the solar locations so as to be able to cache the existing array and other solars when updating a specific one
- Rewrote the calculation for how the biome's temperature modifier gets taken into account for calculating the biome's ambient temperature. Previously in the Evaporation Plant this had a small range where lower temperatures were considered "hotter". We should discuss if we want to cap it at five like I am currently doing or if we want to bump the range up to ten.

src/generators/java/mekanism/generators/common/content/fission/FissionReactorMultiblockData.java
@@ -93,6 +93,7 @@ public class FissionReactorMultiblockData extends MultiblockData implements IVal
     @WrappingComputerMethod(wrapper = ComputerHeatCapacitorWrapper.class, methodNames = "getTemperature")
     public MultiblockHeatCapacitor<FissionReactorMultiblockData> heatCapacitor;
 
+    private double biomeAmbientTemp;
     @ContainerSync
     @SyntheticComputerMethod(getter = "getEnvironmentalLoss")
     public double lastEnvironmentLoss = 0;
@@ -124,6 +125,8 @@ public class FissionReactorMultiblockData extends MultiblockData implements IVal
 
     public FissionReactorMultiblockData(TileEntityFissionReactorCasing tile) {
         super(tile);
+        //Default biome temp to the ambient temperature at the block we are at
+        biomeAmbientTemp = HeatAPI.getAmbientTemp(tile.getLevel(), tile.getTilePos());
         fluidCoolantTank = MultiblockFluidTank.create(this, tile, () -> getVolume() * COOLANT_PER_VOLUME,
               (stack, automationType) -> automationType != AutomationType.EXTERNAL, (stack, automationType) -> isFormed(),
               fluid -> fluid.getFluid().is(FluidTags.WATER) && gasCoolantTank.isEmpty(), null);
@@ -142,13 +145,14 @@ public class FissionReactorMultiblockData extends MultiblockData implements IVal
               gas -> gas == MekanismGases.NUCLEAR_WASTE.getChemical(), ChemicalAttributeValidator.ALWAYS_ALLOW, null);
         gasTanks.addAll(Arrays.asList(fuelTank, heatedCoolantTank, wasteTank, gasCoolantTank));
         heatCapacitor = MultiblockHeatCapacitor.create(this, tile, MekanismGeneratorsConfig.generators.fissionCasingHeatCapacity.get(),
-              () -> INVERSE_CONDUCTION_COEFFICIENT, () -> INVERSE_INSULATION_COEFFICIENT);
+              () -> INVERSE_CONDUCTION_COEFFICIENT, () -> INVERSE_INSULATION_COEFFICIENT, () -> biomeAmbientTemp);
         heatCapacitors.add(heatCapacitor);
     }
 
     @Override
     public void onCreated(World world) {
         super.onCreated(world);
+        biomeAmbientTemp = calculateAverageAmbientTemperature(world);
         // update the heat capacity now that we've read
         heatCapacitor.setHeatCapacity(MekanismGeneratorsConfig.generators.fissionCasingHeatCapacity.get() * locations.size(), true);
         hotZone = new AxisAlignedBB(getMinPos().getX() + 1, getMinPos().getY() + 1, getMinPos().getZ() + 1,
@@ -189,6 +193,14 @@ public class FissionReactorMultiblockData extends MultiblockData implements IVal
             prevWasteScale = wasteScale;
         }
         return needsPacket;
+    }
+
+    @Override
+    public double simulateEnvironment() {
+        double invConduction = HeatAPI.AIR_INVERSE_COEFFICIENT + (INVERSE_INSULATION_COEFFICIENT + INVERSE_CONDUCTION_COEFFICIENT);
+        double heatToTransfer = (heatCapacitor.getTemperature() - biomeAmbientTemp) / invConduction;
+        heatCapacitor.handleHeat(-heatToTransfer * heatCapacitor.getHeatCapacity());
+        return heatToTransfer;
     }
 
     @Override
@@ -286,7 +298,7 @@ public class FissionReactorMultiblockData extends MultiblockData implements IVal
             //Reset the partial waste as we just irradiated it and there is not much sense having it exist in limbo
             partialWaste = 0;
             //Reset the heat to the default of the heat capacitor
-            heatCapacitor.setHeat(heatCapacitor.getHeatCapacity() * HeatAPI.AMBIENT_TEMP);
+            heatCapacitor.setHeat(heatCapacitor.getHeatCapacity() * biomeAmbientTemp);
             //Force sync the update to the cache that corresponds to this multiblock
             MultiblockManager<FissionReactorMultiblockData>.CacheWrapper cacheWrapper = MekanismGenerators.fissionReactorManager.inventories.get(inventoryID);
             if (cacheWrapper != null) {

src/generators/java/mekanism/generators/common/tile/TileEntityHeatGenerator.java
@@ -6,7 +6,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import mekanism.api.Action;
 import mekanism.api.RelativeSide;
-import mekanism.api.heat.HeatAPI;
 import mekanism.api.heat.HeatAPI.HeatTransfer;
 import mekanism.api.heat.IHeatHandler;
 import mekanism.api.inventory.AutomationType;
@@ -14,6 +13,7 @@ import mekanism.api.math.FloatingLong;
 import mekanism.common.capabilities.Capabilities;
 import mekanism.common.capabilities.fluid.BasicFluidTank;
 import mekanism.common.capabilities.heat.BasicHeatCapacitor;
+import mekanism.common.capabilities.heat.CachedAmbientTemperature;
 import mekanism.common.capabilities.holder.fluid.FluidTankHelper;
 import mekanism.common.capabilities.holder.fluid.IFluidTankHolder;
 import mekanism.common.capabilities.holder.heat.HeatCapacitorHelper;
@@ -49,6 +49,7 @@ public class TileEntityHeatGenerator extends TileEntityGenerator {
     public static final int MAX_FLUID = 24_000;
     private static final double THERMAL_EFFICIENCY = 0.5;
     private static final FloatingLong MAX_PRODUCTION = FloatingLong.createConst(500);
+
     /**
      * The FluidTank for this generator.
      */
@@ -92,9 +93,9 @@ public class TileEntityHeatGenerator extends TileEntityGenerator {
 
     @Nonnull
     @Override
-    protected IHeatCapacitorHolder getInitialHeatCapacitors() {
+    protected IHeatCapacitorHolder getInitialHeatCapacitors(CachedAmbientTemperature ambientTemperature) {
         HeatCapacitorHelper builder = HeatCapacitorHelper.forSide(this::getDirection);
-        builder.addCapacitor(heatCapacitor = BasicHeatCapacitor.create(10, 5, 100, this));
+        builder.addCapacitor(heatCapacitor = BasicHeatCapacitor.create(10, 5, 100, ambientTemperature, this));
         return builder.build();
     }
 
@@ -159,10 +160,11 @@ public class TileEntityHeatGenerator extends TileEntityGenerator {
     @Nonnull
     @Override
     public HeatTransfer simulate() {
+        double ambientTemp = ambientTemperature.getAsDouble();
         double temp = getTotalTemperature();
         // 1 - Qc / Qh
-        double carnotEfficiency = 1 - Math.min(HeatAPI.AMBIENT_TEMP, temp) / Math.max(HeatAPI.AMBIENT_TEMP, temp);
-        double heatLost = THERMAL_EFFICIENCY * (temp - HeatAPI.AMBIENT_TEMP);
+        double carnotEfficiency = 1 - Math.min(ambientTemp, temp) / Math.max(ambientTemp, temp);
+        double heatLost = THERMAL_EFFICIENCY * (temp - ambientTemp);
         heatCapacitor.handleHeat(-heatLost);
         getEnergyContainer().insert(MAX_PRODUCTION.min(FloatingLong.create(Math.abs(heatLost) * carnotEfficiency)), Action.EXECUTE, AutomationType.INTERNAL);
         return super.simulate();

commit 58c334bcb2f06b881c91d076f2f219c6ddf67090

Fix more issues with the heat system some of which got introduced by #7346: - Fix displayed environmental loss for evaporation tower - Slightly improve performance of fission port adjacent heat check - Fixed calculation of adjacent heat transfer for conductors to make the network reader give correct information for efficiency percentages - Lazily initialize starting heat so that placing a heat generator in cold places doesn't cause it to generate a bit of heat while it cools to ambient - Revert a bunch of fixes to ITileHeatHandler#simulateAdjacent as they had the side effect of nerfing a variety of things about the heat system a lot which isn't something that should be done this close to a release Additionally, fixed a few typos/did some minor cleanup

build.gradle
@@ -37,7 +37,7 @@ apply plugin: 'org.parchmentmc.librarian.forgegradle'
 
 wrapper {
     //Define wrapper values here so as to not have to always do so when updating gradlew.properties
-    gradleVersion = '7.3.1'
+    gradleVersion = '7.3.2'
     distributionType = Wrapper.DistributionType.ALL
 }
 
@@ -49,8 +49,7 @@ idea {
         inheritOutputDirs = true
         //Exclude directories from being managed
         for (String excludeDirName in ["run", "out", "logs", "gradle"]) {
-            File excludeDir = new File(projectDir, excludeDirName)
-            excludeDirs.add(excludeDir)
+            excludeDirs.add(new File(projectDir, excludeDirName))
         }
     }
 }
@@ -339,10 +338,6 @@ repositories {
         name 'tterrag'
         url 'https://maven.tterrag.com/'
         content {
-            includeGroupByRegex 'com\\.enderio.*'
-            includeGroupByRegex 'com\\.enderzoo.*'
-            includeGroupByRegex 'com\\.tterrag.*'
-            includeGroupByRegex 'tterrag.*'
             includeGroup 'mcjty.theoneprobe'
             includeGroup 'team.chisel.ctm'
         }
@@ -377,10 +372,6 @@ repositories {
                     it.content {
                         excludeGroup 'curse.maven'
                         excludeGroup 'mezz.jei'
-                        excludeGroupByRegex 'com\\.enderio.*'
-                        excludeGroupByRegex 'com\\.enderzoo.*'
-                        excludeGroupByRegex 'com\\.tterrag.*'
-                        excludeGroupByRegex 'tterrag.*'
                         excludeGroup 'mcjty.theoneprobe'
                         excludeGroup 'team.chisel.ctm'
                         excludeGroup 'appeng'

gradle/wrapper/gradle-wrapper.properties
@@ -1,5 +1,5 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-7.3.1-all.zip
+distributionUrl=https\://services.gradle.org/distributions/gradle-7.3.2-all.zip
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists

src/generators/java/mekanism/generators/common/content/fission/FissionReactorMultiblockData.java
@@ -198,9 +198,9 @@ public class FissionReactorMultiblockData extends MultiblockData implements IVal
     @Override
     public double simulateEnvironment() {
         double invConduction = HeatAPI.AIR_INVERSE_COEFFICIENT + (INVERSE_INSULATION_COEFFICIENT + INVERSE_CONDUCTION_COEFFICIENT);
-        double heatToTransfer = (heatCapacitor.getTemperature() - biomeAmbientTemp) / invConduction;
-        heatCapacitor.handleHeat(-heatToTransfer * heatCapacitor.getHeatCapacity());
-        return heatToTransfer;
+        double tempToTransfer = (heatCapacitor.getTemperature() - biomeAmbientTemp) / invConduction;
+        heatCapacitor.handleHeat(-tempToTransfer * heatCapacitor.getHeatCapacity());
+        return Math.max(tempToTransfer, 0);
     }
 
     @Override

src/generators/java/mekanism/generators/common/tile/TileEntityHeatGenerator.java
@@ -172,7 +172,7 @@ public class TileEntityHeatGenerator extends TileEntityGenerator {
 
     @Nullable
     @Override
-    public IHeatHandler getAdjacent(Direction side) {
+    public IHeatHandler getAdjacent(@Nonnull Direction side) {
         if (side == Direction.DOWN) {
             TileEntity adj = WorldUtils.getTileEntity(getLevel(), worldPosition.below());
             return CapabilityUtils.getCapability(adj, Capabilities.HEAT_HANDLER_CAPABILITY, side.getOpposite()).resolve().orElse(null);

src/generators/java/mekanism/generators/common/tile/fission/TileEntityFissionReactorPort.java
@@ -9,12 +9,14 @@ import mekanism.api.chemical.gas.GasStack;
 import mekanism.api.chemical.gas.IGasTank;
 import mekanism.api.heat.IHeatHandler;
 import mekanism.common.MekanismLang;
+import mekanism.common.capabilities.Capabilities;
 import mekanism.common.capabilities.heat.CachedAmbientTemperature;
 import mekanism.common.capabilities.holder.chemical.IChemicalTankHolder;
 import mekanism.common.capabilities.holder.fluid.IFluidTankHolder;
 import mekanism.common.capabilities.holder.heat.IHeatCapacitorHolder;
 import mekanism.common.integration.computer.annotation.ComputerMethod;
 import mekanism.common.tile.base.SubstanceType;
+import mekanism.common.util.CapabilityUtils;
 import mekanism.common.util.ChemicalUtil;
 import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.WorldUtils;
@@ -23,6 +25,7 @@ import mekanism.generators.common.block.attribute.AttributeStateFissionPortMode.
 import mekanism.generators.common.content.fission.FissionReactorMultiblockData;
 import mekanism.generators.common.registries.GeneratorsBlocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Util;
@@ -50,14 +53,14 @@ public class TileEntityFissionReactorPort extends TileEntityFissionReactorCasing
 
     @Nullable
     @Override
-    public IHeatHandler getAdjacent(Direction side) {
-        IHeatHandler handler = super.getAdjacent(side);
-        if (handler != null) {
-            if (WorldUtils.getTileEntity(getLevel(), getBlockPos().relative(side)) instanceof TileEntityFissionReactorPort) {
-                return null;
+    public IHeatHandler getAdjacent(@Nonnull Direction side) {
+        if (canHandleHeat() && getHeatCapacitorCount(side) > 0) {
+            TileEntity adj = WorldUtils.getTileEntity(getLevel(), getBlockPos().relative(side));
+            if (!(adj instanceof TileEntityFissionReactorPort)) {
+                return CapabilityUtils.getCapability(adj, Capabilities.HEAT_HANDLER_CAPABILITY, side.getOpposite()).resolve().orElse(null);
             }
         }
-        return handler;
+        return null;
     }
 
     @Nonnull

src/generators/java/mekanism/generators/common/tile/fusion/TileEntityFusionReactorPort.java
@@ -96,12 +96,14 @@ public class TileEntityFusionReactorPort extends TileEntityFusionReactorBlock im
 
     @Nullable
     @Override
-    public IHeatHandler getAdjacent(Direction side) {
-        TileEntity adj = WorldUtils.getTileEntity(getLevel(), getBlockPos().relative(side));
-        if (adj instanceof TileEntityFusionReactorBlock) {
-            return null;
+    public IHeatHandler getAdjacent(@Nonnull Direction side) {
+        if (canHandleHeat() && getHeatCapacitorCount(side) > 0) {
+            TileEntity adj = WorldUtils.getTileEntity(getLevel(), getBlockPos().relative(side));
+            if (!(adj instanceof TileEntityFusionReactorBlock)) {
+                return CapabilityUtils.getCapability(adj, Capabilities.HEAT_HANDLER_CAPABILITY, side.getOpposite()).resolve().orElse(null);
+            }
         }
-        return CapabilityUtils.getCapability(adj, Capabilities.HEAT_HANDLER_CAPABILITY, side.getOpposite()).resolve().orElse(null);
+        return null;
     }
 
     @Override

src/main/java/mekanism/common/capabilities/heat/BasicHeatCapacitor.java
@@ -28,8 +28,8 @@ public class BasicHeatCapacitor implements IHeatCapacitor {
     private final double inverseInsulationCoefficient;
 
     // set to ambient * heat capacity by default
-    protected double storedHeat;
-    protected double heatToHandle;
+    private double storedHeat = -1;
+    private double heatToHandle;
 
     public static BasicHeatCapacitor create(double heatCapacity, @Nullable DoubleSupplier ambientTempSupplier, @Nullable IContentsListener listener) {
         return create(heatCapacity, HeatAPI.DEFAULT_INVERSE_CONDUCTION, HeatAPI.DEFAULT_INVERSE_INSULATION, ambientTempSupplier, listener);
@@ -53,9 +53,13 @@ public class BasicHeatCapacitor implements IHeatCapacitor {
         this.inverseInsulationCoefficient = inverseInsulationCoefficient;
         this.ambientTempSupplier = ambientTempSupplier;
         this.listener = listener;
+    }
 
-        // update the stored heat based on initial capacity
-        storedHeat = heatCapacity * getAmbientTemperature();
+    private void initStoredHeat() {
+        if (storedHeat == -1) {
+            //If the stored heat hasn't been initialized yet, update the stored heat based on initial capacity
+            storedHeat = heatCapacity * getAmbientTemperature();
+        }
     }
 
     protected double getAmbientTemperature() {
@@ -96,18 +100,19 @@ public class BasicHeatCapacitor implements IHeatCapacitor {
 
     public void update() {
         if (heatToHandle != 0) {
+            initStoredHeat();
             storedHeat += heatToHandle;
             //notify listeners
             onContentsChanged();
+            // reset our handling heat
+            heatToHandle = 0;
         }
-        // reset our handling heat
-        heatToHandle = 0;
     }
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
         NBTUtils.setDoubleIfPresent(nbt, NBTConstants.STORED, heat -> storedHeat = heat);
-        NBTUtils.setDoubleIfPresent(nbt, NBTConstants.HEAT_CAPACITY, capacity -> heatCapacity = capacity);
+        NBTUtils.setDoubleIfPresent(nbt, NBTConstants.HEAT_CAPACITY, capacity -> setHeatCapacity(capacity, false));
     }
 
     @Override
@@ -120,19 +125,20 @@ public class BasicHeatCapacitor implements IHeatCapacitor {
 
     @Override
     public double getHeat() {
+        initStoredHeat();
         return storedHeat;
     }
 
     @Override
     public void setHeat(double heat) {
-        if (storedHeat != heat) {
+        if (getHeat() != heat) {
             storedHeat = heat;
             onContentsChanged();
         }
     }
 
     public void setHeatCapacity(double newCapacity, boolean updateHeat) {
-        if (updateHeat) {
+        if (updateHeat && storedHeat != -1) {
             setHeat(getHeat() + (newCapacity - getHeatCapacity()) * getAmbientTemperature());
         }
         heatCapacity = newCapacity;

src/main/java/mekanism/common/capabilities/heat/ITileHeatHandler.java
@@ -8,8 +8,6 @@ import mekanism.api.heat.HeatAPI.HeatTransfer;
 import mekanism.api.heat.IHeatCapacitor;
 import mekanism.api.heat.IHeatHandler;
 import mekanism.api.heat.IMekanismHeatHandler;
-import mekanism.common.lib.transmitter.TransmissionType;
-import mekanism.common.tile.transmitter.TileEntityTransmitter;
 import mekanism.common.util.EnumUtils;
 import net.minecraft.util.Direction;
 
@@ -33,7 +31,7 @@ public interface ITileHeatHandler extends IMekanismHeatHandler {
      * @return The {@link IHeatHandler} adjacent to this {@link ITileHeatHandler}, otherwise returns {@code null}.
      */
     @Nullable
-    default IHeatHandler getAdjacent(@Nullable Direction side) {
+    default IHeatHandler getAdjacent(Direction side) {
         return null;
     }
 
@@ -57,7 +55,10 @@ public interface ITileHeatHandler extends IMekanismHeatHandler {
             //transfer heat difference based on environment temperature (ambient)
             double tempToTransfer = (getTotalTemperature(side) - getAmbientTemperature(side)) / invConduction;
             handleHeat(-tempToTransfer * heatCapacity, side);
-            environmentTransfer += tempToTransfer;
+            if (tempToTransfer > 0) {
+                //Only count it towards environmental loss if it is hotter than the ambient temperature
+                environmentTransfer += tempToTransfer;
+            }
         }
         return environmentTransfer;
     }
@@ -67,17 +68,41 @@ public interface ITileHeatHandler extends IMekanismHeatHandler {
         for (Direction side : EnumUtils.DIRECTIONS) {
             IHeatHandler sink = getAdjacent(side);
             if (sink != null) {
+                double heatCapacity = getTotalHeatCapacity(side);
                 double invConduction = sink.getTotalInverseConduction() + getTotalInverseConductionCoefficient(side);
-                double tempToTransfer = (getTotalTemperature(side) - sink.getTotalTemperature()) / invConduction;
-                handleHeat(-tempToTransfer * getTotalHeatCapacity(side), side);
-                //Note: Our sinks in mek are "lazy" but they will update the next tick if needed
-                sink.handleHeat(tempToTransfer * sink.getTotalHeatCapacity());
-                if (!(sink instanceof TileEntityTransmitter) || !TransmissionType.HEAT.checkTransmissionType((TileEntityTransmitter) sink)) {
-                    adjacentTransfer += tempToTransfer;
+                double tempToTransfer = (getTotalTemperature(side) - getAmbientTemperature(side)) / invConduction;
+                //TODO - 1.18: Try and figure out how to do this properly/I believe the below is correct
+                // but it seems to nerf the heat system quite a bit so needs more review than being able
+                // to be done just before a release is made
+                /*double temp = getTotalTemperature(side);
+                double sinkTemp = sink.getTotalTemperature();
+                if (temp <= sinkTemp) {
+                    //If our temperature is lower than the sink, we skip calculating what the adjacent loss to the sink
+                    // is as if the sink is able to have heat transferred away from it (which is a bit of a weird concept
+                    // in relation to thermodynamics, but makes some sense with our implementation), it will be handled by
+                    // the sink when the sink simulates adjacent heat transfers. This also prevents us from having heat
+                    // transfers effectively happen "twice" per tick rather than just once
+                    // Note: We also skip if our temp is equal to the sink's temperature so that we can short circuit
+                    // past the following logic
+                    continue;
                 }
+                double heatCapacity = getTotalHeatCapacity(side);
+                double sinkHeatCapacity = sink.getTotalHeatCapacity();
+                //Calculate the target temperature using calorimetry
+                double finalTemp = (temp * heatCapacity + sinkTemp * sinkHeatCapacity) / (heatCapacity + sinkHeatCapacity);
+                double invConduction = sink.getTotalInverseConduction() + getTotalInverseConductionCoefficient(side);
+                double tempToTransfer = (temp - finalTemp) / invConduction;*/
+                double heatToTransfer = tempToTransfer * heatCapacity;
+                handleHeat(-heatToTransfer, side);
+                //Note: Our sinks in mek are "lazy" but they will update the next tick if needed
+                sink.handleHeat(heatToTransfer);
+                adjacentTransfer = incrementAdjacentTransfer(adjacentTransfer, tempToTransfer, side);
             }
-
         }
         return adjacentTransfer;
+    }
+
+    default double incrementAdjacentTransfer(double currentAdjacentTransfer, double tempToTransfer, Direction side) {
+        return currentAdjacentTransfer + tempToTransfer;
     }
 }

src/main/java/mekanism/common/content/boiler/BoilerMultiblockData.java
@@ -233,9 +233,9 @@ public class BoilerMultiblockData extends MultiblockData implements IValveHandle
     @Override
     public double simulateEnvironment() {
         double invConduction = HeatAPI.AIR_INVERSE_COEFFICIENT + (CASING_INVERSE_INSULATION_COEFFICIENT + CASING_INVERSE_CONDUCTION_COEFFICIENT);
-        double heatToTransfer = (heatCapacitor.getTemperature() - biomeAmbientTemp) / invConduction;
-        heatCapacitor.handleHeat(-heatToTransfer * heatCapacitor.getHeatCapacity());
-        return heatToTransfer;
+        double tempToTransfer = (heatCapacitor.getTemperature() - biomeAmbientTemp) / invConduction;
+        heatCapacitor.handleHeat(-tempToTransfer * heatCapacitor.getHeatCapacity());
+        return Math.max(tempToTransfer, 0);
     }
 
     public int getWaterVolume() {

src/main/java/mekanism/common/content/evaporation/EvaporationMultiblockData.java
@@ -169,7 +169,7 @@ public class EvaporationMultiblockData extends MultiblockData implements IValveH
             }
             heatCapacitor.handleHeat(heatCapacity * incr);
             if (incr < 0) {
-                return -incr / heatCapacity;
+                return -incr;
             }
         }
         return 0;

src/main/java/mekanism/common/content/network/HeatNetwork.java
@@ -54,7 +54,7 @@ public class HeatNetwork extends DynamicNetwork<IHeatHandler, HeatNetwork, Therm
         }
         //After we updated the heat values of all the transmitters, we need to update the temperatures
         // we do this after instead of when iterating initially so that if heat is transferred from one
-        // conductor to one we already updated then we want it to have the proper total temperaturer
+        // conductor to one we already updated then we want it to have the proper total temperature
         for (ThermodynamicConductor transmitter : transmitters) {
             transmitter.updateHeatCapacitors(null);
             newSumTemp += transmitter.getTotalTemperature();

src/main/java/mekanism/common/content/network/transmitter/ThermodynamicConductor.java
@@ -154,12 +154,25 @@ public class ThermodynamicConductor extends Transmitter<IHeatHandler, HeatNetwor
 
     @Nullable
     @Override
-    public IHeatHandler getAdjacent(Direction side) {
+    public IHeatHandler getAdjacent(@Nonnull Direction side) {
         if (connectionMapContainsSide(getAllCurrentConnections(), side)) {
             //Note: We use the acceptor cache as the heat network is different and the transmitters count the other transmitters in the
             // network as valid acceptors
             return getAcceptorCache().getConnectedAcceptor(side).resolve().orElse(null);
         }
         return null;
+    }
+
+    @Override
+    public double incrementAdjacentTransfer(double currentAdjacentTransfer, double tempToTransfer, @Nonnull Direction side) {
+        if (tempToTransfer > 0) {
+            //Look up the adjacent tile from the acceptor cache and then do the type checking
+            TileEntity sink = getAcceptorCache().getConnectedAcceptorTile(side);
+            if (sink instanceof TileEntityTransmitter && TransmissionType.HEAT.checkTransmissionType((TileEntityTransmitter) sink)) {
+                //Heat transmitter to heat transmitter, don't count as "adjacent transfer"
+                return currentAdjacentTransfer;
+            }
+        }
+        return ITileHeatHandler.super.incrementAdjacentTransfer(currentAdjacentTransfer, tempToTransfer, side);
     }
 }

src/main/java/mekanism/common/lib/transmitter/acceptor/AcceptorCache.java
@@ -78,6 +78,17 @@ public class AcceptorCache<ACCEPTOR> extends AbstractAcceptorCache<ACCEPTOR, Acc
         return LazyOptional.empty();
     }
 
+    @Nullable
+    public TileEntity getConnectedAcceptorTile(Direction side) {
+        if (cachedAcceptors.containsKey(side)) {
+            TileEntity tile = cachedAcceptors.get(side).getTile();
+            if (!tile.isRemoved()) {
+                return tile;
+            }
+        }
+        return null;
+    }
+
     /**
      * @apiNote Only call this from the server side
      */

src/main/java/mekanism/common/recipe/ingredient/IngredientWithout.java
@@ -111,7 +111,7 @@ public class IngredientWithout extends Ingredient {
                 return new IngredientWithout(Ingredient.fromJson(json.getAsJsonObject(JsonConstants.BASE)),
                       Ingredient.fromJson(json.getAsJsonObject(JsonConstants.WITHOUT)));
             }
-            throw new JsonParseException("A without ingredient must have both a bse ingredient, and a negation ingredient.");
+            throw new JsonParseException("A without ingredient must have both a base ingredient, and a negation ingredient.");
         }
 
         @Override

src/main/java/mekanism/common/tile/TileEntityQuantumEntangloporter.java
@@ -212,7 +212,7 @@ public class TileEntityQuantumEntangloporter extends TileEntityConfigurableMachi
 
     @Nullable
     @Override
-    public IHeatHandler getAdjacent(Direction side) {
+    public IHeatHandler getAdjacent(@Nonnull Direction side) {
         if (hasFrequency()) {
             ISlotInfo slotInfo = configComponent.getSlotInfo(TransmissionType.HEAT, side);
             if (slotInfo != null && slotInfo.canInput()) {

src/main/java/mekanism/common/tile/base/TileEntityMekanism.java
@@ -1070,7 +1070,7 @@ public abstract class TileEntityMekanism extends CapabilityTileEntity implements
 
     @Nullable
     @Override
-    public IHeatHandler getAdjacent(Direction side) {
+    public IHeatHandler getAdjacent(@Nonnull Direction side) {
         if (canHandleHeat() && getHeatCapacitorCount(side) > 0) {
             TileEntity adj = WorldUtils.getTileEntity(getLevel(), getBlockPos().relative(side));
             return CapabilityUtils.getCapability(adj, Capabilities.HEAT_HANDLER_CAPABILITY, side.getOpposite()).resolve().orElse(null);