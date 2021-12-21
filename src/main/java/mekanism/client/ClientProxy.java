5f6b91053d5fb8d7013106f77f03fed6d30966d0
maggi373 <40539743+maggi373@users.noreply.github.com>
2021-12-21 02:01:58 +0100

    remove packages

--- src/main/java/mekanism/client/ClientProxy.java
+++ src/main/java/mekanism/client/ClientProxy.java
@@ -1,8 +1,6 @@
 package mekanism.client;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import com.jadarstudios.developercapes.DevCapes;
 import cpw.mods.fml.common.Loader;
