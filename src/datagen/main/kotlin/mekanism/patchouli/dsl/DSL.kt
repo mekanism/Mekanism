9b41fbaefc078c90c388eacbef4ff6d56106453f
Thiakil <xander@thiakil.com>
2022-02-26 14:29:38 +0800

    attempt kotlin update and fix compilation for class move

--- src/datagen/main/kotlin/mekanism/patchouli/dsl/DSL.kt
+++ src/datagen/main/kotlin/mekanism/patchouli/dsl/DSL.kt
@@ -4,10 +4,10 @@ package mekanism.patchouli.dsl
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import mekanism.api.gear.ModuleData
 import mekanism.api.providers.IBlockProvider
 import mekanism.api.providers.IGasProvider
 import mekanism.api.providers.IItemProvider
-import mekanism.common.content.gear.Modules
 import net.minecraft.client.settings.KeyBinding
 import net.minecraft.item.ItemStack
 import net.minecraft.util.ResourceLocation
@@ -53,7 +53,7 @@ val IItemProvider.bookId: String get() {
 
 val IGasProvider.bookId: String get() = "gas/" + this.registryName.path
 
-val Modules.ModuleData<*>.bookId: String get() = "items/modules/"+this.stack.item.registryName!!
+val ModuleData<*>.bookId: String get() = "items/modules/"+this.itemProvider.registryName
 
 private fun link(id:String, text:String): String = "$(l:${id})${text}$(/l)"
 
@@ -67,10 +67,10 @@ fun link(item: IGasProvider, text: String): String = link(item.bookId, text)
 fun link(guideEntry: IGuideEntry, text: String): String = link(guideEntry.entryId, text)
 
 @PatchouliDSL
-fun link(module: Modules.ModuleData<*>, text: String): String = link(module.bookId, text)
+fun link(module: ModuleData<*>, text: String): String = link(module.bookId, text)
 
 operator fun KeyBinding.invoke(): String {
-    return "$(k:${keyDescription})"
+    return "$(k:${name})"
 }
 
 val LOGGER = LogManager.getLogger("PatchouliDSL")!!
