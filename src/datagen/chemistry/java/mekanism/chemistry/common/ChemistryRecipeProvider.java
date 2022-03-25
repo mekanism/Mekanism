96be877d4d52b974e31317f0897e90ad48684c3f
Justin Hu <justin.hu@alumni.ubc.ca>
2022-03-25 13:23:31 -0700

    Recipe changes
    Ammonium now produced from PRC
    Ammonia now takes 1:3 Nitrogen:Hydrogen

--- src/datagen/chemistry/java/mekanism/chemistry/common/ChemistryRecipeProvider.java
+++ src/datagen/chemistry/java/mekanism/chemistry/common/ChemistryRecipeProvider.java
@@ -1,5 +1,6 @@
 package mekanism.chemistry.common;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -7,6 +8,7 @@ import mekanism.api.chemical.gas.Gas;
 import mekanism.api.datagen.recipe.builder.ChemicalChemicalToChemicalRecipeBuilder;
 import mekanism.api.datagen.recipe.builder.ChemicalCrystallizerRecipeBuilder;
 import mekanism.api.datagen.recipe.builder.DistillingRecipeBuilder;
+import mekanism.api.datagen.recipe.builder.PressurizedReactionRecipeBuilder;
 import mekanism.api.datagen.recipe.builder.RotaryRecipeBuilder;
 import mekanism.api.providers.IFluidProvider;
 import mekanism.api.providers.IGasProvider;
@@ -15,15 +17,17 @@ import mekanism.chemistry.common.registries.ChemistryFluids;
 import mekanism.chemistry.common.registries.ChemistryGases;
 import mekanism.chemistry.common.registries.ChemistryItems;
 import mekanism.common.recipe.BaseRecipeProvider;
-import mekanism.common.recipe.builder.ExtendedShapedRecipeBuilder;
 import mekanism.common.recipe.builder.ExtendedShapelessRecipeBuilder;
 import mekanism.common.registries.MekanismFluids;
 import mekanism.common.registries.MekanismGases;
 import mekanism.common.registries.MekanismItems;
+import mekanism.common.tags.MekanismTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 @ParametersAreNonnullByDefault
@@ -39,7 +43,28 @@ public class ChemistryRecipeProvider extends BaseRecipeProvider {
         addRotaryCondensentratorRecipes(consumer);
         addDistillingRecipes(consumer);
         addChemicalInfuserRecipes(consumer);
-        addChemicalCrystalizerRecipes(consumer);
+        addPressurizedReactionChamberRecipes(consumer);
+    }
+
+    private void addPressurizedReactionChamberRecipes(Consumer<FinishedRecipe> consumer) {
+        String basePath = "reaction/";
+        PressurizedReactionRecipeBuilder.reaction(
+              IngredientCreatorAccess.item().from(ItemTags.COALS),
+              IngredientCreatorAccess.fluid().from(ChemistryFluids.AMMONIA, 100),
+              IngredientCreatorAccess.gas().from(MekanismGases.OXYGEN, 100),
+              100,
+              ChemistryItems.AMMONIUM.getItemStack())
+              .build(consumer, MekanismChemistry.rl(basePath + "ammonium_with_coals"));
+        PressurizedReactionRecipeBuilder.reaction(
+              IngredientCreatorAccess.item().from(BaseRecipeProvider.createIngredient(Arrays.asList(
+                    Tags.Items.STORAGE_BLOCKS_COAL,
+                    MekanismTags.Items.STORAGE_BLOCKS_CHARCOAL
+              ))),
+              IngredientCreatorAccess.fluid().from(ChemistryFluids.AMMONIA, 1000),
+              IngredientCreatorAccess.gas().from(MekanismGases.OXYGEN, 100),
+              900,
+              ChemistryItems.AMMONIUM.getItemStack(10))
+              .build(consumer, MekanismChemistry.rl(basePath + "ammonium_with_blocks_coals"));
     }
 
     private void addCraftingRecipes(Consumer<FinishedRecipe> consumer) {
@@ -50,21 +75,12 @@ public class ChemistryRecipeProvider extends BaseRecipeProvider {
               .build(consumer);
     }
 
-    private void addChemicalCrystalizerRecipes(Consumer<FinishedRecipe> consumer) {
-        String basePath = "crystallizing/";
-        //Ammonia
-        ChemicalCrystallizerRecipeBuilder.crystallizing(
-              IngredientCreatorAccess.gas().from(ChemistryGases.AMMONIA, 100),
-              ChemistryItems.AMMONIUM.getItemStack()
-        ).build(consumer, MekanismChemistry.rl(basePath + "ammonium"));
-    }
-
     private void addChemicalInfuserRecipes(Consumer<FinishedRecipe> consumer) {
         String basePath = "chemical_infusing/";
         //Ammonia
         ChemicalChemicalToChemicalRecipeBuilder.chemicalInfusing(
               IngredientCreatorAccess.gas().from(ChemistryGases.NITROGEN, 1),
-              IngredientCreatorAccess.gas().from(MekanismGases.HYDROGEN, 1),
+              IngredientCreatorAccess.gas().from(MekanismGases.HYDROGEN, 3),
               ChemistryGases.AMMONIA.getStack(1)
         ).build(consumer, MekanismChemistry.rl(basePath + "ammonia"));
     }
