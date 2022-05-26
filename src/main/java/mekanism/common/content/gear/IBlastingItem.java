355f9c2996af9b94806be22c24d49fa13f05c94a
mewacaser <wacaserme@gmail.com>
2022-05-25 19:37:32 -0500

    Cleaned up findPositions

--- src/main/java/mekanism/common/content/gear/IBlastingItem.java
+++ src/main/java/mekanism/common/content/gear/IBlastingItem.java
@@ -7,7 +7,7 @@ import mekanism.common.content.gear.mekatool.ModuleVeinMiningUnit;
 import mekanism.common.util.MultipartUtils;
 import mekanism.common.util.MultipartUtils.RayTraceVectors;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -20,51 +20,40 @@ public interface IBlastingItem {
     Map<BlockPos, BlockState> getBlastedBlocks(Level world, Player player, ItemStack stack, BlockPos pos, BlockState state);
 
     static Map<BlockPos, BlockState> findPositions(Level world, BlockPos targetPos, Player player, int radius) {
-        if (radius <= 0) {
-            return Collections.emptyMap();
-        }
-        RayTraceVectors rayTraceVectors = MultipartUtils.getRayTraceVectors(player);
-        BlockHitResult hitResult = Shapes.block().clip(rayTraceVectors.start(), rayTraceVectors.end(), targetPos);
-        if (hitResult != null) {
-            return findPositions(world, targetPos, hitResult.getDirection(), radius);
+        if (radius > 0) {
+            RayTraceVectors rayTraceVectors = MultipartUtils.getRayTraceVectors(player);
+            BlockHitResult hitResult = Shapes.block().clip(rayTraceVectors.start(), rayTraceVectors.end(), targetPos);
+            if (hitResult != null) {
+                Vec3i lower, upper;
+                switch (hitResult.getDirection()) {
+                    case UP, DOWN -> {
+                        lower = new Vec3i(-radius, 0, -radius);
+                        upper = new Vec3i(radius, 0, radius);
+                    }
+                    case EAST, WEST -> {
+                        lower = new Vec3i(0, -1, -radius);
+                        upper = new Vec3i(0, 2 * radius - 1, radius);
+                    }
+                    case NORTH, SOUTH -> {
+                        lower = new Vec3i(-radius, -1, 0);
+                        upper = new Vec3i(radius, 2 * radius - 1, 0);
+                    }
+                    default -> {
+                        lower = new Vec3i(0, 0, 0);
+                        upper = new Vec3i(0, 0, 0);
+                    }
+                }
+                Map<BlockPos, BlockState> found = new HashMap<>();
+                for (BlockPos nextPos : BlockPos.betweenClosed(targetPos.offset(lower), targetPos.offset(upper))) {
+                    BlockState nextState = world.getBlockState(nextPos);
+                    if (canBlastBlock(world, nextPos, nextState)) {
+                        found.put(nextPos.immutable(), nextState);
+                    }
+                }
+                return found;
+            }
         }
         return Collections.emptyMap();
-    }
-
-    static Map<BlockPos, BlockState> findPositions(Level world, BlockPos targetPos, Direction axis, int radius) {
-        if (radius <= 0) {
-            return Collections.emptyMap();
-        }
-        int minX = 0, minY = 0, minZ = 0;
-        int maxX = 0, maxY = 0, maxZ = 0;
-        switch (axis) {
-            case UP, DOWN -> {
-                minX = -radius;
-                maxX = radius;
-                minZ = -radius;
-                maxZ = radius;
-            }
-            case EAST, WEST -> {
-                minY = -1;
-                maxY = 2 * radius - 1;
-                minZ = -radius;
-                maxZ = radius;
-            }
-            case NORTH, SOUTH -> {
-                minX = -radius;
-                maxX = radius;
-                minY = -1;
-                maxY = 2 * radius - 1;
-            }
-        }
-        Map<BlockPos, BlockState> found = new HashMap<>();
-        for (BlockPos nextPos : BlockPos.betweenClosed(targetPos.offset(minX, minY, minZ), targetPos.offset(maxX, maxY, maxZ))) {
-            BlockState nextState = world.getBlockState(nextPos);
-            if (canBlastBlock(world, nextPos, nextState)) {
-                found.put(nextPos.immutable(), nextState);
-            }
-        }
-        return found;
     }
 
     static boolean canBlastBlock(Level world, BlockPos pos, BlockState state) {
