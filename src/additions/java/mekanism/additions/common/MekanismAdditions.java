e272bba56faeb7f4590a678c21d3dc4a81998804
Sara Freimer <sara@freimer.com>
2022-06-14 16:08:12 -0500

    Inline the remaining methods from spawn helper

--- src/additions/java/mekanism/additions/common/MekanismAdditions.java
+++ src/additions/java/mekanism/additions/common/MekanismAdditions.java
@@ -4,7 +4,7 @@ import javax.annotation.Nonnull;
 import mekanism.additions.client.AdditionsClient;
 import mekanism.additions.common.block.BlockObsidianTNT;
 import mekanism.additions.common.config.MekanismAdditionsConfig;
-import mekanism.additions.common.entity.SpawnHelper;
+import mekanism.additions.common.entity.baby.EntityBabyStray;
 import mekanism.additions.common.registries.AdditionsBiomeModifierSerializers;
 import mekanism.additions.common.registries.AdditionsBlocks;
 import mekanism.additions.common.registries.AdditionsEntityTypes;
@@ -16,13 +16,19 @@ import mekanism.common.Mekanism;
 import mekanism.common.base.IModModule;
 import mekanism.common.config.MekanismModConfig;
 import mekanism.common.lib.Version;
+import mekanism.common.registration.impl.EntityTypeRegistryObject;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockSource;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Parrot;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
@@ -100,7 +106,18 @@ public class MekanismAdditions implements IModModule {
             //Ensure our tags are all initialized
             AdditionsTags.init();
             //Setup some stuff related to entities
-            SpawnHelper.setupEntities();
+            //Register spawn controls for the baby entities based on the vanilla spawn controls
+            registerSpawnControls(AdditionsEntityTypes.BABY_CREEPER, AdditionsEntityTypes.BABY_ENDERMAN, AdditionsEntityTypes.BABY_SKELETON,
+                  AdditionsEntityTypes.BABY_WITHER_SKELETON);
+            //Slightly different restrictions for the baby stray, as strays have a slightly different spawn restriction
+            SpawnPlacements.register(AdditionsEntityTypes.BABY_STRAY.get(), SpawnPlacements.Type.ON_GROUND,
+                  Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, EntityBabyStray::spawnRestrictions);
+            //Add parrot sound imitations for baby mobs
+            //Note: There is no imitation sound for endermen
+            Parrot.MOB_SOUND_MAP.put(AdditionsEntityTypes.BABY_CREEPER.get(), SoundEvents.PARROT_IMITATE_CREEPER);
+            Parrot.MOB_SOUND_MAP.put(AdditionsEntityTypes.BABY_SKELETON.get(), SoundEvents.PARROT_IMITATE_SKELETON);
+            Parrot.MOB_SOUND_MAP.put(AdditionsEntityTypes.BABY_STRAY.get(), SoundEvents.PARROT_IMITATE_STRAY);
+            Parrot.MOB_SOUND_MAP.put(AdditionsEntityTypes.BABY_WITHER_SKELETON.get(), SoundEvents.PARROT_IMITATE_WITHER_SKELETON);
             //Dispenser behavior
             DispenserBlock.registerBehavior(AdditionsBlocks.OBSIDIAN_TNT, new DefaultDispenseItemBehavior() {
                 @Nonnull
@@ -118,6 +135,14 @@ public class MekanismAdditions implements IModModule {
             });
         });
         Mekanism.logger.info("Loaded 'Mekanism: Additions' module.");
+    }
+
+    @SafeVarargs
+    private static void registerSpawnControls(EntityTypeRegistryObject<? extends Monster>... entityTypeROs) {
+        for (EntityTypeRegistryObject<? extends Monster> entityTypeRO : entityTypeROs) {
+            SpawnPlacements.register(entityTypeRO.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                  Monster::checkMonsterSpawnRules);
+        }
     }
 
     private void serverStarting(ServerStartingEvent event) {
