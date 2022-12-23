daea345a0684db4f534a05146ca5c482bd082c2c
maggi373 <40539743+maggi373@users.noreply.github.com>
2022-12-23 23:10:58 +0100

    add min max for int for setting tier level

--- src/main/java/mekanism/common/CommonProxy.java
+++ src/main/java/mekanism/common/CommonProxy.java
@@ -311,8 +311,8 @@ public class CommonProxy implements IGuiProvider
 		general.OreDictPlatinum = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "OreDictPlatinum", false).getBoolean();
 		general.enableSiliconCompat = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "EnableSiliconCompat", true).getBoolean();
 
-		general.IC2SinkTier = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "IC2SinkTier", 2).getInt();
-		general.IC2SourceTier = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "IC2SourceTier", 2).getInt();
+		general.IC2SinkTier = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "IC2SinkTier", 2, "Tier level for machines", 1, 4).getInt();
+		general.IC2SourceTier = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "IC2SourceTier", 2, "Tier level for generators", 1, 4).getInt();
 
 		String s = Mekanism.configuration.get(Configuration.CATEGORY_GENERAL, "EnergyType", "RF", null, new String[]{"J", "RF", "MJ", "EU"}).getString();
 
