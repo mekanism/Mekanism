a9f3b9f0e95529aa7dc5153e0dade967bbde8542
Thiakil <xander@thiakil.com>
2023-08-22 16:57:51 +0800

    ensure originatingElements isnt checked for equality

--- annotation-processor/src/main/java/mekanism/AnnotationParamScanner.java
+++ annotation-processor/src/main/java/mekanism/AnnotationParamScanner.java
@@ -12,4 +12,26 @@ public record AnnotationParamScanner(String targetFile, Set<Class<? extends Anno
     public AnnotationParamScanner(String targetFile, Class<? extends Annotation>... supportedAnnotations) {
         this(targetFile, Set.of(supportedAnnotations), new ArrayList<>());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof AnnotationParamScanner that)) {
+            return false;
+        }
+
+        if (!targetFile.equals(that.targetFile)) {
+            return false;
+        }
+        return supportedAnnotations.equals(that.supportedAnnotations);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = targetFile.hashCode();
+        result = 31 * result + supportedAnnotations.hashCode();
+        return result;
+    }
 }
