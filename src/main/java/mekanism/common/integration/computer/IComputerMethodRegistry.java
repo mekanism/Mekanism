d2074dae8668fa15a063136983c959d5c4b47c3d
Thiakil <xander@thiakil.com>
2023-08-24 13:51:16 +0800

    add missed class

---
+++ src/main/java/mekanism/common/integration/computer/IComputerMethodRegistry.java
@@ -0,0 +1,8 @@
+package mekanism.common.integration.computer;
+
+public interface IComputerMethodRegistry {
+    /**
+     * Register contained methods to {@link FactoryRegistry}
+     */
+    void register();
+}
