dafcc0feec320890523872522218ef1796604424
Thiakil <xander@thiakil.com>
2023-08-29 20:03:22 +0800

    better human names

--- src/datagen/main/java/mekanism/common/integration/computer/ComputerHelpProvider.java
+++ src/datagen/main/java/mekanism/common/integration/computer/ComputerHelpProvider.java
@@ -11,14 +11,36 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import mekanism.api.annotations.ParametersAreNotNullByDefault;
+import mekanism.common.content.filter.IFilter;
+import mekanism.common.content.filter.IItemStackFilter;
+import mekanism.common.content.filter.IModIDFilter;
+import mekanism.common.content.filter.ITagFilter;
+import mekanism.common.content.miner.MinerFilter;
+import mekanism.common.content.oredictionificator.OredictionificatorFilter;
+import mekanism.common.content.oredictionificator.OredictionificatorItemFilter;
+import mekanism.common.content.qio.filter.QIOFilter;
+import mekanism.common.content.transporter.SorterFilter;
 import mekanism.common.integration.computer.MethodHelpData.Param;
 import mekanism.common.integration.computer.MethodHelpData.Returns;
+import mekanism.common.lib.multiblock.MultiblockData;
+import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.factory.TileEntityFactory;
+import mekanism.common.tile.factory.TileEntityItemStackGasToItemStackFactory;
+import mekanism.common.tile.laser.TileEntityBasicLaser;
+import mekanism.common.tile.multiblock.TileEntityInductionPort;
+import mekanism.common.tile.prefab.TileEntityAdvancedElectricMachine;
+import mekanism.common.tile.prefab.TileEntityMultiblock;
+import mekanism.common.tile.prefab.TileEntityProgressMachine;
+import mekanism.common.tile.qio.TileEntityQIOComponent;
+import mekanism.common.tile.qio.TileEntityQIOFilterHandler;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.Util;
 import net.minecraft.data.CachedOutput;
@@ -27,7 +49,9 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.CsvOutput;
 import net.minecraft.util.CsvOutput.Builder;
+import net.minecraft.util.ExtraCodecs;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.apache.commons.lang3.tuple.Pair;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNotNullByDefault
@@ -51,12 +75,13 @@ public class ComputerHelpProvider implements DataProvider {
         return CompletableFuture.allOf(
               DataProvider.saveStable(pOutput, element, this.pathProvider.json(new ResourceLocation(this.modid, "all"))),
               saveCSV(pOutput, this.pathProvider.file(new ResourceLocation(this.modid, "all"), "csv"), CSV_HEADERS, output -> {
-                  for (Entry<Class<?>, List<MethodHelpData>> entry : helpData.entrySet()) {
-                      Class<?> clazz = entry.getKey();
+                  List<Pair<String, List<MethodHelpData>>> friendlyList = helpData.entrySet().stream().map(entry -> Pair.of(getFriendlyName(entry.getKey()), entry.getValue())).sorted(Entry.comparingByKey()).toList();
+                  for (Pair<String, List<MethodHelpData>> entry : friendlyList) {
+                      String friendlyClassName = entry.getKey();
                       List<MethodHelpData> methods = entry.getValue();
                       for (MethodHelpData method : methods) {
                           output.writeRow(
-                                clazz.getSimpleName(),
+                                friendlyClassName,
                                 method.methodName(),
                                 method.params() != null ? method.params().stream().map(Param::name).collect(Collectors.joining(", ")) : "",
                                 csvReturnsValue(method.returns()),
@@ -109,10 +134,78 @@ public class ComputerHelpProvider implements DataProvider {
         }, Util.backgroundExecutor());
     }
 
+    private static String getFriendlyName(Class<?> clazz) {
+        return FRIENDLY_NAME_CACHE.computeIfAbsent(clazz, clz->{
+            if (FRIENDLY_NAMES.containsKey(clz)) {
+                return FRIENDLY_NAMES.get(clz);
+            }
+            String simpleName = clz.getSimpleName();
+            if (simpleName.startsWith("TileEntity")) {
+                simpleName = simpleName.substring(10);
+            }
+            if (simpleName.endsWith("MultiblockData")) {
+                simpleName = simpleName.substring(0, simpleName.indexOf("MultiblockData"));
+                simpleName = MULTIBLOCK_DATA_NAMES.getOrDefault(simpleName, splitClassName(simpleName));
+                return simpleName + " Multiblock (formed)";
+            }
+            if (simpleName.startsWith("TileComponent")) {
+                simpleName = splitClassName(simpleName.substring(13));
+                if ("Config".equals(simpleName)) {
+                    simpleName = "Side Configuration";
+                }
+                return "Machine with "+simpleName+" Component";
+            }
+
+            return splitClassName(simpleName);
+        });
+    }
+
+    private static String splitClassName(String simpleName) {
+        return CLASS_NAME_SPLITTER.splitAsStream(simpleName).collect(Collectors.joining(" "));
+    }
+
     @FunctionalInterface
     interface IOConsumer<T> {
         void accept(T value) throws IOException;
     }
 
-    private static final Codec<Map<Class<?>, List<MethodHelpData>>> JSONCODEC = Codec.unboundedMap(MethodHelpData.CLASS_TO_STRING_CODEC, MethodHelpData.CODEC.listOf());
+    private static final Codec<Class<?>> CLASS_TO_FRIENDLY_NAME_CODEC = ExtraCodecs.stringResolverCodec(ComputerHelpProvider::getFriendlyName, p->null);
+    private static final Codec<Map<Class<?>, List<MethodHelpData>>> JSONCODEC = Codec.unboundedMap(CLASS_TO_FRIENDLY_NAME_CODEC, MethodHelpData.CODEC.listOf());
+
+    private static final Map<Class<?>, String> FRIENDLY_NAMES = Map.ofEntries(
+          Map.entry(IFilter.class, "Filter Wrapper"),
+          Map.entry(IItemStackFilter.class, "Filter Wrapper (ItemStack)"),
+          Map.entry(IModIDFilter.class, "Filter Wrapper (Mod Id)"),
+          Map.entry(ITagFilter.class, "Filter Wrapper (Tag)"),
+          Map.entry(MinerFilter.class, "Filter Wrapper (Digital Miner)"),
+          Map.entry(OredictionificatorFilter.class, "Filter Wrapper (Oredictionificator)"),
+          Map.entry(OredictionificatorItemFilter.class, "Filter Wrapper (Oredictionificator Item)"),
+          Map.entry(QIOFilter.class, "Filter Wrapper (QIO)"),
+          Map.entry(SorterFilter.class, "Filter Wrapper (Logistical Sorter)"),
+          Map.entry(ComputerEnergyHelper.class, "API Global: computerEnergyHelper"),
+          Map.entry(ComputerFilterHelper.class, "API Global: computerFilterHelper"),
+          Map.entry(MultiblockData.class, "Multiblock (formed)"),
+          Map.entry(TileEntityMekanism.class, "Generic Mekanism Machine"),
+          Map.entry(TileEntityFactory.class, "Factory Machine"),
+          Map.entry(TileEntityBasicLaser.class, "Laser"),
+          Map.entry(TileEntityInductionPort.class, "Induction Matrix Port"),
+          Map.entry(TileEntityMultiblock.class, "Multiblock"),
+          Map.entry(TileEntityProgressMachine.class, "Machine with Recipe Progress"),
+          Map.entry(TileEntityQIOComponent.class, "QIO Machine"),
+          Map.entry(TileEntityAdvancedElectricMachine.class, "Compressing/Injecting/Purifying Machine"),
+          Map.entry(TileEntityItemStackGasToItemStackFactory.class, "Compressing/Injecting/Purifying Factory"),
+          Map.entry(TileEntityQIOFilterHandler.class, "QIO Machine with Filter")
+    );
+
+    private static final Map<String, String> MULTIBLOCK_DATA_NAMES = Map.of(
+          "Boiler", "Boiler",
+          "Evaporation", "Thermal Evaporation",
+          "Matrix", "Induction Matrix",
+          "Tank", "Dynamic Tank",
+          "Turbine", "Industrial Turbine"
+    );
+
+    private static final Map<Class<?>, String> FRIENDLY_NAME_CACHE = new HashMap<>();
+
+    private static final Pattern CLASS_NAME_SPLITTER = Pattern.compile("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
 }
