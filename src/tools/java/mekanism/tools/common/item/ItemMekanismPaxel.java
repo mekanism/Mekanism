20a6c700397fa2b6248bed1436b510a53b21b2a2
ChampionAsh5357 <championash5357@gmail.com>
2023-10-20 21:13:01 -0400

    Workaround Axe Enchantments on Paxel

--- src/tools/java/mekanism/tools/common/item/ItemMekanismPaxel.java
+++ src/tools/java/mekanism/tools/common/item/ItemMekanismPaxel.java
@@ -25,13 +25,10 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.AxeItem;
-import net.minecraft.world.item.DiggerItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CampfireBlock;
@@ -188,5 +185,13 @@ public class ItemMekanismPaxel extends DiggerItem implements IHasRepairType, IAt
     public void addToBuilder(ImmutableMultimap.Builder<Attribute, AttributeModifier> builder) {
         builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Tool modifier", getAttackDamage(), Operation.ADDITION));
         builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Tool modifier", material.getPaxelAtkSpeed(), Operation.ADDITION));
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        // Axe enchantments specifically check if the item is an instanceof AxeItem
+        // Paxel cannot extend AxeItem without replacing all methods relating to tag calls
+        // Easier to check an actual instanceof AxeItem to see if it can be applied
+        return enchantment.canEnchant(new ItemStack(Items.WOODEN_AXE)) || super.canApplyAtEnchantingTable(stack, enchantment);
     }
 }
