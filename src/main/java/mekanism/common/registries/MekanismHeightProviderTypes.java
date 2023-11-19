465fa859f55e1166a892ebf1a9bb9efb3cd4f8e6
Sara Freimer <sara@freimer.com>
2023-11-19 15:36:44 -0600

    Remove a few more unneeded deferred register wrappers

--- src/main/java/mekanism/common/registries/MekanismHeightProviderTypes.java
+++ src/main/java/mekanism/common/registries/MekanismHeightProviderTypes.java
@@ -2,8 +2,9 @@ package mekanism.common.registries;
 
 import mekanism.common.Mekanism;
 import mekanism.common.registration.MekanismDeferredHolder;
-import mekanism.common.registration.impl.HeightProviderTypeDeferredRegister;
+import mekanism.common.registration.MekanismDeferredRegister;
 import mekanism.common.world.height.ConfigurableHeightProvider;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.heightproviders.HeightProviderType;
 
 public class MekanismHeightProviderTypes {
@@ -11,7 +12,7 @@ public class MekanismHeightProviderTypes {
     private MekanismHeightProviderTypes() {
     }
 
-    public static final HeightProviderTypeDeferredRegister HEIGHT_PROVIDER_TYPES = new HeightProviderTypeDeferredRegister(Mekanism.MODID);
+    public static final MekanismDeferredRegister<HeightProviderType<?>> HEIGHT_PROVIDER_TYPES = new MekanismDeferredRegister<>(Registries.HEIGHT_PROVIDER_TYPE, Mekanism.MODID);
 
-    public static final MekanismDeferredHolder<HeightProviderType<?>, HeightProviderType<ConfigurableHeightProvider>> CONFIGURABLE = HEIGHT_PROVIDER_TYPES.register("configurable", ConfigurableHeightProvider.CODEC);
+    public static final MekanismDeferredHolder<HeightProviderType<?>, HeightProviderType<ConfigurableHeightProvider>> CONFIGURABLE = HEIGHT_PROVIDER_TYPES.register("configurable", () -> () -> ConfigurableHeightProvider.CODEC);
 }
--- src/main/java/mekanism/common/registries/MekanismIntProviderTypes.java
+++ src/main/java/mekanism/common/registries/MekanismIntProviderTypes.java
@@ -2,9 +2,10 @@ package mekanism.common.registries;
 
 import mekanism.common.Mekanism;
 import mekanism.common.registration.MekanismDeferredHolder;
-import mekanism.common.registration.impl.IntProviderTypeDeferredRegister;
+import mekanism.common.registration.MekanismDeferredRegister;
 import mekanism.common.world.ConfigurableConstantInt;
 import mekanism.common.world.ConfigurableUniformInt;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.util.valueproviders.IntProviderType;
 
 public class MekanismIntProviderTypes {
@@ -12,8 +13,8 @@ public class MekanismIntProviderTypes {
     private MekanismIntProviderTypes() {
     }
 
-    public static final IntProviderTypeDeferredRegister INT_PROVIDER_TYPES = new IntProviderTypeDeferredRegister(Mekanism.MODID);
+    public static final MekanismDeferredRegister<IntProviderType<?>> INT_PROVIDER_TYPES = new MekanismDeferredRegister<>(Registries.INT_PROVIDER_TYPE, Mekanism.MODID);
 
-    public static final MekanismDeferredHolder<IntProviderType<?>, IntProviderType<ConfigurableConstantInt>> CONFIGURABLE_CONSTANT = INT_PROVIDER_TYPES.register("configurable_constant", ConfigurableConstantInt.CODEC);
-    public static final MekanismDeferredHolder<IntProviderType<?>, IntProviderType<ConfigurableUniformInt>> CONFIGURABLE_UNIFORM = INT_PROVIDER_TYPES.register("configurable_uniform", ConfigurableUniformInt.CODEC);
+    public static final MekanismDeferredHolder<IntProviderType<?>, IntProviderType<ConfigurableConstantInt>> CONFIGURABLE_CONSTANT = INT_PROVIDER_TYPES.register("configurable_constant", () -> () -> ConfigurableConstantInt.CODEC);
+    public static final MekanismDeferredHolder<IntProviderType<?>, IntProviderType<ConfigurableUniformInt>> CONFIGURABLE_UNIFORM = INT_PROVIDER_TYPES.register("configurable_uniform", () -> () -> ConfigurableUniformInt.CODEC);
 }
