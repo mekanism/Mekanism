997b48ae407058145fe38cba83fdabf3aef6fa70
Sara Freimer <sara@freimer.com>
2023-11-19 15:36:44 -0600

    Remove a few more unneeded deferred register wrappers

--- src/main/java/mekanism/common/registries/MekanismPlacementModifiers.java
+++ src/main/java/mekanism/common/registries/MekanismPlacementModifiers.java
@@ -2,8 +2,9 @@ package mekanism.common.registries;
 
 import mekanism.common.Mekanism;
 import mekanism.common.registration.MekanismDeferredHolder;
-import mekanism.common.registration.impl.PlacementModifierDeferredRegister;
+import mekanism.common.registration.MekanismDeferredRegister;
 import mekanism.common.world.DisableableFeaturePlacement;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
 
 public class MekanismPlacementModifiers {
@@ -11,7 +12,7 @@ public class MekanismPlacementModifiers {
     private MekanismPlacementModifiers() {
     }
 
-    public static final PlacementModifierDeferredRegister PLACEMENT_MODIFIERS = new PlacementModifierDeferredRegister(Mekanism.MODID);
+    public static final MekanismDeferredRegister<PlacementModifierType<?>> PLACEMENT_MODIFIERS = new MekanismDeferredRegister<>(Registries.PLACEMENT_MODIFIER_TYPE, Mekanism.MODID);
 
-    public static final MekanismDeferredHolder<PlacementModifierType<?>, PlacementModifierType<DisableableFeaturePlacement>> DISABLEABLE = PLACEMENT_MODIFIERS.register("disableable", DisableableFeaturePlacement.CODEC);
+    public static final MekanismDeferredHolder<PlacementModifierType<?>, PlacementModifierType<DisableableFeaturePlacement>> DISABLEABLE = PLACEMENT_MODIFIERS.register("disableable", () -> () -> DisableableFeaturePlacement.CODEC);
 }
