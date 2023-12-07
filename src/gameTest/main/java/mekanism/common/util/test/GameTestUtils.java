60b7932cf07e78707cbbcd29b03370e23dffcd26
Sara Freimer <sara@freimer.com>
2023-12-07 17:59:38 -0600

    Fix gametests not running and switch to devtime only ATs rather than reflection

--- src/gameTest/main/java/mekanism/common/util/test/GameTestUtils.java
+++ src/gameTest/main/java/mekanism/common/util/test/GameTestUtils.java
@@ -1,7 +1,5 @@
 package mekanism.common.util.test;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.function.BooleanSupplier;
 import java.util.function.UnaryOperator;
 import mekanism.common.Mekanism;
@@ -15,18 +13,10 @@ import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.DistanceManager;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.neoforged.fml.util.ObfuscationReflectionHelper;
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.jetbrains.annotations.Nullable;
 
 public class GameTestUtils {
-
-    //Note: We use reflection rather than ATs as this only happens at dev time so no need to do bytecode hacks on things that don't need it at runtime
-    private static final Method UPDATE_CHUNK_SCHEDULING = ObfuscationReflectionHelper.findMethod(DistanceManager.class, "m_7288_",
-          Long.TYPE, Integer.TYPE, ChunkHolder.class, Integer.TYPE);
-    private static final Method GET_CHUNK = ObfuscationReflectionHelper.findMethod(DistanceManager.class, "m_7316_", Long.TYPE);
-    private static final Method PROCESS_UNLOADS = ObfuscationReflectionHelper.findMethod(ChunkMap.class, "m_140353_", BooleanSupplier.class);
-    private static final Method ENSURE_SINGLE_FINAL_CHECK = ObfuscationReflectionHelper.findMethod(GameTestHelper.class, "m_177442_");
 
     public static final int INACCESSIBLE_LEVEL = ChunkMap.MAX_VIEW_DISTANCE + 1;
     private static final int UNLOAD_LEVEL = ChunkLevel.MAX_LEVEL + 1;
@@ -37,11 +27,7 @@ public class GameTestUtils {
     static final boolean DEBUG_CHUNK_LOADING = false;
 
     public static void succeedIfSequence(GameTestHelper helper, UnaryOperator<GameTestSequence> sequence) {
-        try {
-            //Try to mark it as a single check test but if we fail don't worry about it
-            ENSURE_SINGLE_FINAL_CHECK.invoke(helper);
-        } catch (IllegalAccessException | InvocationTargetException ignored) {
-        }
+        helper.ensureSingleFinalCheck();
         sequence.apply(helper.startSequence()).thenSucceed();
     }
 
@@ -94,27 +80,23 @@ public class GameTestUtils {
             //If the chunk isn't watched and is loaded we want to try and unload it
             ChunkMap chunkMap = helper.getLevel().getChunkSource().chunkMap;
             DistanceManager distanceManager = chunkMap.getDistanceManager();
-            try {
-                ChunkHolder holder = (ChunkHolder) GET_CHUNK.invoke(distanceManager, absPos);
-                //Watch the chunk and mark whether it is currently loaded or not
-                if (holder != null) {
-                    //If it is loaded then we need to try and unload it
-                    GameTestEventListeners.watchedChunks.put(absPos, true);
-                    if (DEBUG_CHUNK_LOADING) {
-                        Mekanism.logger.info("Trying to unload chunk at: {}, {}", absolutePos.x, absolutePos.z);
-                    }
-                    //If it is currently loaded, queue it for unload
-                    levelMemory.setValue(holder.getTicketLevel());
-                    UPDATE_CHUNK_SCHEDULING.invoke(distanceManager, absPos, UNLOAD_LEVEL, holder, holder.getTicketLevel());
-                    //And then unload it
-                    PROCESS_UNLOADS.invoke(chunkMap, ALWAYS_TRUE);
-                    fail(helper, "Chunk queued for unloading", absolutePos, relativePos);
-                } else if (DEBUG_CHUNK_LOADING) {
-                    //Note: Even with debug logging enabled odds are this case isn't even possible due to the earlier check to skip if unloaded
-                    Mekanism.logger.info("Trying to unload already unloaded chunk at: {}, {}", absolutePos.x, absolutePos.z);
+            ChunkHolder holder = distanceManager.getChunk(absPos);
+            //Watch the chunk and mark whether it is currently loaded or not
+            if (holder != null) {
+                //If it is loaded then we need to try and unload it
+                GameTestEventListeners.watchedChunks.put(absPos, true);
+                if (DEBUG_CHUNK_LOADING) {
+                    Mekanism.logger.info("Trying to unload chunk at: {}, {}", absolutePos.x, absolutePos.z);
                 }
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                fail(helper, "Error unloading chunk", absolutePos, relativePos);
+                //If it is currently loaded, queue it for unload
+                levelMemory.setValue(holder.getTicketLevel());
+                distanceManager.updateChunkScheduling(absPos, UNLOAD_LEVEL, holder, holder.getTicketLevel());
+                //And then unload it
+                chunkMap.processUnloads(ALWAYS_TRUE);
+                fail(helper, "Chunk queued for unloading", absolutePos, relativePos);
+            } else if (DEBUG_CHUNK_LOADING) {
+                //Note: Even with debug logging enabled odds are this case isn't even possible due to the earlier check to skip if unloaded
+                Mekanism.logger.info("Trying to unload already unloaded chunk at: {}, {}", absolutePos.x, absolutePos.z);
             }
         } else if (DEBUG_CHUNK_LOADING) {
             Mekanism.logger.info("Chunk at: {}, {} is already unloaded", absolutePos.x, absolutePos.z);
@@ -141,29 +123,25 @@ public class GameTestUtils {
             //If the chunk isn't watched and is not loaded we want to try and load it
             ChunkMap chunkMap = helper.getLevel().getChunkSource().chunkMap;
             DistanceManager distanceManager = chunkMap.getDistanceManager();
-            try {
-                ChunkHolder holder = (ChunkHolder) GET_CHUNK.invoke(distanceManager, absPos);
-                if (holder == null) {
-                    //If it is currently unloaded, queue it for loading
-                    GameTestEventListeners.watchedChunks.put(absPos, false);
-                    if (DEBUG_CHUNK_LOADING) {
-                        Mekanism.logger.info("Trying to load chunk at: {}, {}", absolutePos.x, absolutePos.z);
-                    }
-                    //Load the chunk to the level it was unloaded at
-                    holder = (ChunkHolder) UPDATE_CHUNK_SCHEDULING.invoke(distanceManager, absPos, levelMemory.getValue(), null, UNLOAD_LEVEL);
-                    if (holder == null) {//Should never happen unless start value was unloaded
-                        fail(helper, "Error loading chunk", absolutePos, relativePos);
-                    } else {
-                        //And ensure we schedule it based on the status (in general this should be ChunkStatus.FULL)
-                        chunkMap.schedule(holder, ChunkLevel.generationStatus(holder.getTicketLevel()));
-                    }
-                    fail(helper, "Chunk queued for loading", absolutePos, relativePos);
-                } else if (DEBUG_CHUNK_LOADING) {
-                    //Note: Even with debug logging enabled odds are this case isn't even possible due to the earlier check to skip if loaded
-                    Mekanism.logger.info("Trying to load already loaded chunk at: {}, {}", absolutePos.x, absolutePos.z);
+            ChunkHolder holder = distanceManager.getChunk(absPos);
+            if (holder == null) {
+                //If it is currently unloaded, queue it for loading
+                GameTestEventListeners.watchedChunks.put(absPos, false);
+                if (DEBUG_CHUNK_LOADING) {
+                    Mekanism.logger.info("Trying to load chunk at: {}, {}", absolutePos.x, absolutePos.z);
                 }
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                fail(helper, "Error loading chunk", absolutePos, relativePos);
+                //Load the chunk to the level it was unloaded at
+                holder = distanceManager.updateChunkScheduling(absPos, levelMemory.getValue(), null, UNLOAD_LEVEL);
+                if (holder == null) {//Should never happen unless start value was unloaded
+                    fail(helper, "Error loading chunk", absolutePos, relativePos);
+                } else {
+                    //And ensure we schedule it based on the status (in general this should be ChunkStatus.FULL)
+                    chunkMap.schedule(holder, ChunkLevel.generationStatus(holder.getTicketLevel()));
+                }
+                fail(helper, "Chunk queued for loading", absolutePos, relativePos);
+            } else if (DEBUG_CHUNK_LOADING) {
+                //Note: Even with debug logging enabled odds are this case isn't even possible due to the earlier check to skip if loaded
+                Mekanism.logger.info("Trying to load already loaded chunk at: {}, {}", absolutePos.x, absolutePos.z);
             }
         } else if (DEBUG_CHUNK_LOADING) {
             Mekanism.logger.info("Chunk at: {}, {} is already loaded", absolutePos.x, absolutePos.z);
@@ -175,19 +153,13 @@ public class GameTestUtils {
         long absPos = absolutePos.toLong();
         ChunkMap chunkMap = helper.getLevel().getChunkSource().chunkMap;
         DistanceManager distanceManager = chunkMap.getDistanceManager();
-        try {
-            ChunkHolder holder = (ChunkHolder) GET_CHUNK.invoke(distanceManager, absPos);
-            int oldLevel = holder == null ? UNLOAD_LEVEL : holder.getTicketLevel();
-            UPDATE_CHUNK_SCHEDULING.invoke(distanceManager, absPos, newLevel, holder, oldLevel);
-            //Note: We purposely don't unload or load it if that changed as this method is meant for use when we don't know if
-            // we are loading or unloading, and instead want to simulate the weird inaccessible but not unloaded state that
-            // vanilla can get chunks into
-            return oldLevel;
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            fail(helper, "Error adjusting chunk level", absolutePos, relativePos);
-        }
-        //Will not be hit as failing in the catch block throws an exception
-        return UNLOAD_LEVEL;
+        ChunkHolder holder = distanceManager.getChunk(absPos);
+        int oldLevel = holder == null ? UNLOAD_LEVEL : holder.getTicketLevel();
+        distanceManager.updateChunkScheduling(absPos, newLevel, holder, oldLevel);
+        //Note: We purposely don't unload or load it if that changed as this method is meant for use when we don't know if
+        // we are loading or unloading, and instead want to simulate the weird inaccessible but not unloaded state that
+        // vanilla can get chunks into
+        return oldLevel;
     }
 
     public static ChunkPos absolutePos(GameTestHelper helper, ChunkPos relativePos) {
