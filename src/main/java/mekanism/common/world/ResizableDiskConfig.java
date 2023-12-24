4714d711cb2254df4ddc28eb7069c7cbd0af676b
KdotJPG <KdotJPG@users.noreply.github.com>
2023-12-24 16:04:35 -0500

    Refactor salt disk feature to call vanilla placement method

--- src/main/java/mekanism/common/world/ResizableDiskConfig.java
+++ src/main/java/mekanism/common/world/ResizableDiskConfig.java
@@ -2,30 +2,30 @@ package mekanism.common.world;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import java.util.List;
 import java.util.function.IntSupplier;
 import mekanism.common.config.MekanismConfig;
 import mekanism.common.config.WorldConfig.SaltConfig;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.blockpredicates.BlockPredicate;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.RuleBasedBlockStateProvider;
 
 public class ResizableDiskConfig implements FeatureConfiguration {
 
     public static final Codec<ResizableDiskConfig> CODEC = RecordCodecBuilder.create(builder -> builder.group(
-          BlockState.CODEC.fieldOf("state").forGetter(config -> config.state)
-    ).apply(builder, state -> new ResizableDiskConfig(state, MekanismConfig.world.salt)));
+            RuleBasedBlockStateProvider.CODEC.fieldOf("state_provider").forGetter(config -> config.stateProvider)
+    ).apply(builder, stateProvider -> new ResizableDiskConfig(stateProvider, MekanismConfig.world.salt)));
 
-    public final BlockState state;
+    public final RuleBasedBlockStateProvider stateProvider;
     public final IntProvider radius;
     public final IntSupplier halfHeight;
-    public final List<BlockState> targets;
+    public final BlockPredicate target;
 
-    public ResizableDiskConfig(BlockState state, SaltConfig saltConfig) {
-        this.state = state;
+    public ResizableDiskConfig(RuleBasedBlockStateProvider stateProvider, SaltConfig saltConfig) {
+        this.stateProvider = stateProvider;
         this.radius = ConfigurableUniformInt.SALT;
         this.halfHeight = saltConfig.halfHeight;
-        this.targets = List.of(Blocks.DIRT.defaultBlockState(), Blocks.CLAY.defaultBlockState(), this.state);
+        this.target = BlockPredicate.matchesBlocks(Blocks.DIRT, Blocks.CLAY);
     }
 }
--- src/main/java/mekanism/common/world/ResizableDiskReplaceFeature.java
+++ src/main/java/mekanism/common/world/ResizableDiskReplaceFeature.java
@@ -1,16 +1,11 @@
 package mekanism.common.world;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.core.BlockPos;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.configurations.DiskConfiguration;
 
-//Modified copy of vanilla's DiskReplaceFeature/BaseDiskFeature but to support ResizableSphereReplaceConfig
-// and without support for falling blocks
+// Wrapper for vanilla's DiskReplaceFeature to support ResizableDiskConfig.halfHeight (mekanism config IntSupplier)
 public class ResizableDiskReplaceFeature extends Feature<ResizableDiskConfig> {
 
     public ResizableDiskReplaceFeature(Codec<ResizableDiskConfig> codec) {
@@ -19,38 +14,21 @@ public class ResizableDiskReplaceFeature extends Feature<ResizableDiskConfig> {
 
     @Override
     public boolean place(FeaturePlaceContext<ResizableDiskConfig> context) {
-        boolean placed = false;
-        BlockPos pos = context.origin();
-        WorldGenLevel world = context.level();
-        if (world.getFluidState(pos).is(FluidTags.WATER)) {
-            ResizableDiskConfig config = context.config();
-            int halfHeight = config.halfHeight.getAsInt();
-            int yMax = pos.getY() + halfHeight;
-            int yMin = pos.getY() - halfHeight - 1;
-            int radius = config.radius.sample(context.random());
-            int radiusSquared = radius * radius;
-            for (int x = pos.getX() - radius; x <= pos.getX() + radius; ++x) {
-                int xRadius = x - pos.getX();
-                int xRadiusSquared = xRadius * xRadius;
-                for (int z = pos.getZ() - radius; z <= pos.getZ() + radius; ++z) {
-                    int zRadius = z - pos.getZ();
-                    if (xRadiusSquared + zRadius * zRadius <= radiusSquared) {
-                        for (int y = yMax; y > yMin; --y) {
-                            BlockPos targetPos = new BlockPos(x, y, z);
-                            Block blockAtTarget = world.getBlockState(targetPos).getBlock();
-                            for (BlockState target : config.targets) {
-                                if (target.is(blockAtTarget)) {
-                                    world.setBlock(targetPos, config.state, Block.UPDATE_CLIENTS);
-                                    markAboveForPostProcessing(world, targetPos);
-                                    placed = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return placed;
+        ResizableDiskConfig config = context.config();
+        DiskConfiguration vanillaConfig = new DiskConfiguration(
+                config.stateProvider,
+                config.target,
+                config.radius,
+                config.halfHeight.getAsInt()
+        );
+        FeaturePlaceContext<DiskConfiguration> vanillaContext = new FeaturePlaceContext<>(
+                context.topFeature(),
+                context.level(),
+                context.chunkGenerator(),
+                context.random(),
+                context.origin(),
+                vanillaConfig
+        );
+        return Feature.DISK.place(vanillaContext);
     }
 }
