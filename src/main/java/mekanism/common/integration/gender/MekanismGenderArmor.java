d3ea1977de657bec65ab95a29e8c60c554c94d06
Sara Freimer <sara@freimer.com>
2024-02-25 18:16:46 -0600

    Prep for upcoming extra method in IGenderArmor

--- src/main/java/mekanism/common/integration/gender/MekanismGenderArmor.java
+++ src/main/java/mekanism/common/integration/gender/MekanismGenderArmor.java
@@ -7,19 +7,23 @@ import net.minecraft.world.item.ArmorItem;
 import net.neoforged.neoforge.capabilities.ItemCapability;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
-public record MekanismGenderArmor(boolean coversBreasts, boolean alwaysHidesBreasts, float physicsResistance, float tightness) implements IGenderArmor {
+public record MekanismGenderArmor(boolean coversBreasts, boolean alwaysHidesBreasts, float physicsResistance, float tightness, boolean armorStandsCopySettings) implements IGenderArmor {
 
     private static final ItemCapability<IGenderArmor, Void> GENDER_ARMOR_CAPABILITY = ItemCapability.createVoid(new ResourceLocation(MekanismHooks.WILDFIRE_GENDER_MOD_ID, "gender_armor"), IGenderArmor.class);
-    public static final MekanismGenderArmor OPEN_FRONT = new MekanismGenderArmor(false, false, 0, 0);
-    public static final MekanismGenderArmor HIDES_BREASTS = new MekanismGenderArmor(true, true, 0, 0);
-    static final MekanismGenderArmor HAZMAT = new MekanismGenderArmor(0.5F, 0.25F);
+    public static final MekanismGenderArmor OPEN_FRONT = new MekanismGenderArmor(false, false, 0, 0, false);
+    public static final MekanismGenderArmor HIDES_BREASTS = new MekanismGenderArmor(true, true, 0, 0, false);
+    static final MekanismGenderArmor HAZMAT = new MekanismGenderArmor(0.5F, 0.25F, false);
 
     public MekanismGenderArmor(float physicsResistance) {
         this(physicsResistance, 0);
     }
 
     public MekanismGenderArmor(float physicsResistance, float tightness) {
-        this(true, false, physicsResistance, tightness);
+        this(physicsResistance, tightness, true);
+    }
+
+    public MekanismGenderArmor(float physicsResistance, float tightness, boolean armorStandsCopySettings) {
+        this(true, false, physicsResistance, tightness, armorStandsCopySettings);
     }
 
     public MekanismGenderArmor {
