a8188bd14087597786ccdd06368960c69d07ef8f
Sara Freimer <sara@freimer.com>
2024-05-04 11:41:14 -0500

    Simplify stream codec for qio item viewer sync

--- src/main/java/mekanism/common/network/to_client/qio/PacketQIOItemViewerGuiSync.java
+++ src/main/java/mekanism/common/network/to_client/qio/PacketQIOItemViewerGuiSync.java
@@ -3,10 +3,11 @@ package mekanism.common.network.to_client.qio;
 import com.mojang.datafixers.util.Function3;
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
+import java.util.Optional;
+import mekanism.common.lib.inventory.HashedItem;
 import mekanism.common.lib.inventory.HashedItem.UUIDAwareHashedItem;
 import mekanism.common.network.IMekanismPacket;
-import mekanism.common.network.PacketUtils;
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
@@ -17,20 +18,20 @@ import net.minecraft.world.item.ItemStack;
 // we can also send a smaller packet to each client until they disconnect and then we clear what packets they know
 public abstract class PacketQIOItemViewerGuiSync implements IMekanismPacket {
 
+    private static final StreamCodec<RegistryFriendlyByteBuf, Object2LongMap<UUIDAwareHashedItem>> ITEM_MAP_CODEC = ByteBufCodecs.map(Object2LongOpenHashMap::new,
+          StreamCodec.composite(
+                ItemStack.STREAM_CODEC, HashedItem::getInternalStack,
+                ByteBufCodecs.optional(UUIDUtil.STREAM_CODEC), item -> Optional.ofNullable(item.getUUID()),
+                (stack, uuid) -> new UUIDAwareHashedItem(stack, uuid.orElse(null))
+          ), ByteBufCodecs.VAR_LONG
+    );
+
     protected static <PKT extends PacketQIOItemViewerGuiSync> StreamCodec<RegistryFriendlyByteBuf, PKT> streamCodec(
           Function3<Long, Integer, Object2LongMap<UUIDAwareHashedItem>, PKT> constructor) {
         return StreamCodec.composite(
               ByteBufCodecs.VAR_LONG, pkt -> pkt.countCapacity,
               ByteBufCodecs.VAR_INT, pkt -> pkt.typeCapacity,
-              StreamCodec.of(
-                    (buffer, itemMap) -> buffer.writeMap(itemMap, (buf, item) -> {
-                        ItemStack.STREAM_CODEC.encode(buffer, item.getInternalStack());
-                        //Shouldn't be null unless something failed, but if it does try to handle it relatively gracefully
-                        buf.writeNullable(item.getUUID(), (b, u) -> b.writeUUID(u));
-                    }, FriendlyByteBuf::writeVarLong),
-                    buffer -> buffer.readMap(Object2LongOpenHashMap::new, buf -> new UUIDAwareHashedItem(ItemStack.STREAM_CODEC.decode(buffer),
-                          buf.readNullable(b -> b.readUUID())), FriendlyByteBuf::readVarLong)
-              ), pkt -> pkt.itemMap,
+              ITEM_MAP_CODEC, pkt -> pkt.itemMap,
               constructor
         );
     }
