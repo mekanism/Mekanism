mekanism/Mekanism

commit f5d7a1e671cefd52e94855858dc7647ceb88d1c5

Pause recipe processing if there are errors that stop it from being able to be processed. Should make backed up machines a bit more performant

src/main/java/mekanism/common/content/evaporation/EvaporationMultiblockData.java
@@ -112,7 +112,10 @@ public class EvaporationMultiblockData extends MultiblockData implements IValveH
         //Default biome temp to the ambient temperature at the block we are at
         biomeAmbientTemp = HeatAPI.getAmbientTemp(tile.getLevel(), tile.getBlockPos());
         fluidTanks.add(inputTank = VariableCapacityFluidTank.input(this, this::getMaxFluid, this::containsRecipe, createSaveAndComparator(recipeCacheLookupMonitor)));
-        fluidTanks.add(outputTank = VariableCapacityFluidTank.output(this, MekanismConfig.general.evaporationOutputTankCapacity, BasicFluidTank.alwaysTrue, this));
+        fluidTanks.add(outputTank = VariableCapacityFluidTank.output(this, MekanismConfig.general.evaporationOutputTankCapacity, BasicFluidTank.alwaysTrue, () -> {
+            onContentsChanged();
+            recipeCacheLookupMonitor.unpause();
+        }));
         inputHandler = InputHelper.getInputHandler(inputTank, RecipeError.NOT_ENOUGH_INPUT);
         outputHandler = OutputHelper.getOutputHandler(outputTank, RecipeError.NOT_ENOUGH_OUTPUT_SPACE);
         inventorySlots.add(inputInputSlot = FluidInventorySlot.fill(inputTank, this, 28, 20));

src/api/java/mekanism/api/recipes/cache/CachedRecipe.java
@@ -35,6 +35,12 @@ public abstract class CachedRecipe<RECIPE extends MekanismRecipe> {
      */
     private Set<RecipeError> errors = Collections.emptySet();
     /**
+     * Tracks whether recipe processing is currently paused due to errors.
+     *
+     * @since 10.5.15
+     */
+    private boolean pausedForErrors = false;
+    /**
      * Used to check if the {@link CachedRecipe} should recheck for all errors.
      */
     private final BooleanSupplier recheckAllErrors;
@@ -265,8 +271,22 @@ public abstract class CachedRecipe<RECIPE extends MekanismRecipe> {
         //Validate the errors actually changed as they potentially are just the same ones we found last go around
         if (!this.errors.equals(errors)) {
             this.errors = errors;
+            if (this.errors.size() > 1) {
+                pausedForErrors = true;
+            } else {
+                pausedForErrors = !this.errors.contains(RecipeError.NOT_ENOUGH_ENERGY_REDUCED_RATE);
+            }
             onErrorsChange.accept(errors);
         }
+    }
+
+    /**
+     * Unpauses any recipes that got paused due to errors that cause processing to stop.
+     *
+     * @since 10.5.15
+     */
+    public void unpauseErrors() {
+        pausedForErrors = false;
     }
 
     /**
@@ -286,6 +306,11 @@ public abstract class CachedRecipe<RECIPE extends MekanismRecipe> {
     public void process() {
         //TODO: Evaluate adding in some marker that gets set to true here that then denies the various callbacks/builders from being used
         // as once we start processing the cached recipe should basically be immutable
+        if (pausedForErrors) {
+            //Note: We just set active as false if we are paused for errors and then don't do any other processing
+            setActive.accept(false);
+            return;
+        }
         int operations;
         if (canHolderFunction.getAsBoolean()) {
             setupVariableValues();

src/main/java/mekanism/common/entity/EntityRobit.java
@@ -14,6 +14,7 @@ import java.util.function.BooleanSupplier;
 import java.util.function.Function;
 import mekanism.api.Action;
 import mekanism.api.AutomationType;
+import mekanism.api.IContentsListener;
 import mekanism.api.MekanismAPI;
 import mekanism.api.NBTConstants;
 import mekanism.api.energy.IEnergyContainer;
@@ -206,7 +207,11 @@ public class EntityRobit extends PathfinderMob implements IRobit, IMekanismInven
         // recipe the same tick and thus create uneven spikes of CPU usage
         int checkOffset = level().random.nextInt(TileEntityRecipeMachine.RECIPE_CHECK_FREQUENCY);
         recheckAllRecipeErrors = () -> !playersUsing.isEmpty() && level().getGameTime() % TileEntityRecipeMachine.RECIPE_CHECK_FREQUENCY == checkOffset;
-        energyContainers = Collections.singletonList(energyContainer = BasicEnergyContainer.input(MAX_ENERGY, this));
+        IContentsListener recipeCacheUnpauseListener = () -> {
+            onContentsChanged();
+            recipeCacheLookupMonitor.unpause();
+        };
+        energyContainers = Collections.singletonList(energyContainer = BasicEnergyContainer.input(MAX_ENERGY, recipeCacheUnpauseListener));
 
         inventorySlots = new ArrayList<>();
         inventoryContainerSlots = new ArrayList<>();
@@ -221,7 +226,7 @@ public class EntityRobit extends PathfinderMob implements IRobit, IMekanismInven
         inventorySlots.add(smeltingInputSlot = InputInventorySlot.at(this::containsRecipe, recipeCacheLookupMonitor, 51, 35));
         //TODO: Previously used FurnaceResultSlot, check if we need to replicate any special logic it had (like if it had xp logic or something)
         // Yes we probably do want this to allow for experience. Though maybe we should allow for experience for all our recipes/smelting recipes? V10
-        inventorySlots.add(smeltingOutputSlot = OutputInventorySlot.at(this, 116, 35));
+        inventorySlots.add(smeltingOutputSlot = OutputInventorySlot.at(recipeCacheUnpauseListener, 116, 35));
         smeltingInputSlot.tracksWarnings(slot -> slot.warning(WarningType.NO_MATCHING_RECIPE, getWarningCheck(RecipeError.NOT_ENOUGH_INPUT)));
         smeltingOutputSlot.tracksWarnings(slot -> slot.warning(WarningType.NO_SPACE_IN_OUTPUT, getWarningCheck(RecipeError.NOT_ENOUGH_OUTPUT_SPACE)));
 

src/main/java/mekanism/common/recipe/lookup/monitor/RecipeCacheLookupMonitor.java
@@ -18,6 +18,7 @@ public class RecipeCacheLookupMonitor<RECIPE extends MekanismRecipe> implements 
     protected final int cacheIndex;
     protected CachedRecipe<RECIPE> cachedRecipe;
     protected boolean hasNoRecipe;
+    protected boolean shouldUnpause;
 
     public RecipeCacheLookupMonitor(IRecipeLookupHandler<RECIPE> handler) {
         this(handler, 0);
@@ -41,6 +42,11 @@ public class RecipeCacheLookupMonitor<RECIPE extends MekanismRecipe> implements 
     public void onChange() {
         //Mark that we may have a recipe again
         hasNoRecipe = false;
+        unpause();
+    }
+
+    public void unpause() {
+        shouldUnpause = true;
     }
 
     /**
@@ -65,6 +71,10 @@ public class RecipeCacheLookupMonitor<RECIPE extends MekanismRecipe> implements 
             handler.onCachedRecipeChanged(cachedRecipe, cacheIndex);
         }
         if (cachedRecipe != null) {
+            if (shouldUnpause) {
+                shouldUnpause = false;
+                cachedRecipe.unpauseErrors();
+            }
             cachedRecipe.process();
             return true;
         }
@@ -76,8 +86,8 @@ public class RecipeCacheLookupMonitor<RECIPE extends MekanismRecipe> implements 
         if (cachedIndexMatches(cacheIndex)) {
             ICachedRecipeHolder.super.loadSavedData(cached, cacheIndex);
             if (cached instanceof ItemStackConstantChemicalToItemStackCachedRecipe<?, ?, ?, ?> c &&
-                handler instanceof IRecipeLookupHandler.ConstantUsageRecipeLookupHandler handler) {
-                c.loadSavedUsageSoFar(handler.getSavedUsedSoFar(cacheIndex));
+                handler instanceof IRecipeLookupHandler.ConstantUsageRecipeLookupHandler lookupHandler) {
+                c.loadSavedUsageSoFar(lookupHandler.getSavedUsedSoFar(cacheIndex));
             }
         }
     }

src/main/java/mekanism/common/tile/factory/TileEntityFactory.java
@@ -178,7 +178,12 @@ public abstract class TileEntityFactory<RECIPE extends MekanismRecipe> extends T
     @Override
     protected IEnergyContainerHolder getInitialEnergyContainers(IContentsListener listener) {
         EnergyContainerHelper builder = EnergyContainerHelper.forSideWithConfig(this::getDirection, this::getConfig);
-        builder.addContainer(energyContainer = MachineEnergyContainer.input(this, listener));
+        builder.addContainer(energyContainer = MachineEnergyContainer.input(this, () -> {
+            listener.onContentsChanged();
+            for (FactoryRecipeCacheLookupMonitor<RECIPE> cacheLookupMonitor : recipeCacheLookupMonitors) {
+                cacheLookupMonitor.unpause();
+            }
+        }));
         return builder.build();
     }
 

src/main/java/mekanism/common/tile/factory/TileEntityItemToItemFactory.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import mekanism.api.IContentsListener;
 import mekanism.api.providers.IBlockProvider;
 import mekanism.api.recipes.MekanismRecipe;
+import mekanism.api.recipes.SawmillRecipe;
 import mekanism.api.recipes.cache.CachedRecipe.OperationTracker.RecipeError;
 import mekanism.api.recipes.inputs.IInputHandler;
 import mekanism.api.recipes.inputs.InputHelper;
@@ -14,6 +15,7 @@ import mekanism.common.capabilities.holder.slot.InventorySlotHelper;
 import mekanism.common.inventory.slot.FactoryInputInventorySlot;
 import mekanism.common.inventory.slot.OutputInventorySlot;
 import mekanism.common.inventory.warning.WarningTracker.WarningType;
+import mekanism.common.recipe.lookup.monitor.FactoryRecipeCacheLookupMonitor;
 import mekanism.common.tier.FactoryTier;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
@@ -38,7 +40,12 @@ public abstract class TileEntityItemToItemFactory<RECIPE extends MekanismRecipe>
         int baseXMult = tier == FactoryTier.BASIC ? 38 : tier == FactoryTier.ADVANCED ? 26 : 19;
         for (int i = 0; i < tier.processes; i++) {
             int xPos = baseX + (i * baseXMult);
-            OutputInventorySlot outputSlot = OutputInventorySlot.at(updateSortingListener, xPos, 57);
+            FactoryRecipeCacheLookupMonitor<RECIPE> lookupMonitor = recipeCacheLookupMonitors[i];
+            IContentsListener updateSortingAndUnpause = () -> {
+                updateSortingListener.onContentsChanged();
+                lookupMonitor.unpause();
+            };
+            OutputInventorySlot outputSlot = OutputInventorySlot.at(updateSortingAndUnpause, xPos, 57);
             //Note: As we are an item factory that has comparator's based on items we can just use the monitor as a listener directly
             FactoryInputInventorySlot inputSlot = FactoryInputInventorySlot.create(this, i, outputSlot, recipeCacheLookupMonitors[i], xPos, 13);
             int index = i;

src/main/java/mekanism/common/tile/factory/TileEntitySawingFactory.java
@@ -28,6 +28,7 @@ import mekanism.common.recipe.IMekanismRecipeTypeProvider;
 import mekanism.common.recipe.MekanismRecipeType;
 import mekanism.common.recipe.lookup.ISingleRecipeLookupHandler.ItemRecipeLookupHandler;
 import mekanism.common.recipe.lookup.cache.InputRecipeCache.SingleItem;
+import mekanism.common.recipe.lookup.monitor.FactoryRecipeCacheLookupMonitor;
 import mekanism.common.tier.FactoryTier;
 import mekanism.common.tile.machine.TileEntityPrecisionSawmill;
 import mekanism.common.upgrade.IUpgradeData;
@@ -68,10 +69,15 @@ public class TileEntitySawingFactory extends TileEntityFactory<SawmillRecipe> im
         int baseXMult = tier == FactoryTier.BASIC ? 38 : tier == FactoryTier.ADVANCED ? 26 : 19;
         for (int i = 0; i < tier.processes; i++) {
             int xPos = baseX + (i * baseXMult);
-            OutputInventorySlot outputSlot = OutputInventorySlot.at(updateSortingListener, xPos, 57);
-            OutputInventorySlot secondaryOutputSlot = OutputInventorySlot.at(updateSortingListener, xPos, 77);
+            FactoryRecipeCacheLookupMonitor<SawmillRecipe> lookupMonitor = recipeCacheLookupMonitors[i];
+            IContentsListener updateSortingAndUnpause = () -> {
+                updateSortingListener.onContentsChanged();
+                lookupMonitor.unpause();
+            };
+            OutputInventorySlot outputSlot = OutputInventorySlot.at(updateSortingAndUnpause, xPos, 57);
+            OutputInventorySlot secondaryOutputSlot = OutputInventorySlot.at(updateSortingAndUnpause, xPos, 77);
             //Note: As we are an item factory that has comparator's based on items we can just use the monitor as a listener directly
-            FactoryInputInventorySlot inputSlot = FactoryInputInventorySlot.create(this, i, outputSlot, secondaryOutputSlot, recipeCacheLookupMonitors[i], xPos, 13);
+            FactoryInputInventorySlot inputSlot = FactoryInputInventorySlot.create(this, i, outputSlot, secondaryOutputSlot, lookupMonitor, xPos, 13);
             int index = i;
             builder.addSlot(inputSlot).tracksWarnings(slot -> slot.warning(WarningType.NO_MATCHING_RECIPE, getWarningCheck(RecipeError.NOT_ENOUGH_INPUT, index)));
             builder.addSlot(outputSlot).tracksWarnings(slot -> slot.warning(WarningType.NO_SPACE_IN_OUTPUT, getWarningCheck(RecipeError.NOT_ENOUGH_OUTPUT_SPACE, index)));

src/main/java/mekanism/common/tile/machine/TileEntityAntiprotonicNucleosynthesizer.java
@@ -109,7 +109,7 @@ public class TileEntityAntiprotonicNucleosynthesizer extends TileEntityProgressM
 
     @NotNull
     @Override
-    public IChemicalTankHolder<Gas, GasStack, IGasTank> getInitialGasTanks(IContentsListener listener, IContentsListener recipeCacheListener) {
+    public IChemicalTankHolder<Gas, GasStack, IGasTank> getInitialGasTanks(IContentsListener listener, IContentsListener recipeCacheListener, IContentsListener recipeCacheUnpauseListener) {
         ChemicalTankHelper<Gas, GasStack, IGasTank> builder = ChemicalTankHelper.forSideGasWithConfig(this::getDirection, this::getConfig);
         builder.addTank(gasTank = ChemicalTankBuilder.GAS.input(MAX_GAS, gas -> containsRecipeBA(inputSlot.getStack(), gas), this::containsRecipeB, recipeCacheListener));
         return builder.build();
@@ -117,20 +117,20 @@ public class TileEntityAntiprotonicNucleosynthesizer extends TileEntityProgressM
 
     @NotNull
     @Override
-    protected IEnergyContainerHolder getInitialEnergyContainers(IContentsListener listener, IContentsListener recipeCacheListener) {
+    protected IEnergyContainerHolder getInitialEnergyContainers(IContentsListener listener, IContentsListener recipeCacheListener, IContentsListener recipeCacheUnpauseListener) {
         EnergyContainerHelper builder = EnergyContainerHelper.forSideWithConfig(this::getDirection, this::getConfig);
-        builder.addContainer(energyContainer = MachineEnergyContainer.input(this, listener));
+        builder.addContainer(energyContainer = MachineEnergyContainer.input(this, recipeCacheUnpauseListener));
         return builder.build();
     }
 
     @NotNull
     @Override
-    protected IInventorySlotHolder getInitialInventory(IContentsListener listener, IContentsListener recipeCacheListener) {
+    protected IInventorySlotHolder getInitialInventory(IContentsListener listener, IContentsListener recipeCacheListener, IContentsListener recipeCacheUnpauseListener) {
         InventorySlotHelper builder = InventorySlotHelper.forSideWithConfig(this::getDirection, this::getConfig);
         builder.addSlot(gasInputSlot = GasInventorySlot.fillOrConvert(gasTank, this::getLevel, listener, 6, 69));
         builder.addSlot(inputSlot = InputInventorySlot.at(item -> containsRecipeAB(item, gasTank.getStack()), this::containsRecipeA, recipeCacheListener, 26, 40))
               .tracksWarnings(slot -> slot.warning(WarningType.NO_MATCHING_RECIPE, getWarningCheck(RecipeError.NOT_ENOUGH_INPUT)));
-        builder.addSlot(outputSlot = OutputInventorySlot.at(listener, 152, 40))
+        builder.addSlot(outputSlot = OutputInventorySlot.at(recipeCacheUnpauseListener, 152, 40))
               .tracksWarnings(slot -> slot.warning(WarningType.NO_SPACE_IN_OUTPUT, getWarningCheck(RecipeError.NOT_ENOUGH_OUTPUT_SPACE)));
         builder.addSlot(energySlot = EnergyInventorySlot.fillOrConvert(energyContainer, this::getLevel, listener, 173, 69));
         gasInputSlot.setSlotOverlay(SlotOverlay.MINUS);

commit 30c1b84d91ede5435f74a8768dd2b86536990f4c

fix TEPs getting stuck paused when full tank is drained when temp lower than min production (caused by f5d7a1e671cefd52e94855858dc7647ceb88d1c5)

src/main/java/mekanism/common/content/evaporation/EvaporationMultiblockData.java
@@ -116,10 +116,7 @@ public class EvaporationMultiblockData extends MultiblockData implements IValveH
         //Default biome temp to the ambient temperature at the block we are at
         biomeAmbientTemp = HeatAPI.getAmbientTemp(tile.getLevel(), tile.getBlockPos());
         fluidTanks.add(inputTank = VariableCapacityFluidTank.input(this, this::getMaxFluid, this::containsRecipe, createSaveAndComparator(recipeCacheLookupMonitor)));
-        fluidTanks.add(outputTank = VariableCapacityFluidTank.output(this, MekanismConfig.general.evaporationOutputTankCapacity, BasicFluidTank.alwaysTrue, () -> {
-            onContentsChanged();
-            recipeCacheLookupMonitor.unpause();
-        }));
+        fluidTanks.add(outputTank = VariableCapacityFluidTank.output(this, MekanismConfig.general.evaporationOutputTankCapacity, BasicFluidTank.alwaysTrue, this));
         inputHandler = InputHelper.getInputHandler(inputTank, RecipeError.NOT_ENOUGH_INPUT);
         outputHandler = OutputHelper.getOutputHandler(outputTank, RecipeError.NOT_ENOUGH_OUTPUT_SPACE);
         inventorySlots.add(inputInputSlot = FluidInventorySlot.fill(inputTank, this, 28, 20));
@@ -138,6 +135,14 @@ public class EvaporationMultiblockData extends MultiblockData implements IValveH
         // update the heat capacity now that we've read
         heatCapacitor.setHeatCapacity(MekanismConfig.general.evaporationHeatCapacity.get() * height(), true);
         updateSolars(world);
+    }
+
+    @Override
+    public void onContentsChanged() {
+        super.onContentsChanged();
+        if (getTemperature() > HeatAPI.AMBIENT_TEMP) {
+            recipeCacheLookupMonitor.unpause();
+        }
     }
 
     @Override