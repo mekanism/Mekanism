da253948ecf1b8544c9c2b88b55ecb2932ed2f60
Thiakil <thiakil@users.noreply.github.com>
2025-05-07 20:49:43 +0800

    more test

--- src/test/java/mekanism/common/lib/collection/BiMultiLongMapTest.java
+++ src/test/java/mekanism/common/lib/collection/BiMultiLongMapTest.java
@@ -1,7 +1,7 @@
 package mekanism.common.lib.collection;
 
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,17 +13,57 @@ public class BiMultiLongMapTest {
         map.put(1, first);
         map.putAll(new long[]{2, 3}, first);
 
+        Assertions.assertIterableEquals(List.of(1L, 2L, 3L), map.getAllKeys());
+
         for (long i = 1; i < 4; i++) {
             Assertions.assertIterableEquals(Collections.singleton(first), map.getValues(i));
         }
 
-        Assertions.assertIterableEquals(Arrays.asList(1L, 2L, 3L), map.getKeys(first));
+        Assertions.assertIterableEquals(List.of(1L, 2L, 3L), map.getKeys(first));
 
         map.removeKey(3);
-        Assertions.assertIterableEquals(Arrays.asList(1L, 2L), map.getKeys(first));
+        Assertions.assertIterableEquals(List.of(1L, 2L), map.getAllKeys());
+        Assertions.assertIterableEquals(List.of(1L, 2L), map.getKeys(first));
         Assertions.assertNull(map.getValues(3));
 
         map.removeValue(first);
         Assertions.assertTrue(map.getAllKeys().isEmpty());
+    }
+
+    @Test
+    void testMultipleValues() {
+        BiMultiLongmap<String> map = new BiMultiLongmap<>();
+        String first = "first";
+        String second = "second";
+        String third = "third";
+        map.putAll(new long[]{1, 2}, first);
+        map.putAll(new long[]{2, 3}, second);
+        map.putAll(new long[]{3, 4}, third);
+
+        Assertions.assertIterableEquals(List.of(1L, 2L, 3L, 4L), map.getAllKeys());
+        Assertions.assertIterableEquals(List.of(first), map.getValues(1));
+        Assertions.assertIterableEquals(List.of(first, second), map.getValues(2).stream().sorted().toList());
+        Assertions.assertIterableEquals(List.of(second, third), map.getValues(3).stream().sorted().toList());
+        Assertions.assertIterableEquals(List.of(third), map.getValues(4));
+        Assertions.assertIterableEquals(List.of(1L, 2L), map.getKeys(first));
+        Assertions.assertIterableEquals(List.of(2L, 3L), map.getKeys(second));
+        Assertions.assertIterableEquals(List.of(3L, 4L), map.getKeys(third));
+
+        map.remove(1, third);
+        Assertions.assertIterableEquals(List.of(1L, 2L, 3L, 4L), map.getAllKeys(), "no change expected");
+
+        map.remove(4, third);
+        Assertions.assertIterableEquals(List.of(1L, 2L, 3L), map.getAllKeys(), "expected 4 gone");
+        Assertions.assertIterableEquals(List.of(3L), map.getKeys(third));
+        Assertions.assertNull(map.getValues(4));
+
+        map.removeValue(second);
+        Assertions.assertIterableEquals(List.of(1L, 2L, 3L), map.getAllKeys(), "same keys");
+        Assertions.assertIterableEquals(List.of(first), map.getValues(2).stream().sorted().toList());
+        Assertions.assertIterableEquals(List.of(third), map.getValues(3).stream().sorted().toList());
+        Assertions.assertNull(map.getKeys(second));
+
+        map.remove(3, third);
+        Assertions.assertIterableEquals(List.of(1L, 2L), map.getAllKeys(), "expected 3 gone");
     }
 }
