a1d14f5fb54b0fc6a12d2cbe2ae4bc3ca310570a
ちーず(･8･)けーき <31585494+MineCake147E@users.noreply.github.com>
2025-09-06 13:42:20 +0900

    * `TileEntityInternalMultiblock.onNeighborChange` no longer uses bitwise math tricks to validate the `neighborPos`. * `TileEntityInternalMultiblock.onNeighborChange` now uses nested `if` statements again.

--- src/main/java/mekanism/common/tile/prefab/TileEntityInternalMultiblock.java
+++ src/main/java/mekanism/common/tile/prefab/TileEntityInternalMultiblock.java
@@ -61,23 +61,25 @@ public class TileEntityInternalMultiblock extends TileEntityMekanism implements 
     @Override
     public void onNeighborChange(Block block, BlockPos neighborPos) {
         super.onNeighborChange(block, neighborPos);
-        //TODO - V11: Make this properly support changing blocks inside the structure when they aren't touching any part of the multiblocks
-        //Note: We handle when an internal multiblock is removed that isn't touching anything in BlockMekanism#onRemove
-        if (level == null || multiblock == null || isRemote()) return;
-        //If the neighbor change happened to a block inside a multiblock, and it isn't a block that is part of the multiblock
-        if (!level.isEmptyBlock(neighborPos) && multiblock.isKnownLocation(neighborPos)) return;
-        //If the neighbor change happened to a block outside a multiblock, and it isn't a block that could be part of the multiblock
+        //Check if the neighborPos is really a neighbor of this block
         //Note: This is necessary to eliminate unnecessary structure recheck
         // caused by redstone optimization mods like Alternate Current reporting the different neighborPos from Vanilla implementation.
-        int dX = Math.abs(neighborPos.getX() - worldPosition.getX());
-        int dY = Math.abs(neighborPos.getY() - worldPosition.getY());
-        int dZ = Math.abs(neighborPos.getZ() - worldPosition.getZ());
-        if (((dX | dY | dZ) >>> 1) > 0) return;
-        //And we are not already an internal part of the structure, or we are changing an internal part to air
-        // then we mark the structure as needing to be re-validated
-        //Note: This isn't a super accurate check as if a node gets replaced by command or mod with say dirt
-        // it won't know to invalidate it but oh well. (See java docs on internalLocations for more caveats)
-        multiblock.recheckStructure = true;
+        boolean fX = Integer.toUnsignedLong(Math.abs(neighborPos.getX() - worldPosition.getX())) > 1;
+        boolean fY = Integer.toUnsignedLong(Math.abs(neighborPos.getY() - worldPosition.getY())) > 1;
+        boolean fZ = Integer.toUnsignedLong(Math.abs(neighborPos.getZ() - worldPosition.getZ())) > 1;
+        if (fX | fY | fZ) return;
+        //TODO - V11: Make this properly support changing blocks inside the structure when they aren't touching any part of the multiblocks
+        //Note: We handle when an internal multiblock is removed that isn't touching anything in BlockMekanism#onRemove
+        if (level != null && multiblock != null && !isRemote()) {
+            //If the neighbor change happened to a block inside a multiblock, and it isn't a block that is part of the multiblock
+            if (level.isEmptyBlock(neighborPos) || !multiblock.isKnownLocation(neighborPos)) {
+                //And we are not already an internal part of the structure, or we are changing an internal part to air
+                // then we mark the structure as needing to be re-validated
+                //Note: This isn't a super accurate check as if a node gets replaced by command or mod with say dirt
+                // it won't know to invalidate it but oh well. (See java docs on internalLocations for more caveats)
+                multiblock.recheckStructure = true;
+            }
+        }
     }
 
     @Override
