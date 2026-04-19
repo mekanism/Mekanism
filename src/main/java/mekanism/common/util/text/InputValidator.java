958d6e164ddc3681ed78c63ec0c9c9467216fbd7
Thiakil <thiakil@users.noreply.github.com>
2026-04-19 15:57:55 +0800

    Continue working on fixing compile errors

--- src/main/java/mekanism/common/util/text/InputValidator.java
+++ src/main/java/mekanism/common/util/text/InputValidator.java
@@ -2,7 +2,7 @@ package mekanism.common.util.text;
 
 import it.unimi.dsi.fastutil.chars.CharOpenHashSet;
 import it.unimi.dsi.fastutil.chars.CharSet;
-import mekanism.api.functions.CharPredicate;
+import java.util.function.IntPredicate;
 import net.minecraft.resources.Identifier;
 
 public class InputValidator {
@@ -10,36 +10,37 @@ public class InputValidator {
     private InputValidator() {
     }
 
-    public static final CharPredicate ALL = c -> true;
-    public static final CharPredicate DIGIT = c -> c >= '0' && c <= '9';
-    public static final CharPredicate DIGIT_OR_NEGATIVE = DIGIT.or(c -> c == '-');
-    public static final CharPredicate LETTER = c -> (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-    public static final CharPredicate LETTER_OR_DIGIT = LETTER.or(DIGIT);
+    //int == codepoint - note that for Character.isBmpCodePoint(c) == true then the integer value is the same as char
+    public static final IntPredicate ALL = c -> true;
+    public static final IntPredicate DIGIT = c -> c >= '0' && c <= '9';
+    public static final IntPredicate DIGIT_OR_NEGATIVE = DIGIT.or(c -> c == '-');
+    public static final IntPredicate LETTER = c -> (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    public static final IntPredicate LETTER_OR_DIGIT = LETTER.or(DIGIT);
     //Note: This is stricter than StringUtil#isValidPlayerName, but follows what is valid based on mojang's support site
-    public static final CharPredicate USERNAME = LETTER_OR_DIGIT.or(c -> c == '_');
-    public static final CharPredicate RL_NAMESPACE = Identifier::validNamespaceChar;
-    public static final CharPredicate RL_PATH = Identifier::validPathChar;
-    public static final CharPredicate RESOURCE_LOCATION = Identifier::isAllowedInIdentifier;
-    public static final CharPredicate DECIMAL = DIGIT.or(from('.'));
-    public static final CharPredicate SCI_NOTATION = DECIMAL.or(from('E'));
+    public static final IntPredicate USERNAME = LETTER_OR_DIGIT.or(c -> c == '_');
+    public static final IntPredicate RL_NAMESPACE = c -> Character.isBmpCodePoint(c) && Identifier.validNamespaceChar((char) c);
+    public static final IntPredicate RL_PATH = c -> Character.isBmpCodePoint(c) && Identifier.validPathChar((char) c);
+    public static final IntPredicate RESOURCE_LOCATION = c -> Character.isBmpCodePoint(c) && Identifier.isAllowedInIdentifier((char) c);
+    public static final IntPredicate DECIMAL = DIGIT.or(from('.'));
+    public static final IntPredicate SCI_NOTATION = DECIMAL.or(from('E'));
 
-    public static final CharPredicate WILDCARD_CHARS = from('*', '#', '?');
-    public static final CharPredicate FREQUENCY_CHARS = from('-', ' ', '|', '\'', '\"', '_', '+', ':', '(', ')', '?', '!', '/', '@', '$', '`', '~', ',', '.', '#');
+    public static final IntPredicate WILDCARD_CHARS = from('*', '#', '?');
+    public static final IntPredicate FREQUENCY_CHARS = from('-', ' ', '|', '\'', '\"', '_', '+', ':', '(', ')', '?', '!', '/', '@', '$', '`', '~', ',', '.', '#');
 
-    public static CharPredicate from(char character) {
-        return c -> c == character;
+    public static IntPredicate from(char character) {
+        return c -> Character.isBmpCodePoint(c) && c == character;
     }
 
-    public static CharPredicate from(char... chars) {
+    public static IntPredicate from(char... chars) {
         return new SetInputValidator(chars);
     }
 
-    public static CharPredicate or(CharPredicate... validators) {
+    public static IntPredicate or(IntPredicate... validators) {
         if (validators.length == 1) {
             return validators[0];
         }
         return c -> {
-            for (CharPredicate v : validators) {
+            for (IntPredicate v : validators) {
                 if (v.test(c)) {
                     return true;
                 }
@@ -48,9 +49,10 @@ public class InputValidator {
         };
     }
 
-    public static boolean test(String s, CharPredicate predicate) {
-        for (char c : s.toCharArray()) {
-            if (!predicate.test(c)) {
+    public static boolean test(String s, IntPredicate predicate) {
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (!predicate.test(s.codePointAt(i))) {
                 //Invalid name
                 return false;
             }
@@ -58,7 +60,7 @@ public class InputValidator {
         return true;
     }
 
-    private static class SetInputValidator implements CharPredicate {
+    private static class SetInputValidator implements IntPredicate {
 
         private final CharSet validSet;
 
@@ -67,8 +69,8 @@ public class InputValidator {
         }
 
         @Override
-        public boolean test(char c) {
-            return validSet.contains(c);
+        public boolean test(int c) {
+            return Character.isBmpCodePoint(c) && validSet.contains((char) c);
         }
     }
 }
