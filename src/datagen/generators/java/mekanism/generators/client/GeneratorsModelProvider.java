aede81eb1c741c33664421dbdb5398fa8cd4038f
Thiakil <thiakil@users.noreply.github.com>
2026-05-01 14:37:12 +0800

    add generators blockstates

--- src/datagen/generators/java/mekanism/generators/client/GeneratorsModelProvider.java
+++ src/datagen/generators/java/mekanism/generators/client/GeneratorsModelProvider.java
@@ -2,6 +2,7 @@ package mekanism.generators.client;
 
 import mekanism.client.model.BaseModelProvider;
 import mekanism.generators.common.MekanismGenerators;
+import mekanism.generators.common.registries.GeneratorsBlocks;
 import mekanism.generators.common.registries.GeneratorsFluids;
 import mekanism.generators.common.registries.GeneratorsItems;
 import net.minecraft.client.data.models.BlockModelGenerators;
@@ -23,5 +24,35 @@ public class GeneratorsModelProvider extends BaseModelProvider {
         registerBuckets(GeneratorsFluids.FLUIDS, itemModels);
         registerModules(GeneratorsItems.ITEMS, itemModels);
         registerGenerated(itemModels, GeneratorsItems.HOHLRAUM, GeneratorsItems.SOLAR_PANEL, GeneratorsItems.TURBINE_BLADE);
+
+        registerManualStates();
+    }
+
+    private void registerManualStates() {
+        markManualBlockState(GeneratorsBlocks.ADVANCED_SOLAR_GENERATOR);
+        markManualBlockState(GeneratorsBlocks.BIO_GENERATOR);
+        markManualBlockState(GeneratorsBlocks.CONTROL_ROD_ASSEMBLY);
+        markManualBlockState(GeneratorsBlocks.ELECTROMAGNETIC_COIL);
+        markManualBlockState(GeneratorsBlocks.FISSION_FUEL_ASSEMBLY);
+        markManualBlockState(GeneratorsBlocks.FISSION_REACTOR_CASING);
+        markManualBlockState(GeneratorsBlocks.FISSION_REACTOR_LOGIC_ADAPTER);
+        markManualBlockState(GeneratorsBlocks.FISSION_REACTOR_PORT);
+        markManualBlockState(GeneratorsBlocks.FUSION_REACTOR_CONTROLLER);
+        markManualBlockState(GeneratorsBlocks.FUSION_REACTOR_FRAME);
+        markManualBlockState(GeneratorsBlocks.FUSION_REACTOR_LOGIC_ADAPTER);
+        markManualBlockState(GeneratorsBlocks.FUSION_REACTOR_PORT);
+        markManualBlockState(GeneratorsBlocks.GAS_BURNING_GENERATOR);
+        markManualBlockState(GeneratorsBlocks.HEAT_GENERATOR);
+        markManualBlockState(GeneratorsBlocks.LASER_FOCUS_MATRIX);
+        markManualBlockState(GeneratorsBlocks.REACTOR_GLASS);
+        markManualBlockState(GeneratorsBlocks.ROTATIONAL_COMPLEX);
+        markManualBlockState(GeneratorsBlocks.SATURATING_CONDENSER);
+        markManualBlockState(GeneratorsBlocks.SOLAR_GENERATOR);
+        //markManualBlockState(GeneratorsBlocks.SUPERHEATING_ELEMENT); old file?
+        markManualBlockState(GeneratorsBlocks.TURBINE_CASING);
+        markManualBlockState(GeneratorsBlocks.TURBINE_ROTOR);
+        markManualBlockState(GeneratorsBlocks.TURBINE_VALVE);
+        markManualBlockState(GeneratorsBlocks.TURBINE_VENT);
+        markManualBlockState(GeneratorsBlocks.WIND_GENERATOR);
     }
 }
