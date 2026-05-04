77bea2e9c00ff7f195fcc047811a8e8a41875241
Sara Freimer <sara@freimer.com>
2026-05-04 13:51:11 -0500

    Make BinInsertRecipe make use of transactions

--- src/main/java/mekanism/common/recipe/bin/BinInsertRecipe.java
+++ src/main/java/mekanism/common/recipe/bin/BinInsertRecipe.java
@@ -4,9 +4,6 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMaps;
 import it.unimi.dsi.fastutil.objects.ObjectIterator;
-import java.util.ArrayList;
-import java.util.List;
-import mekanism.api.Action;
 import mekanism.api.AutomationType;
 import mekanism.api.annotations.NothingNullByDefault;
 import mekanism.common.attachments.containers.item.ComponentBackedBinInventorySlot;
@@ -21,6 +18,7 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent.ItemCraftedEvent;
 import net.neoforged.neoforge.transfer.item.ItemResource;
+import net.neoforged.neoforge.transfer.transaction.Transaction;
 
 //TODO: Test this recipe in various modded crafting tables/auto crafters
 @NothingNullByDefault
@@ -31,7 +29,7 @@ public class BinInsertRecipe extends BinRecipe {
     @Override
     public boolean matches(CraftingInput inv, Level world) {
         ItemStack binStack = ItemStack.EMPTY;
-        ItemStack foundType = ItemStack.EMPTY;
+        ItemResource foundType = ItemResource.EMPTY;
         //Note: We don't use inv#getItems as that may do unnecessary copies depending on impl
         for (int i = 0, slots = inv.size(); i < slots; ++i) {
             ItemStack stackInSlot = inv.getItem(i);
@@ -43,8 +41,8 @@ public class BinInsertRecipe extends BinRecipe {
                     }
                     binStack = stackInSlot;
                 } else if (foundType.isEmpty()) {
-                    foundType = stackInSlot;
-                } else if (!ItemStack.isSameItemSameComponents(foundType, stackInSlot)) {
+                    foundType = ItemResource.of(stackInSlot);
+                } else if (!foundType.matches(stackInSlot)) {
                     //If we have types that don't stack in the grid at once,
                     // then we cannot combine them both into the bin
                     return false;
@@ -56,16 +54,19 @@ public class BinInsertRecipe extends BinRecipe {
             return false;
         }
         ComponentBackedBinInventorySlot slot = convertToSlot(binStack);
-        ItemStack remaining = slot.insertItem(foundType, Action.SIMULATE, AutomationType.MANUAL);
-        //Return that it doesn't match if our simulation claims we would not be able to accept any items into the bin
-        return !ItemStack.matches(remaining, foundType);
+        try (Transaction simulation = Transaction.openRoot()) {
+            //TODO - 26.1: Validate this is not called from a transactional context
+            // Because auto crafters exist it might be safer to just open this and pass Transaction#getCurrentOpenedTransaction to it
+            //Return that it doesn't match if our simulation claims we would not be able to accept any items into the bin
+            return slot.insert(foundType, 1, simulation, AutomationType.MANUAL) > 0;
+        }
     }
 
     @Override
     public ItemStack assemble(CraftingInput inv) {
         ItemStack binStack = ItemStack.EMPTY;
-        ItemStack foundType = ItemStack.EMPTY;
-        List<ItemStack> foundItems = new ArrayList<>();
+        ItemResource foundType = ItemResource.EMPTY;
+        int toInsert = 0;
         //Note: We don't use inv#getItems as that may do unnecessary copies depending on impl
         for (int i = 0, slots = inv.size(); i < slots; ++i) {
             ItemStack stackInSlot = inv.getItem(i);
@@ -78,13 +79,16 @@ public class BinInsertRecipe extends BinRecipe {
                     binStack = stackInSlot;
                     continue;
                 } else if (foundType.isEmpty()) {
-                    foundType = stackInSlot;
-                } else if (!ItemStack.isSameItemSameComponents(foundType, stackInSlot)) {
+                    foundType = ItemResource.of(stackInSlot);
+                } else if (!foundType.matches(stackInSlot)) {
                     //If we have types that don't stack in the grid at once,
                     // then we cannot combine them both into the bin
                     return ItemStack.EMPTY;
                 }
-                foundItems.add(stackInSlot);
+                //Try inserting a single item (as crafting grids only go one item at a time)
+                //TODO: This is part of what causes it to show a lower number than what potentially gets handled by the container
+                // and is the part we need to address and change for when handling it as a SpecialQIORecipe
+                toInsert += 1;//stackInSlot.count();
             }
         }
         if (binStack.isEmpty() || foundType.isEmpty()) {
@@ -94,27 +98,22 @@ public class BinInsertRecipe extends BinRecipe {
         //Copy the stack
         binStack = binStack.copy();
         ComponentBackedBinInventorySlot slot = convertToSlot(binStack);
-        boolean hasInserted = false;
-        for (ItemStack stack : foundItems) {
-            //Try inserting a single item (as crafting grids only go one item at a time)
-            //TODO: This is part of what causes it to show a lower number than what potentially gets handled by the container
-            // and is the part we need to address and change for when handling it as a SpecialQIORecipe
-            ItemStack toInsert = stack.copyWithCount(1);
-            ItemStack remainder = slot.insertItem(toInsert, Action.EXECUTE, AutomationType.MANUAL);
-            if (remainder.isEmpty()) {
-                //We could insert it
-                hasInserted = true;
-            } else if (hasInserted) {
-                //We managed to insert some of it into the bin, just return our bin stack without marking it as being from a recipe
-                // as there is no benefit to checking if we can insert extra stuff if we know we can't fit more
-                return binStack;
-            } else {
+        try (Transaction transaction = Transaction.openRoot()) {
+            //TODO - 26.1: Validate this is not called from a transactional context
+            // Because auto crafters exist it might be safer to just open this and pass Transaction#getCurrentOpenedTransaction to it
+            int inserted = slot.insert(foundType, toInsert, transaction, AutomationType.MANUAL);
+            if (inserted == 0) {
                 //Return that it doesn't match if we aren't actually able to insert any items into the bin
                 return ItemStack.EMPTY;
+            } else if (inserted == toInsert) {
+                //We could insert it all
+                //TODO: I think we can just skip this when handling it as a SpecialQIORecipe
+                binStack.set(MekanismDataComponents.FROM_RECIPE, true);
             }
+            //Note: If we only managed to insert some of it into the bin, we skip marking our bin stack as being from a recipe
+            // as there is no benefit to checking if we can insert extra stuff if we know we can't fit anymore
+            transaction.commit();
         }
-        //TODO: I think we can just skip this when handling it as a SpecialQIORecipe
-        binStack.set(MekanismDataComponents.FROM_RECIPE, true);
         return binStack;
     }
 
@@ -123,7 +122,7 @@ public class BinInsertRecipe extends BinRecipe {
         int slots = inv.size();
         NonNullList<ItemStack> remainingItems = NonNullList.withSize(slots, ItemStack.EMPTY);
         ItemStack binStack = ItemStack.EMPTY;
-        ItemStack foundType = ItemStack.EMPTY;
+        ItemResource foundType = ItemResource.EMPTY;
         Int2ObjectMap<ItemStack> foundSlots = new Int2ObjectArrayMap<>(slots);
         for (int i = 0; i < slots; ++i) {
             ItemStack stackInSlot = inv.getItem(i);
@@ -136,8 +135,8 @@ public class BinInsertRecipe extends BinRecipe {
                     binStack = stackInSlot;
                     continue;
                 } else if (foundType.isEmpty()) {
-                    foundType = stackInSlot;
-                } else if (!ItemStack.isSameItemSameComponents(foundType, stackInSlot)) {
+                    foundType = ItemResource.of(stackInSlot);
+                } else if (!foundType.matches(stackInSlot)) {
                     //If we have types that don't stack in the grid at once,
                     // then we cannot combine them both into the bin
                     return remainingItems;
@@ -152,17 +151,22 @@ public class BinInsertRecipe extends BinRecipe {
         //Copy the stack
         binStack = binStack.copy();
         ComponentBackedBinInventorySlot slot = convertToSlot(binStack);
-        for (ObjectIterator<Int2ObjectMap.Entry<ItemStack>> iterator = Int2ObjectMaps.fastIterator(foundSlots); iterator.hasNext(); ) {
-            Int2ObjectMap.Entry<ItemStack> entry = iterator.next();
-            ItemStack slotItem = entry.getValue();
-            //Only try inserting a single item into the bin. We execute on a copy of the bin stack so that we can mutate it and chain insertions
-            // to validate if we can insert across multiple slots
-            //TODO: Do we want to allow inserting more when we are acting as a SpecialQIORecipe? (Is that even the case for this as it is the remainder)
-            ItemStack remaining = slot.insertItem(slotItem.copyWithCount(1), Action.EXECUTE, AutomationType.MANUAL);
-            if (!remaining.isEmpty()) {
-                //Can't insert the stack so just mark that we still have a left-over item in that slot
-                remainingItems.set(entry.getIntKey(), remaining);
+        try (Transaction transaction = Transaction.openRoot()) {
+            //TODO - 26.1: Validate this is not called from a transactional context
+            // Because auto crafters exist it might be safer to just open this and pass Transaction#getCurrentOpenedTransaction to it
+            for (ObjectIterator<Int2ObjectMap.Entry<ItemStack>> iterator = Int2ObjectMaps.fastIterator(foundSlots); iterator.hasNext(); ) {
+                Int2ObjectMap.Entry<ItemStack> entry = iterator.next();
+                //Only try inserting a single item into the bin. We execute on a copy of the bin stack so that we can mutate it and chain insertions
+                // to validate if we can insert across multiple slots
+                //TODO: Do we want to allow inserting more when we are acting as a SpecialQIORecipe? (Is that even the case for this as it is the remainder)
+                int inserted = slot.insert(foundType, 1, transaction, AutomationType.MANUAL);
+                if (inserted == 0) {
+                    //Can't insert the stack so just mark that we still have a left-over item in that slot
+                    //TODO - 26.1: Is this meant to be returning a stack of the size that was in the position of the crafting grid?
+                    remainingItems.set(entry.getIntKey(), foundType.toStack(1));
+                }
             }
+            transaction.commit();
         }
         return remainingItems;
     }
@@ -182,22 +186,27 @@ public class BinInsertRecipe extends BinRecipe {
                 ComponentBackedBinInventorySlot slot = convertToSlot(result);
                 ItemResource storedResource = slot.getResource();
                 if (!storedResource.isEmpty()) {
-                    Container craftingMatrix = event.getInventory();
-                    for (int i = 0, slots = craftingMatrix.getContainerSize(); i < slots; ++i) {
-                        ItemStack stack = craftingMatrix.getItem(i);
-                        //Check remaining items
-                        if (stack.count() > 1 && storedResource.matches(stack)) {
-                            //Try to insert any excess items in the slot (we lower it by one as the input slots have not been lowered yet)
-                            ItemStack toInsert = stack.copyWithCount(stack.count() - 1);
-                            ItemStack remaining = slot.insertItem(toInsert, Action.EXECUTE, AutomationType.MANUAL);
-                            if (remaining.isEmpty()) {
-                                //Set it to the single item we skipped
-                                craftingMatrix.setItem(i, stack.copyWithCount(1));
-                            } else if (remaining.count() < toInsert.count()) {
-                                //Set the stack to whatever amount we were unable to insert
-                                craftingMatrix.setItem(i, stack.copyWithCount(remaining.count() + 1));
+                    try (Transaction transaction = Transaction.openRoot()) {
+                        //TODO - 26.1: Validate this is not called from a transactional context
+                        // Because auto crafters exist it might be safer to just open this and pass Transaction#getCurrentOpenedTransaction to it
+                        Container craftingMatrix = event.getInventory();
+                        for (int i = 0, slots = craftingMatrix.getContainerSize(); i < slots; ++i) {
+                            ItemStack stack = craftingMatrix.getItem(i);
+                            //Check remaining items
+                            if (stack.count() > 1 && storedResource.matches(stack)) {
+                                //Try to insert any excess items in the slot (we lower it by one as the input slots have not been lowered yet)
+                                int toInsert = stack.count() - 1;
+                                int inserted = slot.insert(ItemResource.of(stack), stack.count() - 1, transaction, AutomationType.MANUAL);
+                                if (inserted == toInsert) {
+                                    //Set it to the single item we skipped
+                                    craftingMatrix.setItem(i, stack.copyWithCount(1));
+                                } else if (inserted < toInsert) {
+                                    //Set the stack to whatever amount we were unable to insert
+                                    craftingMatrix.setItem(i, stack.copyWithCount(toInsert + 1 - inserted));
+                                }
                             }
                         }
+                        transaction.commit();
                     }
                 }
             }
