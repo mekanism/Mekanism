d210074914a7da68d36de49f1990c0fc8e26aa5c
Thiakil <thiakil@users.noreply.github.com>
2026-05-26 17:20:31 +0800

    fix normals/shader

--- src/main/java/mekanism/client/render/ConfiguratorOverlayHandler.java
+++ src/main/java/mekanism/client/render/ConfiguratorOverlayHandler.java
@@ -4,10 +4,11 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import mekanism.common.lib.transmitter.TransmissionType;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.level.BlockOutlineRenderState;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -41,18 +42,18 @@ class ConfiguratorOverlayHandler implements CustomBlockOutlineRenderer {
         if (renderState.isTranslucent() == translucentPass) {
             Vec3 viewPosition = levelRenderState.cameraRenderState.pos;
             TextureAtlasSprite sprite = MekanismRenderer.overlays.get(type);
-            VertexConsumer buffer = renderer.getBuffer(Sheets.translucentBlockSheet());
+            VertexConsumer buffer = renderer.getBuffer(RenderTypes.eyes(TextureAtlas.LOCATION_BLOCKS));
             poseStack.pushPose();
             poseStack.translate(pos.getX() - viewPosition.x, pos.getY() - viewPosition.y, pos.getZ() - viewPosition.z);
 
             //rotate so the drawn top face is oriented correctly
-            //todo - 26.1: rotate when up/down so the sprite is always correct per the user's perspective
+            //todo - 26.1: rotate when up/down so the sprite is always correct per the user's perspective?
             Vector3fc vecForDirection = face.getUnitVec3f();
             poseStack.rotateAround(new Quaternionf().setAngleAxis(90 * Mth.DEG_TO_RAD, vecForDirection.x(), vecForDirection.y(), vecForDirection.z()), 0.5F, 0.5F, 0.5F);
             poseStack.rotateAround(face.getRotation(), 0.5F, 0.5F, 0.5F);
 
             PoseStack.Pose pose = poseStack.last();
-            Vector3f normal = pose.transformNormal(1, 1, 1, new Vector3f());
+            Vector3f normal = pose.transformNormal(face.getUnitVec3f(), new Vector3f());
             Matrix4f matrix = pose.pose();
             drawFace(buffer, matrix, sprite.getU(1), sprite.getU(0), sprite.getV(0), sprite.getV(1), LightCoordsUtil.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, normal, transmissionColor,
                   0, 1.01F, 1,
