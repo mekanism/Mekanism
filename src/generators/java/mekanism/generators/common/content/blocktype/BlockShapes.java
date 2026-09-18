9a3d3843a0a66736a91dacb32f2c151c9f4e0f95
Sara Freimer <sara@freimer.com>
2026-09-18 13:32:31 -0500

    Fix GBG block shape

--- src/generators/java/mekanism/generators/common/content/blocktype/BlockShapes.java
+++ src/generators/java/mekanism/generators/common/content/blocktype/BlockShapes.java
@@ -112,18 +112,14 @@ public final class BlockShapes {
         ), SOLAR_GENERATOR);
 
         VoxelShapeUtils.setShape(VoxelShapeUtils.combine(
-              box(4, 12, 13, 12, 13, 14), // port_connector_south1
-              box(4, 11.3536, 12.6464, 12, 12.8536, 13.6464), // port_connector_south2
-              box(0.5, 12, 4, 2, 13, 12), // port_connector_west1
-              box(2, 12, 4, 3, 13, 12), // port_connector_west2
+              box(4, 12, 13, 12, 13, 15), // port_connector_south
+              box(1, 12, 4, 3, 13, 12), // port_connector_west
               box(1, 5, 4, 2, 5, 12), // bottom_connector1
               box(4, 5, 14, 12, 5, 15), // bottom_connector2
               box(14, 5, 4, 15, 5, 12), // bottom_connector3
               box(4, 5, 1, 12, 5, 2), // bottom_connector4
-              box(4, 12, 2, 12, 13, 3), // port_connector_north1
-              box(4, 10.6464, 2.0607, 12, 12.1464, 3.0607), // port_connector_north2
-              box(12.8431, 17.0858, 4, 14.3431, 18.0858, 12), // port_connector_east1
-              box(13, 12, 4, 14, 13, 12), // port_connector_east2
+              box(4, 12, 1, 12, 13, 3), // port_connector_north
+              box(13, 12, 4, 15, 13, 12), // port_connector_east
               box(3, 6, 3, 13, 16, 13), // chamber
               box(12, 5, 1, 15, 14, 4), // tank1
               box(1, 5, 1, 4, 14, 4), // tank4
